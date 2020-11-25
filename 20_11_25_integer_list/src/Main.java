8c91a3fc5db06aeea200e905d58529bc7ebf572e
enextus <enextus@gmail.com>
2020-11-25 23:25:07 +0100

    integer list upd

--- 20_11_25_integer_list/src/Main.java
+++ 20_11_25_integer_list/src/Main.java
@@ -1,2 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+
+        System.out.println("list.size(): " + list.size());
+        if (list.size() != 0) System.out.println(list.get(0));
+        else System.out.println("null");
+
+        System.out.println("After try");
+    }
 }
