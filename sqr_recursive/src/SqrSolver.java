9ba8b34933597117354f92dec33aba55c26f0297
enextus <enextus@gmail.com>
2020-11-25 23:41:44 +0100

    upd

---
+++ sqr_recursive/src/SqrSolver.java
@@ -0,0 +1,17 @@
+/**
+ * class SqrSolver
+ */
+public class SqrSolver {
+
+    /**
+     * The method accepts only positive int numbers.
+     *
+     * @param n the sqr of which should be found
+     * @return n * n
+     */
+    public int sqr(int n) {
+        if (n == 1)
+            return 1;
+        return sqr(n - 1) + n + n - 1;
+    }
+}
