dbe27cd0f3597031959865c2fc9aaf9fc403d7dc
enextus <enextus@gmail.com>
2020-11-26 19:34:36 +0100

    upd

--- 20_11_25_integer_list/src/Main.java
+++ 20_11_25_integer_list/src/Main.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         IntegerList listTwo = new ArrayIntegerList();
-        System.out.println("0 -> listTwo.size(): " + listTwo.size());
+/*        System.out.println("0 -> listTwo.size(): " + listTwo.size());
 
         listTwo.addLast(10);
         listTwo.addLast(25);
@@ -13,14 +13,14 @@ public class Main {
 
         System.out.println("-> listTwo.size(): " + listTwo.size());
         System.out.println("-> ArrayIntegerList.INITIAL_CAPACITY: " + ArrayIntegerList.INITIAL_CAPACITY);
-       /* listTwo.set(1, 10);
+       *//* listTwo.set(1, 10);
         System.out.println("2 -> size(): " + listTwo.size());
         listTwo.set(1, 66);
         System.out.println("3 -> size(): " + listTwo.size());
         listTwo.set(3, 566);
         System.out.println("4 -> size(): " + listTwo.size());
         listTwo.set(3, 78);
-        System.out.println("5 -> size(): " + listTwo.size());*/
+        System.out.println("5 -> size(): " + listTwo.size());*//*
         System.out.println();
 
         System.out.print(listTwo.get(0) + ", ");
@@ -33,6 +33,6 @@ public class Main {
         System.out.println("remotedValue = " + listTwo.removeById(0));
         System.out.println("6 -> size(): " + listTwo.size());
         System.out.println("" + listTwo.toString());
-        System.out.println();
+        System.out.println();*/
     }
 }
