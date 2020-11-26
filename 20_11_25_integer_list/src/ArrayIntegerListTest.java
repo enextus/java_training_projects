799230ff7d6ca4b9bb8536fb49c716058eebf54f
enextus <enextus@gmail.com>
2020-11-26 20:36:05 +0100

    test get upd

--- 20_11_25_integer_list/src/ArrayIntegerListTest.java
+++ 20_11_25_integer_list/src/ArrayIntegerListTest.java
@@ -4,26 +4,50 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ArrayIntegerListTest {
 
+
+    ArrayIntegerList list = new ArrayIntegerList();
+
+    /**
+     *
+     */
     @Test
     void addLast() {
     }
 
+    /**
+     *
+     */
     @Test
-    void get() {
+    void testGet_() {
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            list.get(0);
+        });
     }
 
+    /**
+     *
+     */
     @Test
     void set() {
     }
 
+    /**
+     *
+     */
     @Test
     void removeById() {
     }
 
+    /**
+     *
+     */
     @Test
     void size() {
     }
 
+    /**
+     *
+     */
     @Test
     void clear() {
     }
