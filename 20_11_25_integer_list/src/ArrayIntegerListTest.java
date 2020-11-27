f6a0f047600421e43a4e714c3e263f8145a94c3c
enextus <enextus@gmail.com>
2020-11-27 07:04:59 +0100

    test upd

--- 20_11_25_integer_list/src/ArrayIntegerListTest.java
+++ 20_11_25_integer_list/src/ArrayIntegerListTest.java
@@ -56,6 +56,9 @@ class ArrayIntegerListTest {
 
     @Test
     void testSize_() {
+        assertThrows(IndexOutOfBoundsException.class, () -> {
+            list.set(0, 10);
+        });
     }
 
     @Test
