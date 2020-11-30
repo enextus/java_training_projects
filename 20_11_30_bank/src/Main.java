34b4cd4647b7240be88c6347d5b4ba61980aaf8a
enextus <enextus@gmail.com>
2020-11-30 19:22:04 +0100

    bank upd

--- 20_11_30_bank/src/Main.java
+++ 20_11_30_bank/src/Main.java
@@ -3,9 +3,9 @@ public class Main {
 
         LoanConsumer loanConsumer1 = new LoanConsumer("Mark", 18, 25000);
         LoanConsumer loanConsumer2 = new LoanConsumer("Petr", 50, 30000);
-        LoanConsumer loanConsumer3 = new LoanConsumer("Petr", 50, 19000);
+        LoanConsumer loanConsumer3 = new LoanConsumer("Mario", 55, 19000);
         LoanConsumer loanConsumer4 = new LoanConsumer("Gerhard", 17, 200000);
-        LoanConsumer loanConsumer5 = new LoanConsumer("Gerhard", 80, 200000);
+        LoanConsumer loanConsumer5 = new LoanConsumer("Axel", 80, 200000);
 
 
         LoanIssuer loanIssuer1 = new LoanIssuer("Anton", false, false);
