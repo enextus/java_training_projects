338823bdbead53c0d906cc8723273efccdc4f720
enextus <enextus@gmail.com>
2020-11-30 12:43:34 +0100

    bank upd

---
+++ 20_11_30_bank/src/LoanIssuer.java
@@ -0,0 +1,28 @@
+public class LoanIssuer {
+    private String name;
+    private boolean isLazy;
+    private boolean isKind;
+
+    public LoanIssuer(String name, boolean isLazy, boolean isKind) {
+        this.name = name;
+        this.isLazy = isLazy;
+        this.isKind = isKind;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public boolean isLazy() {
+        return isLazy;
+    }
+
+    public boolean isKind() {
+        return isKind;
+    }
+
+    public boolean toIssue() {
+
+        return false;
+    }
+}
