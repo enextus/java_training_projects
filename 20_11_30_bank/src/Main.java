3d8c78bd4ab8072caf3052daacb94b948b966151
enextus <enextus@gmail.com>
2020-11-30 19:03:42 +0100

    bank upd

--- 20_11_30_bank/src/Main.java
+++ 20_11_30_bank/src/Main.java
@@ -3,9 +3,12 @@ public class Main {
 
         LoanConsumer loanConsumer1 = new LoanConsumer("Mark", 18, 25000);
         LoanConsumer loanConsumer2 = new LoanConsumer("Petr", 50, 30000);
+        LoanConsumer loanConsumer3 = new LoanConsumer("Gerhard", 17, 200000);
 
         LoanIssuer loanIssuer1 = new LoanIssuer("Anton", false, false);
         LoanIssuer loanIssuer2 = new LoanIssuer("Stephan", true, false);
+        LoanIssuer loanIssuer3 = new LoanIssuer("Alex", false, true);
+        LoanIssuer loanIssuer4 = new LoanIssuer("Max", true, true);
 
 
         System.out.println(loanIssuer1.printMsg(loanIssuer1.toIssue(loanConsumer1), loanConsumer1));
