b3511d6ce1a76ed600e9962a21a61c525f6487b6
enextus <enextus@gmail.com>
2020-11-30 23:47:57 +0100

    bank upd

--- 20_11_30_bank/src/LoanIssuerTest.java
+++ 20_11_30_bank/src/LoanIssuerTest.java
@@ -4,7 +4,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class LoanIssuerTest {
 
+    LoanIssuer loanIssuer1 = new LoanIssuer("Anton", false, false);
+    LoanIssuer loanIssuer2 = new LoanIssuer("Stephan", true, false);
+    LoanIssuer loanIssuer3 = new LoanIssuer("Alex", false, true);
+    LoanIssuer loanIssuer4 = new LoanIssuer("Max", true, true);
+
+    LoanConsumer loanConsumer1 = new LoanConsumer("Mark", 18, 25000);
+    LoanConsumer loanConsumer2 = new LoanConsumer("Petr", 50, 30000);
+    LoanConsumer loanConsumer3 = new LoanConsumer("Mario", 55, 19000);
+    LoanConsumer loanConsumer4 = new LoanConsumer("Gerhard", 17, 200000);
+    LoanConsumer loanConsumer5 = new LoanConsumer("Axel", 80, 200000);
+
     @Test
-    void toIssue() {
+    void testToIssue_() {
+        assertEquals(false, loanIssuer1.toIssue(loanConsumer1));
     }
 }
