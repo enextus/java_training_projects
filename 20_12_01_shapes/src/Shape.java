2c5dfaad530d7eb2ca51756ce38f9d726d5a6098
enextus <enextus@gmail.com>
2020-12-02 17:49:13 +0100

    unitTests upd

--- 20_12_01_shapes/src/Shape.java
+++ 20_12_01_shapes/src/Shape.java
@@ -2,4 +2,13 @@ public abstract class Shape {
 
     protected char symbol;
 
+    public Shape(char symbol) {
+        this.symbol = symbol;
+    }
+
+    public abstract void draw();
+
+    public char getSymbol() {
+        return symbol;
+    }
 }
