70c096b2efea10e86992c9f303da84b0afbc01f2
enextus <enextus@gmail.com>
2020-12-02 17:58:31 +0100

    Picture upd

---
+++ 20_12_01_shapes/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        Line line1 = new Line('-', 15);
+        Rectangle rectangle1 = new Rectangle('*', 20, 20);
+
+        Shape[] shapes = {line1, rectangle1};
+
+        Picture picture1 = new Picture('=', shapes);
+    }
+}
