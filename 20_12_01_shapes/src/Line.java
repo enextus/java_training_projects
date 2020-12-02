7d5e85edcc414801c07cb5bc117ebe72e04c66a2
enextus <enextus@gmail.com>
2020-12-02 17:52:04 +0100

    Line upd

---
+++ 20_12_01_shapes/src/Line.java
@@ -0,0 +1,16 @@
+
+
+public class Line extends Shape {
+
+    private int length;
+
+    public Line(char symbol) {
+        super(symbol);
+        this.length = length;
+    }
+
+    @Override
+    public void draw() {
+
+    }
+}
