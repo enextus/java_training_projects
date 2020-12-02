7c777b023d0abbf4fd68e089dd9508fe7c90b9a9
enextus <enextus@gmail.com>
2020-12-03 00:53:37 +0100

    Test upd

--- 20_12_02_codingString/src/StringToCodeTest.java
+++ 20_12_02_codingString/src/StringToCodeTest.java
@@ -5,31 +5,31 @@ import static org.junit.jupiter.api.Assertions.*;
 class StringToCodeTest {
 
     @Test
-    void test_Coded_String_aa_Result_a2() {
+    void test_String_aa_Result_a2() {
         StringToCode stringToCode = new StringToCode("aa");
         assertEquals("a2", stringToCode.stringCoding());
     }
 
     @Test
-    void test_Coded_String_ab_Result_ab() {
+    void test_String_ab_Result_ab() {
         StringToCode stringToCode = new StringToCode("ab");
         assertEquals("ab", stringToCode.stringCoding());
     }
 
     @Test
-    void test_Coded_String_aaa_Result_a3() {
+    void test_String_aaa_Result_a3() {
         StringToCode stringToCode = new StringToCode("aaa");
         assertEquals("a3", stringToCode.stringCoding());
     }
 
     @Test
-    void test_Coded_String_abb_Result_ab2() {
+    void test_String_abb_Result_ab2() {
         StringToCode stringToCode = new StringToCode("abb");
         assertEquals("ab2", stringToCode.stringCoding());
     }
 
     @Test
-    void test_Coded_String_aaabbcdddd_Result_a3b2cd4() {
+    void test_String_aaabbcdddd_Result_a3b2cd4() {
         StringToCode stringToCode = new StringToCode("aaabbcdddd");
         assertEquals("a3b2cd4", stringToCode.stringCoding());
     }
