8319d8b0b29588626791e7ec3ad83ec49b1d4da2
enextus <enextus@gmail.com>
2020-12-02 17:57:46 +0100

    Picture upd

---
+++ 20_12_01_shapes/src/Picture.java
@@ -0,0 +1,14 @@
+public class Picture extends Shape {
+
+    private Shape[] shapes;
+
+    public Picture(char symbol, Shape[] shapes) {
+        super(symbol);
+        this.shapes = shapes;
+    }
+
+    @Override
+    public void draw() {
+
+    }
+}
