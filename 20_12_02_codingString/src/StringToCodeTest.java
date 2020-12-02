7ee88011ec8d5da0302bc2356f3584d1df8a3044
enextus <enextus@gmail.com>
2020-12-03 00:51:37 +0100

    Test upd

--- 20_12_02_codingString/src/StringToCodeTest.java
+++ 20_12_02_codingString/src/StringToCodeTest.java
@@ -23,6 +23,12 @@ class StringToCodeTest {
     }
 
     @Test
+    void test_Coded_String_abb_Result_ab2() {
+        StringToCode stringToCode = new StringToCode("abb");
+        assertEquals("ab2", stringToCode.stringCoding());
+    }
+
+    @Test
     void test_Coded_String_aaabbcdddd_Result_a3b2cd4() {
         StringToCode stringToCode = new StringToCode("aaabbcdddd");
         assertEquals("a3b2cd4", stringToCode.stringCoding());
