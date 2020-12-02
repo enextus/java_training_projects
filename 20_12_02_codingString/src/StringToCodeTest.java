101d591ed6e866c626944dde5a257df5b0cb2f61
enextus <enextus@gmail.com>
2020-12-03 00:22:13 +0100

    StringToCode ref

--- 20_12_02_codingString/src/StringToCodeTest.java
+++ 20_12_02_codingString/src/StringToCodeTest.java
@@ -1,8 +1,10 @@
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class StringToCodeTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
     void stringCoding() {
     }
 }
