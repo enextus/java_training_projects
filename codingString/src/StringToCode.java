901ec391daba8cac82dfa65f1b48f263814cba2c
enextus <enextus@gmail.com>
2020-12-02 21:12:19 +0100

    Main upd

--- codingString/src/StringToCode.java
+++ codingString/src/StringToCode.java
@@ -1,5 +1,6 @@
 public class StringToCode {
     String stringToCode;
+    String result;
 
     public StringToCode(String stringToCode) {
         this.stringToCode = stringToCode;
@@ -11,16 +12,44 @@ public class StringToCode {
 
     public String codeString() {
 
+        String result = "";
+        int count = 1;
+
         System.out.println(stringToCode.length());
 
         for (int i = 0; i < stringToCode.length() - 1; i++) {
-            // stringToCode += stringToCode.charAt(i);
 
-            System.out.print(stringToCode.charAt(i) + ",  ");
+            System.out.print("char[i]: " + stringToCode.charAt(i) + ",  ");
 
-            if ()
+            if ((i > 0) && (stringToCode.charAt(i - 1) == stringToCode.charAt(i))) {
+
+                count++;
+                System.out.println("count: " + count);
+
+            } else {
+                System.out.println("count -> " + count);
+
+                addToResult(stringToCode.charAt(i), count);
+                count = 1;
+            }
+
+            System.out.println("i: " + i + ", count: " + count + ", 36. result: " + getResult());
         }
 
-        return stringToCode;
+        return getResult();
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    private void addToResult(char a, int b) {
+        if (b == 1) {
+            result += "" + a;
+        } else {
+            result += "" + a + b;
+        }
+
+        System.out.println(result);
     }
 }
