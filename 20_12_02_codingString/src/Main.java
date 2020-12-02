2b8ae516cf07e95bdcbb0cd9225aa8aabc6da324
enextus <enextus@gmail.com>
2020-12-03 00:05:14 +0100

    StringToCode ref

--- 20_12_02_codingString/src/Main.java
+++ 20_12_02_codingString/src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         StringToCode stringToCode = new StringToCode("aaabbcdddd");
-
         System.out.println(stringToCode.stringCoding());
+
+        StringToCode stringToCodeTwo = new StringToCode("ddfffolllklllpp");
+        System.out.println(stringToCodeTwo.stringCoding());
     }
 }
