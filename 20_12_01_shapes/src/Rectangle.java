39771f4f93af0757b0395ba66186923e3dfec6db
enextus <enextus@gmail.com>
2020-12-02 17:55:32 +0100

    Rectangle upd

---
+++ 20_12_01_shapes/src/Rectangle.java
@@ -0,0 +1,16 @@
+public class Rectangle extends Shape {
+
+    private int width;
+    private int height;
+
+    public Rectangle(char symbol, int width, int height) {
+        super(symbol);
+        this.width = width;
+        this.height = height;
+    }
+
+    @Override
+    public void draw() {
+
+    }
+}
