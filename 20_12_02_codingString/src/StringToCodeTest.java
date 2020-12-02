7c57e3e39beabaacc332f0c710d588b5e694e232
enextus <enextus@gmail.com>
2020-12-03 00:33:04 +0100

    Test upd

--- 20_12_02_codingString/src/StringToCodeTest.java
+++ 20_12_02_codingString/src/StringToCodeTest.java
@@ -5,7 +5,13 @@ import static org.junit.jupiter.api.Assertions.*;
 class StringToCodeTest {
 
     @Test
-    void test_aaabbcdddd_a3b2cd4() {
+    void test_Coded_String_a_Result_a() {
+        StringToCode stringToCode = new StringToCode("a");
+        assertEquals("a", stringToCode.stringCoding());
+    }
+
+    @Test
+    void test_Coded_String_aaabbcdddd_Result_a3b2cd4() {
         StringToCode stringToCode = new StringToCode("aaabbcdddd");
         assertEquals("a3b2cd4", stringToCode.stringCoding());
     }
