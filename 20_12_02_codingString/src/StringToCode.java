3a7656c85397d4292be05cd1535a304e71a47f7e
enextus <enextus@gmail.com>
2020-12-03 01:06:02 +0100

    Test upd

--- 20_12_02_codingString/src/StringToCode.java
+++ 20_12_02_codingString/src/StringToCode.java
@@ -8,8 +8,9 @@ public class StringToCode {
     }
 
     /**
-     * Method returned coded variable inputString; inputString has min length of two characters; Code it's as this rule: it will be counted the length of series
-     * of similar characters and after initial character will placed a count number of each series length.
+     * Method returned coded variable inputString; inputString has min length of two characters;
+     * Code it's as this rule: it will be counted the length of series of similar characters and
+     * after initial character will placed a count number of each series length.
      *
      * @return result
      */
