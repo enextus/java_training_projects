24f01b7d309b46270d3e449a4b10b6a1e11bf603
enextus <enextus@gmail.com>
2020-12-03 08:09:09 +0100

    some upd

--- 20_12_01_shapes/src/Main.java
+++ 20_12_01_shapes/src/Main.java
@@ -3,15 +3,23 @@ public class Main {
 
         Line line1 = new Line('-', 15);
         Rectangle rectangle1 = new Rectangle('*', 9, 7);
+        Rectangle rectangle2 = new Rectangle('@', 5, 11);
 
         Shape[] shapes = new Shape[]{line1, rectangle1};
-        Picture picture1 = new Picture('=', shapes);
+        Shape[] shapes1 = new Shape[]{rectangle2, line1, rectangle1};
 
-        // line1.draw();
-        // rectangle1.draw();
+        Picture picture1 = new Picture('=', shapes);
+        Picture picture2 = new Picture('=', shapes1);
+
+        line1.draw();
+        rectangle1.draw();
 
         picture1.drawBorder();
         picture1.draw();
         picture1.drawBorder();
+
+        picture2.drawBorder();
+        picture2.draw();
+        picture2.drawBorder();
     }
 }
