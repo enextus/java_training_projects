2ef6a42fb5317ed22dc9a7023a2117824ac19549
enextus <enextus@gmail.com>
2020-12-09 09:17:06 +0100

    some upd

--- 20_12_04_iterator_example/src/Main.java
+++ 20_12_04_iterator_example/src/Main.java
@@ -8,11 +8,11 @@ public class Main {
         int[] array = {10, -8, 0, 15};
         System.out.println(getLineNumber() + "   " + Arrays.toString(array));
 
-/*        Iterator<Integer> simpleArrayIterator = new SimpleArrayIterator(array);
+        Iterator<Integer> simpleArrayIterator = new SimpleArrayIterator(array);
         Iterator<Integer> backwardArrayIterator = new BackwardArrayIterator(array);
         Iterator<Integer> increasingArrayIterator = new IncreasingArrayIterator(array);
 
-        SimpleArrayIterator simpleArrayIterator = new SimpleArrayIterator(array);
+/*        SimpleArrayIterator simpleArrayIterator = new SimpleArrayIterator(array);
         BackwardArrayIterator backwardArrayIterator = new BackwardArrayIterator(array);
         IncreasingArrayIterator increasingArrayIterator = new IncreasingArrayIterator(array);
 
@@ -20,9 +20,9 @@ public class Main {
         Iterator backwardArrayIterator = new BackwardArrayIterator(array);
         Iterator increasingArrayIterator = new IncreasingArrayIterator(array);*/
 
-        Object simpleArrayIterator = new SimpleArrayIterator(array);
+/*        Object simpleArrayIterator = new SimpleArrayIterator(array);
         Object backwardArrayIterator = new BackwardArrayIterator(array);
-        Object increasingArrayIterator = new IncreasingArrayIterator(array);
+        Object increasingArrayIterator = new IncreasingArrayIterator(array);*/
 
         iterate(simpleArrayIterator);
         iterate(backwardArrayIterator);
