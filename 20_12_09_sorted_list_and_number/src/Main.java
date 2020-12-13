1267cdf779d8e09c9c2ced3dd092f4259a3975b5
enextus <enextus@gmail.com>
2020-12-13 10:56:47 +0100

    upd

--- 20_12_09_sorted_list_and_number/src/Main.java
+++ 20_12_09_sorted_list_and_number/src/Main.java
@@ -1,3 +1,4 @@
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -9,30 +10,21 @@ public class Main {
 
         System.out.println(intArrOne[0]);
 
-
-
         Integer a = Integer.valueOf("123"); // Integer a = new Integer("123");
 
         List<Integer> list = Arrays.asList(intArrOne);
 
+        List<Integer> list2 = new ArrayList<>(Arrays.asList(intArrOne));
 
-        System.out.println(list.isEmpty());
+        System.out.println("list2.isEmpty(): " + list2.isEmpty());
 
+        System.out.println("list2:  " + list2);
+        System.out.println("list2.size(): " + list2.size());
+        System.out.println("list2:  " + list2.getClass());
 
-        System.out.println("list:  " + list);
-        System.out.println("list.size(): " + list.size());
-        System.out.println("list:  " + list.getClass());
+        list2.add(a);
 
-        list.add(a);
-
-        System.out.println(list);
-
-
-
-
-
-
-
+        System.out.println("list2: " + list2);
 
         Solution solution = new Solution();
         System.out.println(solution.solve(list, 6));
