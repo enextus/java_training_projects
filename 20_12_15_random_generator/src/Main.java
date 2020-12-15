a48921b311446ead750a141170678f2c3e22e4cc
enextus <enextus@gmail.com>
2020-12-15 20:52:50 +0100

    upd

--- 20_12_15_random_generator/src/Main.java
+++ 20_12_15_random_generator/src/Main.java
@@ -31,17 +31,18 @@ public class Main {
         System.out.println("randomGenerator.nextInts(n): " + randomGenerator2.nextInts(12));
 
         /**
-         * 11
+         * /home/enextus/.jdks/openjdk-14.0.2/bin/java -javaagent:/snap/intellij-idea-ultimate/263/lib/idea_rt.jar=35401:/snap/intellij-idea-ultimate/263/bin -Dfile.encoding=UTF-8 -classpath /home/enextus/IdeaProjects/java_training_projects/20_12_15_random_generator/out/production/20_12_15_random_generator Main
+         * 7
          *
          * 468121027
          *
          * RangeRandomRule{max=25, random=java.util.Random@6ce253f1}
          *
+         * -3
+         * -3
          * 0
+         * 11
          * 0
-         * 11
-         * 11
-         * 11
          * randomGenerator.nextInts(n): [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
          * randomGenerator.nextInts(n): [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
          *
