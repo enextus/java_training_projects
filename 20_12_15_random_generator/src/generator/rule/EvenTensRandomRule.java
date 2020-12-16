cc958c2a479419d355d8d0ae1eadf58fba6cb5bb
enextus <enextus@gmail.com>
2020-12-16 18:55:02 +0100

    upd

---
+++ 20_12_15_random_generator/src/generator/rule/EvenTensRandomRule.java
@@ -0,0 +1,37 @@
+package generator.rule;
+
+import generator.RandomRule;
+
+        import java.util.Random;
+
+/**
+ * implementation of the interface named "OddTensRandomRule". The Random odd tens numbers.
+ */
+public class EvenTensRandomRule implements RandomRule {
+
+    private int max;
+    private Random random;
+
+    public EvenTensRandomRule(int max) {
+        this.max = max;
+        this.random = new Random();
+    }
+
+    @Override
+    public int nextInt() {
+
+        while (true) {
+            int nextInt = random.nextInt(max);
+            if ((nextInt % 2) == 0)
+                return ((nextInt % 100) % 10) * 10;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "OddTensRandomRule{" +
+                "max=" + max +
+                ", random=" + random +
+                '}';
+    }
+}
