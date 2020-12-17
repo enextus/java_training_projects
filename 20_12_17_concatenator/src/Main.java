4156a947563817bd8895b6083a7a573e3d24534e
enextus <enextus@gmail.com>
2020-12-18 00:14:59 +0100

    upd

--- 20_12_17_concatenator/src/Main.java
+++ 20_12_17_concatenator/src/Main.java
@@ -1,13 +1,52 @@
+import concatenator.ConcatenationTest;
+import concatenator.IConcatenator;
+import concatenator.concatenator.StringBufferConcatenator;
+import concatenator.concatenator.StringBuilderConcatenator;
+import concatenator.concatenator.StringConcatenator;
+
 public class Main {
+
     public static void main(String[] args) {
-        long now = System.currentTimeMillis();
+// time test
+//        String res = "";
+//        long beforeConcatenation = System.currentTimeMillis();
+//        for (int i = 0; i < 10000; i++) {
+//            res += "Evgeny Borisovich";
+//        }
+//        long timeConcatenated = System.currentTimeMillis() - beforeConcatenation;
+//        System.out.println(timeConcatenated);
+//
+//        List<String> strings = new ArrayList<>();
+//        strings.toArray();
 
 
-        String res = "";
-        System.out.println("1. now: " + now);
-        for (int i = 0; i < 1000; i++){
-            res+= "ABCDE";
-        }
-        System.out.println("2. now: " + now);
+        // StringBuilder
+
+//        StringBuilder builder = new StringBuilder();
+//        builder.append("Evgeny")
+//                .append(" ")
+//                .append("Borisovich");
+//
+//        builder.append("Evgeny");
+//        builder.append(" ");
+//        builder.append("Borisovich");
+//
+//        String str = builder.toString();
+//        System.out.println(str);
+
+        IConcatenator stringConcatenator = new StringConcatenator();
+        IConcatenator stringBuilderConcatenator = new StringBuilderConcatenator();
+        IConcatenator stringBufferConcatenator = new StringBufferConcatenator();
+
+        ConcatenationTest test;
+        //string test
+        test = new ConcatenationTest(stringConcatenator);
+        test.test(100000, "Evgeny Borisovich");
+        //string builder test
+        test = new ConcatenationTest(stringBuilderConcatenator);
+        test.test(10000000, "Evgeny Borisovich");
+        //string buffer test
+        test = new ConcatenationTest(stringBufferConcatenator);
+        test.test(10000000, "Evgeny Borisovich");
     }
 }
---
+++ 20_12_17_concatenator/src/concatenator/concatenator/StringBufferConcatenator.java
@@ -0,0 +1,16 @@
+package concatenator.concatenator;
+
+import concatenator.IConcatenator;
+
+public class StringBufferConcatenator implements IConcatenator {
+
+    @Override
+    public void concatenate(String[] arrayToConcatenate) {
+        StringBuffer buffer = new StringBuffer();
+
+        for (String str : arrayToConcatenate)
+            buffer.append(str);
+
+        buffer.toString(); // anschließende Umwandlung Obj toString
+    }
+}
