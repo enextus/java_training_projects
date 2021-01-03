09a2bb24b3360a30c01fa2d343b4e20a8d567cbb
enextus <enextus@gmail.com>
2021-01-03 17:06:00 +0300

    upd

--- 21_01_03_my_max_2/src/MyStackTest.java
+++ 21_01_03_my_max_2/src/MyStackTest.java
@@ -11,10 +11,16 @@ class MyStackTest {
         assertThrows(IndexOutOfBoundsException.class, () -> myStack.getLast());
     }
 
-
     @Test
     public void testAddLast() {
+        myStack.addLast(0);
+        assertEquals(0, myStack.getLast());
 
+        myStack.addLast(-1);
+        assertEquals(-1, myStack.getLast());
+
+        myStack.addLast(10);
+        assertEquals(10, myStack.getLast());
     }
 
     @Test
@@ -30,12 +36,12 @@ class MyStackTest {
     }
 
     @Test
-    void testSize_empty_Stack() {
+    public void testSize_empty_Stack() {
         assertEquals(0, myStack.size());
     }
 
     @Test
-    void testSize_not_empty_Stack() {
+    public void testSize_not_empty_Stack() {
         myStack.addLast(1);
         assertEquals(1, myStack.size());
 
@@ -48,7 +54,15 @@ class MyStackTest {
 
 
     @Test
-    void getMax() {
+    public void testGetMax() {
+        myStack.addLast(1);
+        assertEquals(1, myStack.getMax());
+
+        myStack.addLast(100);
+        assertEquals(100, myStack.getMax());
+
+        myStack.addLast(-5);
+        assertEquals(100, myStack.getMax());
     }
 
 }
