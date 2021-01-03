2afa35a90770a245459b2bc4295466726a50f0d3
enextus <enextus@gmail.com>
2021-01-03 17:00:11 +0300

    upd

--- 21_01_03_my_max_2/src/MyStackTest.java
+++ 21_01_03_my_max_2/src/MyStackTest.java
@@ -4,23 +4,48 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MyStackTest {
 
+    MyStack myStack = new MyStack();
+
+    @Test
+    public void testRemoveLast_empty_Stack_throwsIOOBE() {
+        assertThrows(IndexOutOfBoundsException.class, () -> myStack.getLast());
+    }
+
+    @Test
+    public void testRemoveLast_not_empty_Stack() {
+        myStack.addLast(0);
+        assertEquals(0, myStack.getLast());
+
+        myStack.addLast(-1);
+        assertEquals(-1, myStack.getLast());
+
+        myStack.addLast(1);
+        assertEquals(1, myStack.getLast());
+    }
+
+    @Test
+    void testSize_empty_Stack() {
+        assertEquals(0, myStack.size());
+    }
+
+    @Test
+    void testSize_not_empty_Stack() {
+        myStack.addLast(1);
+        assertEquals(1, myStack.size());
+
+        myStack.addLast(1);
+        assertEquals(2, myStack.size());
+
+        myStack.addLast(1);
+        assertEquals(3, myStack.size());
+    }
+
+
     @Test
     void getMax() {
     }
 
     @Test
-    void addLast() {
-    }
-
-    @Test
-    void removeLast() {
-    }
-
-    @Test
-    void getLast() {
-    }
-
-    @Test
-    void size() {
+    public void addLast() {
     }
 }
