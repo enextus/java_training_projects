1b901a10d73a2a06014ad08d8d02fbb58f7cb7ce
enextus <enextus@gmail.com>
2021-01-03 16:28:14 +0300

    upd

--- 21_01_03_my_max_2/src/Main.java
+++ 21_01_03_my_max_2/src/Main.java
@@ -4,11 +4,11 @@ public class Main {
 
         MyStack myStack = new MyStack();
 
-        System.out.println(myStack.getLast());
+/*        System.out.println(myStack.getLast());
         System.out.println(myStack.getMax());
         System.out.println(myStack.size());
 
-        System.out.println();
+        System.out.println();*/
 
         System.out.println(myStack.addLast(5));
         System.out.println(myStack.getLast());
---
+++ 21_01_03_my_max_2/src/MyStackTest.java
@@ -0,0 +1,26 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyStackTest {
+
+    @Test
+    void getMax() {
+    }
+
+    @Test
+    void addLast() {
+    }
+
+    @Test
+    void removeLast() {
+    }
+
+    @Test
+    void getLast() {
+    }
+
+    @Test
+    void size() {
+    }
+}
