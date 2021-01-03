46a4360f6b42b5b3f4fd5338467236cfbdd19e34
enextus <enextus@gmail.com>
2021-01-03 14:21:08 +0300

    upd

--- 20_12_18_my_max/src/Main.java
+++ 20_12_18_my_max/src/Main.java
@@ -4,50 +4,15 @@ public class Main {
 
         MyStack myStack = new MyStack();
 
-        myStack.getMax();
-
         myStack.addLast(144);
 
         myStack.getMax();
+        myStack.size();
 
-        myStack.addLast(44);
-        myStack.addLast(77);
-        myStack.addLast(700);
-
+        // removeLast()
         myStack.getMax();
 
-//        myStack.push(44);
-//        myStack.push(34);
-//        myStack.push(344);
-//
-//        myStack.push(3);
-//        myStack.push(5);
-//        myStack.getMax();
-//
-//        System.out.println();
-//
-//        myStack.push(7);
-//        myStack.push(19);
-//        myStack.push(60);
-//        myStack.getMax();
-//
-//        System.out.println();
-//        myStack.pop();
-//        myStack.pop();
-//        myStack.pop();
-//        myStack.pop();
-//        myStack.pop();
-//        myStack.pop();
-//        myStack.getMax();
 
         System.out.println();
-
-        // myStack.pop();
-        // myStack.peek();
     }
 }
-
-// getLast()
-// removeLast()
-// addLast()
-// https://www.geeksforgeeks.org/find-maximum-in-a-stack-in-o1-time-and-o1-extra-space/
