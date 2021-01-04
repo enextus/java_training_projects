8cdd1bcf8dd745f5c6cb61e95e8d0d67a8576762
enextus <enextus@gmail.com>
2021-01-04 15:59:46 +0300

    upd

---
+++ 21_01_04_stackMax_3/src/StackMax.java
@@ -0,0 +1,32 @@
+import java.util.ArrayDeque;
+
+public class StackMax {
+
+    private final ArrayDeque<Integer> source;
+    private final ArrayDeque<Integer> currentMax;
+
+    public StackMax() {
+        source = new ArrayDeque<>();
+        currentMax = new ArrayDeque<>();
+    }
+
+    public void addLast(int elt) {
+        source.addLast(elt);
+    }
+
+    public int getLast() {
+        return source.getLast();
+    }
+
+/*    public int removeLast() {
+
+    }
+
+    public int size() {
+
+    }
+
+    public int getMax() {
+        // foreach here over all the elements of the 'source'
+    }*/
+}
