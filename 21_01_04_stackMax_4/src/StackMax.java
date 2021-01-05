5f2a0372874a257f8b338ee3da99d90795fed2c3
enextus <enextus@gmail.com>
2021-01-05 08:51:52 +0300

    upd

---
+++ 21_01_04_stackMax_4/src/StackMax.java
@@ -0,0 +1,53 @@
+import java.util.ArrayDeque;
+
+public class StackMax {
+
+    private final ArrayDeque<Integer> dequeElem;
+    private final ArrayDeque<Integer> dequeMaxs;
+
+    public StackMax() {
+        dequeElem = new ArrayDeque<>();
+        dequeMaxs = new ArrayDeque<>();
+    }
+
+    public void addLast(int elt) {
+        dequeElem.addLast(elt);
+
+        if (dequeMaxs.isEmpty()) {
+            dequeMaxs.addLast(elt);
+        } else {
+            if (elt > dequeMaxs.getLast()) {
+                dequeMaxs.addLast(elt);
+            } else {
+                dequeMaxs.addLast(dequeMaxs.getLast());
+            }
+        }
+    }
+
+    public int getLast() {
+        if (dequeElem.isEmpty())
+            throw new IndexOutOfBoundsException();
+
+        return dequeElem.getLast();
+    }
+
+    public int removeLast() {
+        if (dequeElem.isEmpty())
+            throw new IndexOutOfBoundsException();
+
+        dequeMaxs.removeLast();
+
+        return dequeElem.removeLast();
+    }
+
+    public int size() {
+        return dequeElem.size();
+    }
+
+    public int getMax() {
+        if (dequeElem.isEmpty())
+            throw new IndexOutOfBoundsException();
+
+        return dequeMaxs.getLast();
+    }
+}
