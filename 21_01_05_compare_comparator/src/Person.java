b15593b966292958a954714f1fd9ba01126744d7
enextus <enextus@gmail.com>
2021-01-07 14:06:04 +0300

    upd

--- 21_01_05_compare_comparator/src/Person.java
+++ 21_01_05_compare_comparator/src/Person.java
@@ -1,9 +1,13 @@
 import com.sun.source.util.Trees;
 
+import java.util.Comparator;
 import java.util.Set;
 import java.util.TreeSet;
 
 public class Person implements Comparable<Person> {
+    public int getAge() {
+        return age;
+    }
 
     int age;
 
@@ -25,16 +29,26 @@ public class Person implements Comparable<Person> {
     }
 }
 
+class ComparePerson implements Comparator<Person>{
+
+    @Override
+    public int compare(Object o1, Object o2) {
+        return o1.getAge() - o2.getAge();
+    }
+}
+
 class Main {
     public static void main(String[] args) {
 
         Person person1 = new Person(10);
         Person person2 = new Person(1);
+        Person person3 = new Person(100);
 
-        Set set = new TreeSet();
+        Set set = new TreeSet(new ComparePerson());
 
         set.add(person1);
         set.add(person2);
+        set.add(person3);
 
         for (Object o:set
              ) {
