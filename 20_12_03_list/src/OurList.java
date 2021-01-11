184b0702a1931ccf6f529335d37b3898b78764fb
enextus <enextus@gmail.com>
2021-01-11 08:47:00 +0100

    some upd

--- 20_12_03_list/src/OurList.java
+++ 20_12_03_list/src/OurList.java
@@ -1,6 +1,17 @@
+import java.util.Arrays;
+import java.util.Comparator;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
-public interface OurList<Type> {
+public interface OurList<Type> extends Iterable<Type> {
+
+
+    /**
+     * Adds the element to the 'this' instance
+     *
+     * @param element - to add
+     */
+    void addLast(Type element);
 
     /**
      * Adds the element to the 'this' instance
@@ -65,4 +76,75 @@ public interface OurList<Type> {
      * @return iterator object which iterates from the end to the beginning of the list
      */
     Iterator<Type> backwardIterator();
+
+    /**
+     * sorts the list according to the 'comparator' rule
+     *
+     * @param comparator the rule to sort the list
+     */
+    default void sort(Comparator<Type> comparator) {
+        Type[] copy = (Type[]) new Object[size()];
+
+        int i = 0;
+        for (Type elt : this) {
+            copy[i++] = elt;
+        }//copy: {15, -8, 3}
+
+        Arrays.sort(copy, comparator);
+
+        // copy:{-8, 3, 15}
+        this.clear();
+        for (Type elt : copy) {
+            this.addLast(elt);
+        }
+    }
+
+    default Type max(Comparator<Type> comparator) {
+        if (size() == 0)
+            throw new NoSuchElementException();
+
+//        Iterator<Type> iterator = iterator();
+//        Type max = iterator.next();
+//
+//        while (iterator.hasNext()) {
+//            Type currentElt = iterator.next();
+//            if (comparator.compare(currentElt, max) > 0)
+//                max = currentElt;
+//        }
+
+        Type max = this.get(0);
+
+        for (Type currentElt : this) {
+            if (comparator.compare(currentElt, max) > 0)
+                max = currentElt;
+        }
+
+        return max;
+    }
+
+    /**
+     * The method uses the natural ordering of the elements inside the list.
+     * Meaning the elements must be Comparable
+     *
+     * @return max according to the natural ordering
+     */
+    default Type max() {
+        if (size() == 0)
+            throw new NoSuchElementException();
+
+        Type max = this.get(0);
+
+        for (Type currentElt : this) {
+            Comparable<Type> compCurrentElement = (Comparable<Type>) currentElt;
+            if (compCurrentElement.compareTo(max) > 0)
+                max = currentElt;
+        }
+
+        return max;
+    }
+
+    default Type min(Comparator<Type> comparator) {
+        return max(comparator.reversed());
+    }
+
 }
