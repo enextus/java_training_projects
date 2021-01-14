8f1f286af559312042ec2718154d6b65d0aaf5da
enextus <enextus@gmail.com>
2021-01-14 06:53:33 +0100

    upd

---
+++ 21_01_13_find_section/src/Solution.java
@@ -0,0 +1,24 @@
+import java.util.List;
+
+public class Solution {
+
+    public int maxCoveredPoints(List<Integer> number, int cut) {
+        int points = 0;
+        int maxPoints = 0;
+
+        for (int i = 0; i < number.size(); i++) {
+            for (int j = 1 + 1; j < number.size(); j++) {
+                if (number.get(j) - number.get(i) <= cut) {
+                    points++;
+                }
+            }
+
+            if (points >= maxPoints)
+                maxPoints = points;
+
+            points = 0;
+        }
+
+        return maxPoints;
+    }
+}
