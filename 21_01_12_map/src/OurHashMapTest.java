c47021b3e0db7abcf24f82418bdc494c0d8a83ff
enextus <enextus@gmail.com>
2021-01-14 22:00:31 +0100

    upd

--- 21_01_12_map/src/OurHashMapTest.java
+++ 21_01_12_map/src/OurHashMapTest.java
@@ -1,24 +1,47 @@
+import org.junit.jupiter.api.Test;
+
+import java.util.Map;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class OurHashMapTest {
 
-    @org.junit.jupiter.api.Test
+    @Test
+    public void testAssertMap() {
+
+        OurHashMap<String, String> map = new OurHashMap<String, String>();
+
+        map.put("j", "java");
+        map.put("c", "c++");
+        map.put("p", "python");
+        map.put("n", "node");
+
+        OurHashMap<String, String> expected = new OurHashMap<String, String>();
+        expected.put("n", "node");
+        expected.put("c", "c++");
+        expected.put("j", "java");
+        expected.put("p", "python");
+
+        assertEquals(4, map.size());
+    }
+
+    @Test
     void size() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void put() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void get() {
     }
 
-    @org.junit.jupiter.api.Test
-    void remove() {
+    @Test
+    void test_remove_Pair_() {
     }
 
-    @org.junit.jupiter.api.Test
+    @Test
     void isEmpty() {
     }
 }
