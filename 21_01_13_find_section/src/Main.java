7f0f0fd1b8314cde895370dc86fe200bced55cae
enextus <enextus@gmail.com>
2021-01-14 08:46:17 +0100

    upd

--- 21_01_13_find_section/src/Main.java
+++ 21_01_13_find_section/src/Main.java
@@ -1,10 +1,7 @@
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class Main {
-    private static Object List;
-
     public static void main(String[] args) {
 
         Solution sol = new Solution();
@@ -14,8 +11,3 @@ public class Main {
         System.out.println("maxCoveredPoints: " + sol.maxCoveredPoints(integerList, 55));
     }
 }
-
-//        int[] integers = {0, 1, 6, 8, 12, 22, 34, 44, 55, 77, 79, 89, 92, 100};
-//        List<Integer> integerList = new ArrayList<Integer>(integers.length);
-//        for (int i : ints)
-//            integerList.add(i);
