2c3e8e26f88cd89704e312b8ef152c7c4997529e
enextus <enextus@gmail.com>
2021-01-15 10:00:52 +0100

    upd

--- 21_01_12_map/src/OurHashMapTest.java
+++ 21_01_12_map/src/OurHashMapTest.java
@@ -4,17 +4,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class OurHashMapTest {
 
+    OurHashMap<String, String> map = new OurHashMap<>();
+    OurHashMap<String, String> expected = new OurHashMap<String, String>();
+
     @Test
     public void testAssertMap() {
-
-        OurHashMap<String, String> map = new OurHashMap<String, String>();
 
         map.put("j", "java");
         map.put("c", "c++");
         map.put("p", "python");
         map.put("n", "node");
 
-        OurHashMap<String, String> expected = new OurHashMap<String, String>();
         expected.put("n", "node");
         expected.put("c", "c++");
         expected.put("j", "java");
@@ -46,54 +46,53 @@ class OurHashMapTest {
 
     @Test
     void test_isEmpty_true() {
-        OurHashMap<Integer> ourLinkedList = new OurHashMap<>();
+
         assertTrue(ourLinkedList.isEmpty());
     }
 
     @Test
     void test_sizeOfEmptyLinkedListIsZero_size() {
 
-        OurHashMap<Integer> ourLinkedList = new OurHashMap<>();
+
         assertEquals(0, ourLinkedList.size());
     }
 
     @Test
     void test_NullPointerException_after_add_null() {
 
-        OurHashMap<Integer> ourLinkedList = new OurHashMap<>();
+
         assertThrows(NullPointerException.class, () -> ourLinkedList.add(null));
     }
 
     @Test
     void test_sizeOfLinkedList_after_null_add_size_0() {
 
-        OurHashMap<Integer> ourLinkedList = new OurHashMap<>();
-        ourLinkedList.add(0);
+
+        ourLinkedList.put(0);
         assertEquals(1, ourLinkedList.size());
     }
 
     @Test
     void test_sizeOfLinkedList_after_one_add_size_1() {
 
-        OurHashMap<Integer> ourLinkedList = new OurHashMap<>();
-        ourLinkedList.add(333);
+
+        ourLinkedList.put(333);
         assertEquals(1, ourLinkedList.size());
     }
 
     @Test
     void test_sizeOfLinkedList_after_3_adds_size_3() {
 
-        OurHashMap<Integer> ourLinkedList = new OurHashMap<>();
-        ourLinkedList.add(1);
-        ourLinkedList.add(2);
-        ourLinkedList.add(3);
+
+        ourLinkedList.put(1);
+        ourLinkedList.put(2);
+        ourLinkedList.put(3);
         assertEquals(3, ourLinkedList.size());
     }
 
     @Test
     void test_sizeOfLinkedList_after_17_adds_size_17() {
 
-        OurHashMap<Integer> ourLinkedList = new OurHashMap<>();
 
         for (int i = 0; i < 17; i++)
             ourLinkedList.add(i);
@@ -105,50 +104,49 @@ class OurHashMapTest {
     @Test
     void test_remove_null_from_empty_list_exception() {
 
-        OurHashMap<Integer> ourLinkedList = new OurHashMap<>();
+
         assertThrows(IllegalStateException.class, () -> ourLinkedList.remove(null));
     }
 
     @Test
     void test_remove_1_from_empty_list_exception() {
 
-        OurHashMap<Integer> ourLinkedList = new OurHashMap<>();
         assertThrows(IllegalStateException.class, () -> ourLinkedList.remove(1));
     }
 
     @Test
     void test_remove_element_from_list_cont_1_element_true() {
 
-        OurHashMap<Integer> ourLinkedList = new OurHashMap<>();
-        ourLinkedList.add(1);
+
+        ourLinkedList.put(1);
         assertTrue(ourLinkedList.remove(1));
     }
 
     @Test
     void test_remove_element_from_list_a_not_cont_elem_exception() {
 
-        OurHashMap<Integer> ourLinkedList = new OurHashMap<>();
-        ourLinkedList.add(10);
+
+        ourLinkedList.put(10);
         assertThrows(NullPointerException.class, () -> ourLinkedList.remove(2));
     }
 
     @Test
     void test_remove_elements_true() {
 
-        OurHashMap<Integer> ourLinkedList = new OurHashMap<>();
-        for (int i = 0; i < 17; i++)
-            ourLinkedList.add(i);
 
         for (int i = 0; i < 17; i++)
-            assertTrue(ourLinkedList.remove(i));
+            map.put(i);
+
+        for (int i = 0; i < 17; i++)
+            assertTrue(map.remove(i));
     }
 
     @Test
     void test_SizeChange_results() {
-        OurHashMap<Integer> ourLinkedList = new OurHashMap<>();
+
 
         for (int i = 0; i < 5; i++)
-            ourLinkedList.add(i);
+            ourLinkedList.put(i);
 
         assertEquals(5, ourLinkedList.size());
         ourLinkedList.remove(0);
@@ -158,14 +156,13 @@ class OurHashMapTest {
     @Test
     void test_get_more_as_them_size_exception() {
 
-        OurHashMap<Integer> ourLinkedList = new OurHashMap<>();
+
         assertThrows(IllegalArgumentException.class, () -> ourLinkedList.get(2));
     }
 
     @Test
     void test_get_wrong_index_exception() {
 
-        OurHashMap<Integer> ourLinkedList = new OurHashMap<>();
 
         for (int i = 0; i < 5; i++)
             ourLinkedList.add(i);
@@ -177,28 +174,27 @@ class OurHashMapTest {
     @Test
     void test_get_after_remove_exception() {
 
-        OurHashMap<Integer> ourLinkedList = new OurHashMap<>();
 
         for (int i = 0; i < 5; i++)
-            ourLinkedList.add(i);
+            map.put(i);
 
-        assertEquals(4, ourLinkedList.get(4));
+        assertEquals(4, map.get(4));
 
-        assertEquals(0, ourLinkedList.get(0));
-        assertTrue(ourLinkedList.remove(0));
+        assertEquals(0, map.get(0));
+        assertTrue(map.remove(0));
 
-        assertEquals(1, ourLinkedList.get(0));
-        assertEquals(2, ourLinkedList.get(1));
-        assertEquals(3, ourLinkedList.get(2));
+        assertEquals(1, map.get(0));
+        assertEquals(2, map.get(1));
+        assertEquals(3, map.get(2));
 
-        assertTrue(ourLinkedList.remove(2));
+        assertTrue(map.remove(2));
 
-        assertEquals(1, ourLinkedList.get(0));
-        assertEquals(3, ourLinkedList.get(1));
-        assertEquals(4, ourLinkedList.get(2));
+        assertEquals(1, map.get(0));
+        assertEquals(3, map.get(1));
+        assertEquals(4, map.get(2));
 
-        assertThrows(IllegalArgumentException.class, () -> ourLinkedList.get(3));
-        assertThrows(IllegalArgumentException.class, () -> ourLinkedList.get(4));
+        assertThrows(IllegalArgumentException.class, () -> map.get(3));
+        assertThrows(IllegalArgumentException.class, () -> map.get(4));
 
     }
 
