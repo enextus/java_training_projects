f735b38e0f31ce4369dceb2e0115ed256100674e
enextus <enextus@gmail.com>
2021-01-17 21:37:38 +0100

    upd

--- 21_01_17_how_points_are_in_alpha_angle/src/App.java
+++ 21_01_17_how_points_are_in_alpha_angle/src/App.java
@@ -10,20 +10,20 @@ public class App {
         Point point0 = new Point(2.1, 8.2);
         Point point1 = new Point(-4.2, -16.4);
         Point point2 = new Point(3.0, 3.3);
-        Point point3 = new Point(3.2, 4.4);
+        Point point3 = new Point(7.2, 7.4);
         Point point4 = new Point(4.9, 5.0);
         Point point5 = new Point(5.0, 6.5);
         Point point6 = new Point(6.3, 7.0);
         Point point7 = new Point(6.4, 8.2);
         Point point8 = new Point(-7.5, -2.0);
-        Point point9 = new Point(8.5, 3.3);
-        Point point10 = new Point(8.5, 6.3);
-        Point point11 = new Point(9.0, 8.9);
+        Point point9 = new Point(8.5, 33.3);
+        Point point10 = new Point(6.5, 6.3);
+        Point point11 = new Point(0.5, 0.9);
         Point point12 = new Point(3.3, 6.0);
-        Point point13 = new Point(6.4, 2.2);
+        Point point13 = new Point(26.4, 2.2);
         Point point14 = new Point(5.5, 2.0);
         Point point15 = new Point(8.3, 5.3);
-        Point point16 = new Point(8.2, 1.3);
+        Point point16 = new Point(3.2, 1.3);
         Point point17 = new Point(-9.4, -8.9);
 
         List<Point> pointsList = Arrays.asList(point0, point1, point2, point3,
@@ -35,6 +35,6 @@ public class App {
         for (Point pnt : pointsList)
             System.out.println(pnt);
 
-        System.out.println("\nmaxTrees: " + sol.maxTrees(pointsList, 15));
+        System.out.println("\nmaxTrees: " + sol.maxTrees(pointsList, 7));
     }
 }
