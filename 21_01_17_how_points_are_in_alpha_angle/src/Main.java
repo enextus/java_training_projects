17f8d0e5ea2426663f6a85e231bf761829b14803
enextus <enextus@gmail.com>
2021-01-17 12:12:21 +0100

    upd

--- 21_01_17_how_points_are_in_alpha_angle/src/Main.java
+++ 21_01_17_how_points_are_in_alpha_angle/src/Main.java
@@ -3,9 +3,9 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        List<Integer> integerList = Arrays.asList(0, 1, 4, 5, 13, 22, 31, 34, 44, 55, 100);
+        List<Integer> integerList = Arrays.asList(0, 1, 4, 5, 13, 22, 31, 34, 44, 55, 100, 190, 210, 245, 270, 360);
         Solution sol = new Solution();
 
-        System.out.println("points: " + sol.coveredPoints(integerList, 0, 15));
+        System.out.println("points: " + sol.coveredPoints(integerList, 100, 180));
     }
 }
