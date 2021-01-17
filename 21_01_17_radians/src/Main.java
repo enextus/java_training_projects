8e6479d9497ad47d4d4ada22df1975116e0b6513
enextus <enextus@gmail.com>
2021-01-17 22:18:35 +0100

    upd

---
+++ 21_01_17_radians/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+/*    public static void main(String[] args) {
+        List<Integer> integerList = Arrays.asList(0, 1, 4, 22, 31, 34, 44, 55, 100, 104, 105, 106, 107, 108, 109, 110, 190, 210, 245, 270, 360);
+        misc.misc.Solution sol = new misc.misc.Solution();
+
+        System.out.println("points: " + sol.coveredPoints(integerList, 0, 40));
+    }*/
+}
---
+++ 21_01_17_radians/src/Test.java
@@ -0,0 +1,29 @@
+
+import java.text.NumberFormat;
+
+public class Test {
+
+
+  /*  int x = 180; //градусы
+    double radian = (Math.PI * x) / 180; //перевод градусов в радианы
+    double rez = Math.sin(radian); //входное значение в радианах
+    NumberFormat nf = NumberFormat.getInstance();
+        nf.setMaximumFractionDigits(4);
+        System.out.println("sin(" + x + ")градусов=" + nf.format(rez));
+    //Переводим радианы в градусы, минуты, секунды (1 градус 60 минут или 360 секунд)
+    double rad = 3.1416;
+    double grad = (rad * 180) / Math.PI; //перевод из радиан в градусы
+    int g = (int) grad;
+    double min = (grad * 60) % 60;
+    int m = (int) min;
+    int sec = (int) (min * 60) % 60;
+        System.out.println("радиан(" + rad + ")=" + g + "(градусов)" + "=" + m + "(минут)" + "=" + sec + "(секунд)");
+    //Переводим градусы, минуты, секунды в радианы
+    int sec1 = 1;
+    int min1 = 0;
+    int grad1 = 180;
+    int g1 = sec1 / 60 / 60 + min1 / 60 + grad1;
+    double rad1 = (g1 * Math.PI) / 180;
+        System.out.println(grad1 + "(градусов)" + "=" + min1 + "(минут)" + "=" + sec1 + "(секунд)" + "=" + nf.format(rad1) + "(радиан)");
+*/
+}
