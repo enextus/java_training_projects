da42ba5fe14614e2f1162f7de66e3957f235dfa6
enextus <enextus@gmail.com>
2021-01-17 19:54:22 +0100

    upd

--- 21_01_17_how_points_are_in_alpha_angle/src/App.java
+++ 21_01_17_how_points_are_in_alpha_angle/src/App.java
@@ -7,22 +7,22 @@ public class App {
         List<Integer> integerList = Arrays.asList(0, 1, 3, 4, 11, 22, 24, 25, 26, 249, 270, 271, 272, 275, 276, 277, 360);
         SolutionDeque sol = new SolutionDeque();
 
-        Point point1 = new Point(2, 8);
-        Point point2 = new Point(3, 3);
-        Point point3 = new Point(3, 4);
-        Point point4 = new Point(4, 5);
-        Point point5 = new Point(5, 6);
-        Point point6 = new Point(6, 7);
-        Point point7 = new Point(6, 8);
-        Point point8 = new Point(7, 2);
-        Point point9 = new Point(8, 3);
-        Point point10 = new Point(8, 6);
-        Point point11 = new Point(9, 8);
+        Point point1 = new Point(2.0, 8.0);
+        Point point2 = new Point(3.0, 3.0);
+        Point point3 = new Point(3.0, 4.0);
+        Point point4 = new Point(4.0, 5.0);
+        Point point5 = new Point(5.0, 6.0);
+        Point point6 = new Point(6.0, 7.0);
+        Point point7 = new Point(6.0, 8.0);
+        Point point8 = new Point(7.0, 2.0);
+        Point point9 = new Point(8.0, 3.0);
+        Point point10 = new Point(8.0, 6.0);
+        Point point11 = new Point(9.0, 8.0);
 
         List<Point> pointsList = Arrays.asList(point1, point2, point3, point4, point5, point6, point7, point8, point9, point10, point11);
 
-        for (Point pnt:pointsList
-             ) {
+        for (Point pnt : pointsList
+        ) {
             System.out.println(pnt);
         }
 
@@ -33,7 +33,7 @@ public class App {
         double b = 2.0;
         double c = 0.5;
         double d = Math.PI;
-        double e = Math.PI/4;
+        double e = Math.PI / 4;
 
         System.out.format("%.1f радиан = %.3f градусов %n", a, Math.toDegrees(a));
         System.out.format("%.1f радиан = %.3f градусов %n", b, Math.toDegrees(b));
@@ -44,8 +44,8 @@ public class App {
         //System.out.println("Points: " + sol.coveredPoints(pointsList, 1));
 
 
-        int x = 180 ; //градусы
-        double radian = (Math.PI*x)/180; //перевод градусов в радианы
+        int x = 180; //градусы
+        double radian = (Math.PI * x) / 180; //перевод градусов в радианы
         double rez = Math.sin(radian); //входное значение в радианах
         NumberFormat nf = NumberFormat.getInstance();
         nf.setMaximumFractionDigits(4);
@@ -62,9 +62,33 @@ public class App {
         int sec1 = 1;
         int min1 = 0;
         int grad1 = 180;
-        int g1 = sec1/60/60 +min1/60 +grad1;
-        double rad1 = (g1*Math.PI)/180 ;
-        System.out.println(grad1 + "(градусов)" + "=" + min1 + "(минут)" + "=" + sec1 + "(секунд)"+"=" + nf.format(rad1)+ "(радиан)");
+        int g1 = sec1 / 60 / 60 + min1 / 60 + grad1;
+        double rad1 = (g1 * Math.PI) / 180;
+        System.out.println(grad1 + "(градусов)" + "=" + min1 + "(минут)" + "=" + sec1 + "(секунд)" + "=" + nf.format(rad1) + "(радиан)");
+
+
+        Point poi_10_10 = new Point(10, 10);
+        System.out.println(poi_10_10.getX());
+        System.out.println(poi_10_10.getY());
+
+        double hypotenuse1 = Math.sqrt((Math.pow(poi_10_10.getY(), 2)) + (Math.pow(poi_10_10.getX(), 2)));
+        double sinx1 = poi_10_10.getX() / (Math.sqrt((Math.pow(poi_10_10.getY(), 2)) + (Math.pow(poi_10_10.getX(), 2))));
+
+        System.out.println("sinx1: " + sinx1);
+        System.out.println("Angle_in_Radian: " + Math.asin(sinx1));
+        System.out.println("Angle_in_Grad: " + Math.toDegrees(Math.asin(sinx1)));
+
+        Point poi_1_1 = new Point(1, 1);
+        System.out.println(poi_1_1.getX());
+        System.out.println(poi_1_1.getY());
+
+        double hypotenuse2 = Math.sqrt((Math.pow(poi_1_1.getY(), 2)) + (Math.pow(poi_1_1.getX(), 2)));
+        double sinx2 = poi_1_1.getX() / (Math.sqrt((Math.pow(poi_1_1.getY(), 2)) + (Math.pow(poi_1_1.getX(), 2))));
+
+        System.out.println("sinx2: " + sinx2);
+        System.out.println("Angle_in_Radian: " + Math.asin(sinx2));
+        System.out.println("Angle_in_Grad: " + Math.toDegrees(Math.asin(sinx2)));
 
     }
+
 }
--- 21_01_17_how_points_are_in_alpha_angle/src/Point.java
+++ 21_01_17_how_points_are_in_alpha_angle/src/Point.java
@@ -1,12 +1,17 @@
 import java.util.Objects;
 
 public class Point {
-    int x;
-    int y;
+    double x;
+    double y;
 
-    public Point(int x, int y) {
+    public Point(double x, double y) {
         this.x = x;
         this.y = y;
+    }
+
+
+    public double getX() {
+        return x;
     }
 
     @Override
@@ -14,7 +19,16 @@ public class Point {
         if (this == o) return true;
         if (!(o instanceof Point)) return false;
         Point point = (Point) o;
-        return x == point.x && y == point.y;
+        return Double.compare(point.getX(), getX()) == 0 && Double.compare(point.getY(), getY()) == 0;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getX(), getY());
+    }
+
+    public double getY() {
+        return y;
     }
 
     @Override
@@ -23,18 +37,5 @@ public class Point {
                 "x=" + x +
                 ", y=" + y +
                 '}';
-    }
-
-    public int getX() {
-        return x;
-    }
-
-    public int getY() {
-        return y;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(x, y);
     }
 }
