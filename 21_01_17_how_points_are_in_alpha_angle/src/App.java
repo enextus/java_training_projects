b8550cb28d2cf1eed8b09d86145470a58d23250e
enextus <enextus@gmail.com>
2021-01-17 16:04:54 +0100

    upd

--- 21_01_17_how_points_are_in_alpha_angle/src/App.java
+++ 21_01_17_how_points_are_in_alpha_angle/src/App.java
@@ -3,9 +3,9 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) {
-        List<Integer> integerList = Arrays.asList(0, 1, 4, 22, 31, 34, 44, 55, 100, 104, 105, 106, 107, 108, 109, 110, 190, 210, 245, 270, 360);
+        List<Integer> integerList = Arrays.asList(0, 1, 2, 4, 11, 22, 24, 25, 26, 31, 34, 44, 55, 100, 104, 108, 109, 110, 190, 210, 248, 249, 270, 360);
         SolutionDeque sol = new SolutionDeque();
 
-        System.out.println("Points: " + sol.coveredPoints(integerList, 0, 40));
+        System.out.println("Points: " + sol.coveredPoints(integerList, 0, 20));
     }
 }
--- 21_01_17_how_points_are_in_alpha_angle/src/SolutionDeque.java
+++ 21_01_17_how_points_are_in_alpha_angle/src/SolutionDeque.java
@@ -1,5 +1,4 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -8,32 +7,44 @@ public class SolutionDeque {
 
     public int coveredPoints(List<Integer> integerList, int leftCorner, int rightCorner) {
 
-        int points = 0;
         int maxPoints = 0;
         int cut = rightCorner - leftCorner;
 
         // get(i) -> ArrayList O(1)
         ArrayList<Integer> numbers = new ArrayList<>(integerList);
 
+        ArrayDeque<Integer> maximusDeque = new ArrayDeque<Integer>();
+
         for (int i = 0; i < numbers.size(); i++) {
             for (int j = i; j < numbers.size(); j++) {
-
 
 /*                System.out.println("(i): " + numbers.get(i) + ", (j): " + numbers.get(j));
                 System.out.println("(j) - (i): " + (numbers.get(j) - numbers.get(i)) + ", cut = " + cut);
                 System.out.println();*/
 
+                if (numbers.get(j) - numbers.get(i) <= cut) {
 
-                if (numbers.get(j) - numbers.get(i) <= cut)
-                    points++;
+                    if (maximusDeque.isEmpty()) {
+                        maximusDeque.addLast(1);
+                    } else {
+                        int val = maximusDeque.getLast() + 1;
+
+                        System.out.println("val " + val);
+                        maximusDeque.addLast(val);
+                        System.out.println("maximusDeque.size(): " + maximusDeque.size());
+                    }
+
+                }
             }
 
-            if (points >= maxPoints) {
-                maxPoints = points;
-                System.out.println("Points: " + points);
+
+
+            if (maximusDeque.size() >= maxPoints) {
+                maxPoints = maximusDeque.size();
+                System.out.println("maximusDeque.size(): " + maximusDeque.size());
             }
 
-            points = 0;
+            maximusDeque.clear();
         }
 
         return maxPoints;
