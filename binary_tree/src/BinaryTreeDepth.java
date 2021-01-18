946334eace36d5467e547ded9e16490f6ac5e4a5
enextus <enextus@gmail.com>
2021-01-18 19:50:44 +0100

    upd

--- binary_tree/src/BinaryTreeDepth.java
+++ binary_tree/src/BinaryTreeDepth.java
@@ -1,18 +1,15 @@
 public class BinaryTreeDepth {
 
     int heightTree(Node root) {
-        if (root == null) {
+        if (root == null)
             return -1;
-        } else {
 
-            int left = heightTree(root.left);
-            int right = heightTree(root.right);
+        int left = heightTree(root.left);
+        int right = heightTree(root.right);
 
-            if (left > right)
-                return 1 + left;
-            else
-                return 1 + right;
-
-        }
+        if (left > right)
+            return 1 + left;
+        else
+            return 1 + right;
     }
 }
