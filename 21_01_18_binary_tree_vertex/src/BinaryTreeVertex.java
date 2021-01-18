e0387fb6132ccf8ff7293764520df66cb11dc9c5
enextus <enextus@gmail.com>
2021-01-18 19:34:09 +0100

    upd

---
+++ 21_01_18_binary_tree_vertex/src/BinaryTreeVertex.java
@@ -0,0 +1,18 @@
+public class BinaryTreeVertex {
+
+    int heightTree(Node root) {
+        if (root == null) {
+            return -1;
+        } else {
+
+            int left = heightTree(root.left);
+            int right = heightTree(root.right);
+
+            if (left > right)
+                return 1 + left;
+            else
+                return 1 + right;
+
+        }
+    }
+}
---
+++ 21_01_18_binary_tree_vertex/src/Node.java
@@ -0,0 +1,9 @@
+public class Node {
+    Node left;
+    Node right;
+
+    public Node(Node left, Node right) {
+        this.left = left;
+        this.right = right;
+    }
+}
