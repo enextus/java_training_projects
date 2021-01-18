a2602034846a3b99d5c905c08b35855021edd3c1
enextus <enextus@gmail.com>
2021-01-18 22:59:49 +0100

    upd

--- 21_01_18_binary_tree_depth/src/App.java
+++ 21_01_18_binary_tree_depth/src/App.java
@@ -13,6 +13,18 @@ public class App {
 
         BinaryTreeDepth binaryTreeDepth = new BinaryTreeDepth();
 
+        System.out.println("node_4.left: " + node_4.left + ", node_4.right: " + node_4.right);
+
+        System.out.println("node_2.left: " + node_2.left + ", node_2.right: " + node_2.right);
+        System.out.println("node_1.left: " + node_1.left + ", node_1.right: " + node_1.right);
+        System.out.println("node_3.left: " + node_3.left + ", node_3.right: " + node_3.right);
+
+        System.out.println("node_5.left: " + node_5.left + ", node_5.right: " + node_5.right);
+        System.out.println("node_6.left: " + node_6.left + ", node_6.right: " + node_6.right);
+        System.out.println("node_7.left: " + node_7.left + ", node_7.right: " + node_7.right);
+        System.out.println("node_8.left: " + node_8.left + ", node_8.right: " + node_8.right);
+        System.out.println("node_9.left: " + node_9.left + ", node_9.right: " + node_9.right);
+
         System.out.println(binaryTreeDepth.heightTree(node_4));
     }
 }
