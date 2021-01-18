a83441e16be92b99b9cfca80b641f8b308c7c19f
enextus <enextus@gmail.com>
2021-01-18 12:56:08 +0100

    upd

--- binary_tree/src/BinaryTreeTest.java
+++ binary_tree/src/BinaryTreeTest.java
@@ -4,7 +4,22 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BinaryTreeTest {
 
+    Node node_1 = new Node(null, null);
+    Node node_3 = new Node(null, null);
+    Node node_7 = new Node(null, null);
+    Node node_9 = new Node(null, null);
+    Node node_8 = new Node(node_9, null);
+    Node node_6 = new Node(node_7, node_8);
+    Node node_5 = new Node(null, node_6);
+    Node node_2 = new Node(node_1, node_3);
+    Node node_4 = new Node(node_2, node_5);
+
+    BinaryTree binaryTree = new BinaryTree();
+
     @Test
-    void height() {
+    void height_Test_zero() {
+        assertEquals(0, binaryTree.height(node_1));
     }
+
+
 }
