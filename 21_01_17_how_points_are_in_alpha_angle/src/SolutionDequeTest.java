663bb324253357e1ea0f946208800c35945484cc
enextus <enextus@gmail.com>
2021-01-18 11:11:15 +0100

    upd

--- 21_01_17_how_points_are_in_alpha_angle/src/SolutionDequeTest.java
+++ 21_01_17_how_points_are_in_alpha_angle/src/SolutionDequeTest.java
@@ -2,11 +2,51 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SolutionDequeTest {
 
+
+    SolutionDeque sol = new SolutionDeque();
+
+    List<Point> pointsList = Arrays.asList(
+            new Point(-2.1, 8.2),
+            new Point(-4.2, -16.4),
+            new Point(3.0, 3.3),
+            new Point(7.2, 7.4),
+            new Point(4.9, 5.0),
+            new Point(5.0, 6.5),
+            new Point(6.3, 7.0),
+            new Point(6.4, 8.2),
+            new Point(-7.5, -2.0),
+            new Point(8.5, 33.3),
+            new Point(6.5, 6.3),
+            new Point(0.5, 0.9),
+            new Point(-3.3, -6.0),
+            new Point(26.4, 2.2),
+            new Point(5.5, 2.0),
+            new Point(8.3, 5.3),
+            new Point(3.2, 1.3),
+            new Point(-9.4, -8.9),
+            new Point(-5.5, -2.0),
+            new Point(8.9, 9.9),
+            new Point(2.2, -1.3),
+            new Point(-9.4, -8.9)
+    );
+
     @Test
     void maxTrees() {
+        Collections.sort(pointsList);
+
+        for (Point pnt : pointsList)
+            System.out.println(pnt);
+
+        System.out.println("\nmaxTrees: " + sol.maxTrees(pointsList, 15));
     }
+
+
 }
