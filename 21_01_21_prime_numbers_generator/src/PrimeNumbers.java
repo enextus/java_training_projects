25c46bb47be70fc7e537ffd343bef587a103326d
enextus <enextus@gmail.com>
2021-01-21 19:36:23 +0100

    some upd

--- 21_01_21_prime_numbers_generator/src/PrimeNumbers.java
+++ 21_01_21_prime_numbers_generator/src/PrimeNumbers.java
@@ -31,7 +31,7 @@ public class PrimeNumbers {
         if (number % 2 == 0)
             return number == 2;
 
-        int limit = (int) (0.1 + Math.sqrt(number));
+        int limit = (int) ((1 / 10) + Math.sqrt(number));
 
         for (int i = 3; i <= limit; i += 2) {
             if (number % i == 0)
