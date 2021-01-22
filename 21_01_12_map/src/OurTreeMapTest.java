a1ffd85eade0f4150c2b69e6991d8dec8a04b0de
enextus <enextus@gmail.com>
2021-01-22 09:44:10 +0100

    upd

---
+++ 21_01_12_map/src/OurTreeMapTest.java
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.BeforeEach;
+
+class OurTreeMapTest extends OurMapTest {
+
+    @BeforeEach
+    public void init() {
+        map = new OurTreeMap<>();
+        intMap = new OurTreeMap<>();
+    }
+}
