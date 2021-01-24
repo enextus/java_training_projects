886ed5e49c64c918794037f246e08bed524fddb9
enextus <enextus@gmail.com>
2021-01-24 19:01:46 +0100

    some upd

--- 21_01_21_prime_numbers_generator/src/Main.java
+++ 21_01_21_prime_numbers_generator/src/Main.java
@@ -15,5 +15,16 @@ public class Main {
         for (Integer number : primeNumbers.getPrimes(upperBound))
             System.out.println(number);
 
+        int upperBound2 = 2;
+        System.out.println("\nList of prime numbers between 2 and " + upperBound2 + ":");
+
+        for (Integer number : primeNumbers.getPrimes(upperBound2))
+            System.out.println(number);
+
+        int upperBound3 = 3;
+        System.out.println("\nList of prime numbers between 2 and " + upperBound3 + ":");
+
+        for (Integer number : primeNumbers.getPrimes(upperBound3))
+            System.out.println(number);
     }
 }
