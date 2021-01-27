a1477aae77a3319d3d15f806d8018b4adb2dbbdb
enextus <enextus@gmail.com>
2021-01-27 21:13:36 +0100

    upd

--- OurSet/src/OurHashSet.java
+++ OurSet/src/OurHashSet.java
@@ -17,7 +17,7 @@ public class OurHashSet<T> implements OurSet<T> {
      * Constructs a new, empty set; the backing <tt>HashMap</tt> instance has
      * default initial capacity (16) and load factor (0.75).
      */
-    public HashSet() {
+    public OurHashSet() {
         map = new OurHashMap<>();
     }
 
@@ -44,8 +44,8 @@ public class OurHashSet<T> implements OurSet<T> {
      * @throws IllegalArgumentException if the initial capacity is less
      *                                  than zero, or if the load factor is nonpositive
      */
-    public HashSet(int initialCapacity, float loadFactor) {
-        map = new HashMap<>(initialCapacity, loadFactor);
+    public OurHashSet(int initialCapacity, float loadFactor) {
+        map = new OurHashMap<>(initialCapacity, loadFactor);
     }
 
     /**
@@ -56,8 +56,8 @@ public class OurHashSet<T> implements OurSet<T> {
      * @throws IllegalArgumentException if the initial capacity is less
      *                                  than zero
      */
-    public HashSet(int initialCapacity) {
-        map = new HashMap<>(initialCapacity);
+    public OurHashSet(int initialCapacity) {
+        map = new OurHashMap<>(initialCapacity);
     }
 
     /**
@@ -73,7 +73,7 @@ public class OurHashSet<T> implements OurSet<T> {
      * @throws IllegalArgumentException if the initial capacity is less
      *                                  than zero, or if the load factor is nonpositive
      */
-    HashSet(int initialCapacity, float loadFactor, boolean dummy) {
+    OurHashSet(int initialCapacity, float loadFactor, boolean dummy) {
         map = new LinkedHashMap<>(initialCapacity, loadFactor);
     }
 
@@ -97,14 +97,6 @@ public class OurHashSet<T> implements OurSet<T> {
         return map.size();
     }
 
-    /**
-     * Returns <tt>true</tt> if this set contains no elements.
-     *
-     * @return <tt>true</tt> if this set contains no elements
-     */
-    public boolean isEmpty() {
-        return map.isEmpty();
-    }
 
     /**
      * Returns <tt>true</tt> if this set contains the specified element.
@@ -148,107 +140,7 @@ public class OurHashSet<T> implements OurSet<T> {
      * @return <tt>true</tt> if the set contained the specified element
      */
     public boolean remove(Object o) {
-        return map.remove(o) == PRESENT;
-    }
-
-    /**
-     * Removes all of the elements from this set.
-     * The set will be empty after this call returns.
-     */
-    public void clear() {
-        map.clear();
-    }
-
-    /**
-     * Returns a shallow copy of this <tt>HashSet</tt> instance: the elements
-     * themselves are not cloned.
-     *
-     * @return a shallow copy of this set
-     */
-    @SuppressWarnings("unchecked")
-    public Object clone() {
-        try {
-            HashSet<T> newSet = (HashSet<T>) super.clone();
-            newSet.map = (HashMap<T, Object>) map.clone();
-            return newSet;
-        } catch (CloneNotSupportedException e) {
-            throw new InternalError(e);
-        }
-    }
-
-    /**
-     * Save the state of this <tt>HashSet</tt> instance to a stream (that is,
-     * serialize it).
-     *
-     * @serialData The capacity of the backing <tt>HashMap</tt> instance
-     * (int), and its load factor (float) are emitted, followed by
-     * the size of the set (the number of elements it contains)
-     * (int), followed by all of its elements (each an Object) in
-     * no particular order.
-     */
-    private void writeObject(java.io.ObjectOutputStream s)
-            throws java.io.IOException {
-        // Write out any hidden serialization magic
-        s.defaultWriteObject();
-
-        // Write out HashMap capacity and load factor
-        s.writeInt(map.capacity());
-        s.writeFloat(map.loadFactor());
-
-        // Write out size
-        s.writeInt(map.size());
-
-        // Write out all elements in the proper order.
-        for (T e : map.keySet())
-            s.writeObject(e);
-    }
-
-    /**
-     * Reconstitute the <tt>HashSet</tt> instance from a stream (that is,
-     * deserialize it).
-     */
-    private void readObject(java.io.ObjectInputStream s)
-            throws java.io.IOException, ClassNotFoundException {
-        // Read in any hidden serialization magic
-        s.defaultReadObject();
-
-        // Read capacity and verify non-negative.
-        int capacity = s.readInt();
-        if (capacity < 0) {
-            throw new InvalidObjectException("Illegal capacity: " +
-                    capacity);
-        }
-
-        // Read load factor and verify positive and non NaN.
-        float loadFactor = s.readFloat();
-        if (loadFactor <= 0 || Float.isNaN(loadFactor)) {
-            throw new InvalidObjectException("Illegal load factor: " +
-                    loadFactor);
-        }
-
-        // Read size and verify non-negative.
-        int size = s.readInt();
-        if (size < 0) {
-            throw new InvalidObjectException("Illegal size: " +
-                    size);
-        }
-
-        // Set the capacity according to the size and load factor ensuring that
-        // the HashMap is at least 25% full but clamping to maximum capacity.
-        capacity = (int) Math.min(size * Math.min(1 / loadFactor, 4.0f),
-                HashMap.MAXIMUM_CAPACITY);
-
-        // Create backing HashMap
-        map = (((HashSet<?>) this) instanceof LinkedHashSet ?
-                new LinkedHashMap<T, Object>(capacity, loadFactor) :
-                new HashMap<T, Object>(capacity, loadFactor));
-
-        // Read in all elements in the proper order.
-        for (int i = 0; i < size; i++) {
-            @SuppressWarnings("unchecked")
-            T e = (T) s.readObject();
-            map.put(e, PRESENT);
-        }
+        return map.remove((T) o) == PRESENT;
     }
 
 }
---
+++ OurSet/src/OurSet.java
@@ -0,0 +1,16 @@
+public interface OurSet<T> extends Iterable<T> {
+
+    boolean add(T elt);
+
+    boolean remove(T elt);
+
+    boolean contains(T elt);
+
+    int size();
+
+    void addAll(OurSet<T> another);
+
+    void retainAll(OurSet<T> another);
+
+    void removeAll(OurSet<T> another);
+}
