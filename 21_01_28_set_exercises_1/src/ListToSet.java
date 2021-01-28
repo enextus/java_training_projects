c447535b9877c4d1f57287a0d03ec8aa48694f06
enextus <enextus@gmail.com>
2021-01-28 12:35:44 +0100

    upd

--- 21_01_28_set_exercises_1/src/ListToSet.java
+++ 21_01_28_set_exercises_1/src/ListToSet.java
@@ -1,3 +1,4 @@
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
@@ -5,6 +6,13 @@ public class ListToSet {
 
     public Set<Integer> listToSet(List<Integer> list) {
 
-        return null;
+        Set<Integer> set = new HashSet<>();
+
+        for (int v : list) {
+            set.add(v);
+        }
+
+        return set;
     }
+
 }
---
+++ 21_01_28_set_exercises_1/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        List<Integer> arrayList = Arrays.asList(1, 1, 2, 1);
+
+        System.out.println("Our List: " + arrayList);
+
+        ListToSet listToSet = new ListToSet();
+
+        System.out.println(listToSet.listToSet(arrayList));
+
+    }
+}
