00a97f105c69dea9a937c89ec9ceaa3b1f7c4daf
enextus <enextus@gmail.com>
2021-01-28 12:40:15 +0100

    upd

--- 21_01_28_set_exercises_1/src/ListToSetTest.java
+++ 21_01_28_set_exercises_1/src/ListToSetTest.java
@@ -1,8 +1,12 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+import java.util.List;
+
 class ListToSetTest {
 
     ListToSet listToSet = new ListToSet();
+    List<Integer> arrayList = Arrays.asList(1, 1, 2, 1);
 
     @Test
     void test_listToSet_size() {
