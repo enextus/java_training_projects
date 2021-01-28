2c72b758431344c71e8fe80dad9502fe7d5b4e06
enextus <enextus@gmail.com>
2021-01-28 23:42:29 +0100

    upd

--- 21_01_28_map_with_keys_of_make/src/Main.java
+++ 21_01_28_map_with_keys_of_make/src/Main.java
@@ -1,9 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        Auto testAuto = new Auto("Opel", "Green");
+        Auto testAuto1 = new Auto("Opel", "Green");
+        Auto testAuto2 = new Auto("BMW", "White");
+        Auto testAuto3 = new Auto("Audi", "Red");
 
-        System.out.println("Test-Auto: " + testAuto);
+        System.out.println("Test-Auto: " + testAuto1);
+        System.out.println("Test-Auto: " + testAuto2);
+        System.out.println("Test-Auto: " + testAuto3);
     }
 
 }
