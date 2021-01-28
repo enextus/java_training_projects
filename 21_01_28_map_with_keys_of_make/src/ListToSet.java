ca3f9a4b8cd282acdf5bb79462762f19f83990fe
enextus <enextus@gmail.com>
2021-01-28 23:52:37 +0100

    upd

---
+++ 21_01_28_map_with_keys_of_make/src/ListToSet.java
@@ -0,0 +1,17 @@
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class ListToSet {
+
+    public Set<Integer> addToSet(List<Integer> list) {
+
+        Set<Integer> intSet = new HashSet<>();
+
+        // for (int v : list) set.add(v);
+        intSet.addAll(list);
+
+        return intSet;
+    }
+
+}
