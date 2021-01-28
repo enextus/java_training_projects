2da80e0e360b35905fcd33f6a6b826de40bac1b9
enextus <enextus@gmail.com>
2021-01-28 23:27:50 +0100

    upd

---
+++ 21_01_28_map_with_keys_of_make/src/Auto.java
@@ -0,0 +1,25 @@
+import java.util.Objects;
+
+public class Auto {
+    String make;
+    String color;
+
+    public Auto(String make, String color) {
+        this.make = make;
+        this.color = color;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Auto)) return false;
+        Auto auto = (Auto) o;
+        return make.equals(auto.make) && color.equals(auto.color);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(make, color);
+    }
+}
