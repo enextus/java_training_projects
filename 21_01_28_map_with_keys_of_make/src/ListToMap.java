e333c129c2422b8a47e9950b9f069f8a65df83c3
enextus <enextus@gmail.com>
2021-01-29 00:21:01 +0100

    upd

--- 21_01_28_map_with_keys_of_make/src/ListToMap.java
+++ 21_01_28_map_with_keys_of_make/src/ListToMap.java
@@ -2,7 +2,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class ListToSet {
+public class ListToMap {
 
     public Set<Integer> addToSet(List<Integer> list) {
 
