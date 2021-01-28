4bb301a761f9f064cedab76cc7f5fcfd0289265b
enextus <enextus@gmail.com>
2021-01-28 13:32:16 +0100

    upd

--- 21_01_28_set_exercises_1/src/ListToSet.java
+++ 21_01_28_set_exercises_1/src/ListToSet.java
@@ -3,7 +3,7 @@ import java.util.List;
 import java.util.Set;
 
 public class ListToSet {
-
+    
     public Set<Integer> listToSet(List<Integer> list) {
 
         Set<Integer> set = new HashSet<>();
