891c867b9be7400e4d735bd4a2a77dbeae62adcb
enextus <enextus@gmail.com>
2021-01-28 01:01:01 +0100

    upd

--- OurSet/src/OurHashSetTest.java
+++ OurSet/src/OurHashSetTest.java
@@ -1,13 +1,14 @@
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.util.Date;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class OurHashSetTest {
 
     @Test
-    void hashSetsizeTestAfterAddElement() {
+    void hashSet_Test_Size_After_Add_One_Element() {
         OurSet<Date> hashSet = new OurHashSet<>();
 
         Date date = new Date();
@@ -17,7 +18,7 @@ class OurHashSetTest {
     }
 
     @Test
-    void hashSetsizeTestAfterAddSameElement() {
+    void hashSet_Test_Size_After_Add_Several_Elements() {
         OurSet<Date> hashSet = new OurHashSet<>();
 
         Date date = new Date();
@@ -30,7 +31,7 @@ class OurHashSetTest {
     }
 
     @Test
-    void hashSetCanFailToFindTheSameElementPreviouslyAdded() {
+    void hashSet_Test_Can_Fail_To_Find_TheSame_Element_Previously_Added() {
         OurSet<Date> hashSet = new OurHashSet<>();
 
         Date date = new Date();
@@ -40,4 +41,77 @@ class OurHashSetTest {
         assertFalse(hashSet.contains(date));
     }
 
+    private static final int LIST_SIZE = 100;
+
+    public static class RandomUtils {
+
+        private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyz1234567890";
+
+        public static final int MIN_STRING_LENGTH = 10;
+        public static final int MAX_STRING_LENGTH = 20;
+
+        public static String createString(int length) {
+
+            Random rnd = new Random();
+            StringBuilder sb = new StringBuilder(length);
+
+            for (int i = 0; i < length; ++i)
+                sb.append(ALPHABET.charAt(rnd.nextInt(ALPHABET.length())));
+
+            return sb.toString();
+        }
+
+        public static List<String> createStringList(int size) {
+
+            Random rnd = new Random();
+            List<String> result = new ArrayList<>(size);
+
+            for (int i = 0; i < size; ++i)
+                result.add(createString(MIN_STRING_LENGTH + rnd.nextInt(MAX_STRING_LENGTH - MIN_STRING_LENGTH + 1)));
+
+            return result;
+        }
+    }
+
+    @Test
+    public void testWithAdd() {
+        List<String> order1 = RandomUtils.createStringList(LIST_SIZE);
+        List<String> order2 = new ArrayList<>(order1);
+
+        Collections.shuffle(order2);
+
+        Set<String> set1 = new HashSet<>();
+        Set<String> set2 = new HashSet<>();
+
+        for (String item : order1)
+            set1.add(item);
+
+        for (String item : order2)
+            set2.add(item);
+
+
+        List<String> list1 = new ArrayList<>(set1);
+        List<String> list2 = new ArrayList<>(set2);
+
+        assertNotEquals(list1, list2);
+    }
+
+    @Test
+    public void testWithAddAll() {
+        List<String> order1 = RandomUtils.createStringList(LIST_SIZE);
+        List<String> order2 = new ArrayList<>(order1);
+
+        Collections.shuffle(order2);
+
+        OurSet<String> set1 = new OurHashSet<>();
+        OurSet<String> set2 = new OurHashSet<>();
+
+        set1.addAll(order1);
+        set2.addAll(order2);
+
+        List<String> list1 = new ArrayList<String>((Collection<? extends String>) set1);
+        List<String> list2 = new ArrayList<String>((Collection<? extends String>) set2);
+
+        assertNotEquals(list1, list2);
+    }
 }
