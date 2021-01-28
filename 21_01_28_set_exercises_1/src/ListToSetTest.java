105ed8a3823c8557dc1efca133b16c5d79697207
enextus <enextus@gmail.com>
2021-01-28 12:51:13 +0100

    upd

--- 21_01_28_set_exercises_1/src/ListToSetTest.java
+++ 21_01_28_set_exercises_1/src/ListToSetTest.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
@@ -14,5 +15,14 @@ class ListToSetTest {
     @Test
     void Test_listToSet_size() {
         assertEquals(2, listToSet.listToSet(arrayList).size());
+    }
+
+
+    @Test
+    void Test_listToSet_NoOrder() {
+        Set<Integer> set_1 = listToSet.listToSet(arrayList);
+        Set<Integer> set_2 = listToSet.listToSet(arrayList);
+
+        assertEqualsNoOrder(set_1, set_2);
     }
 }
