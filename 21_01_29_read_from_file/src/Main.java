1795a4b4996221ff22a2fa5d1d47023320c72ea3
enextus <enextus@gmail.com>
2021-01-30 00:40:14 +0100

    upd

--- 21_01_29_read_from_file/src/Main.java
+++ 21_01_29_read_from_file/src/Main.java
@@ -6,6 +6,10 @@ public class Main {
     final static String FILE_NAME = "/home/enextus/IdeaProjects/java_training_projects/21_01_29_read_from_file/src/input.txt";
 
     public static void main(String[] args) throws IOException {
+
+        final String dir = System.getProperty("user.dir");
+        System.out.println("current dir = " + dir);
+
         FileInputStream inputStream = new FileInputStream(FILE_NAME);
         byte[] bytes = new byte[inputStream.available()];
 
