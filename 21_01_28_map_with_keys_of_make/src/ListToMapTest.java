5e8535627e38a9ee77d64c342343c9f5fbfd8724
enextus <enextus@gmail.com>
2021-01-29 02:38:31 +0100

    upd

---
+++ 21_01_28_map_with_keys_of_make/src/ListToMapTest.java
@@ -0,0 +1,17 @@
+import org.junit.jupiter.api.Test;
+
+class ListToMapTest {
+
+    @Test
+    void findKeys() {
+    }
+
+    @Test
+    void createListOfMake() {
+    }
+
+    @Test
+    void createMap() {
+    }
+
+}
