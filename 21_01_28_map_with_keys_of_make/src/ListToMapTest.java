2410f01f39f39c4e372a757c310c4d4eb8685296
enextus <enextus@gmail.com>
2021-01-29 03:25:20 +0100

    upd

--- 21_01_28_map_with_keys_of_make/src/ListToMapTest.java
+++ 21_01_28_map_with_keys_of_make/src/ListToMapTest.java
@@ -1,6 +1,15 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class ListToMapTest {
+
+    private final List<Auto> lstAutos = new ArrayList<>();
+
+    private final Auto car_1 = new Auto("Opel", "Green");
 
     @Test
     void findKeys() {
@@ -13,5 +22,4 @@ class ListToMapTest {
     @Test
     void createMap() {
     }
-
 }
