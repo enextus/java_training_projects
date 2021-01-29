4ef7e3172f3e2bac8253586dc220c3ac037d241e
enextus <enextus@gmail.com>
2021-01-29 03:35:58 +0100

    upd

--- 21_01_28_map_with_keys_of_make/src/ListToMapTest.java
+++ 21_01_28_map_with_keys_of_make/src/ListToMapTest.java
@@ -22,13 +22,28 @@ class ListToMapTest {
     void test_findKeys_size_of_keySet() {
 
         lstAutos.add(car_1);
+        assertEquals(1, testMap.findKeys(lstAutos).size());
+
+        lstAutos.add(car_1);
+        lstAutos.add(car_2);
+        assertEquals(2, testMap.findKeys(lstAutos).size());
+
+        lstAutos.add(car_1);
+        lstAutos.add(car_2);
+        lstAutos.add(car_3);
+        assertEquals(3, testMap.findKeys(lstAutos).size());
+
+        lstAutos.add(car_1);
+        lstAutos.add(car_2);
+        lstAutos.add(car_3);
+        lstAutos.add(car_4);
+        assertEquals(3, testMap.findKeys(lstAutos).size());
+
+        lstAutos.add(car_1);
         lstAutos.add(car_2);
         lstAutos.add(car_3);
         lstAutos.add(car_4);
         lstAutos.add(car_5);
-
-        testMap.findKeys(lstAutos);
-
         assertEquals(3, testMap.findKeys(lstAutos).size());
     }
 
