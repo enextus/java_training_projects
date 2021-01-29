6ac16a40542da879944ea117e2f1c1043b2ece50
enextus <enextus@gmail.com>
2021-01-30 00:26:36 +0100

    upd

--- 21_01_28_set_exercises_1/src/ListToSet.java
+++ 21_01_28_set_exercises_1/src/ListToSet.java
@@ -5,9 +5,7 @@ import java.util.Set;
 public class ListToSet {
     
     public Set<Integer> addToSet(List<Integer> list) {
-
         Set<Integer> intSet = new HashSet<>();
-
         // for (int v : list) set.add(v);
         intSet.addAll(list);
 
