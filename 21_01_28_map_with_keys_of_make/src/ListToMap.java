4d710f8be9962fd4e6ffa6b62fe734b6324e0ce2
enextus <enextus@gmail.com>
2021-01-29 02:33:49 +0100

    upd

--- 21_01_28_map_with_keys_of_make/src/ListToMap.java
+++ 21_01_28_map_with_keys_of_make/src/ListToMap.java
@@ -14,16 +14,22 @@ public class ListToMap<K, V> {
 
     public List<Auto> createListOfMake(String make, List<Auto> lstOfAutos) {
 
+        List<Auto> res = new ArrayList<>();
+
         for (Auto car : lstOfAutos) {
             if (car.getMake().equals(make)) {
+
                 // return car;
+                // mashina najdena i pishem ee w List
+                res.add(car);
             }
         }
 
-        return null;
+        // wozwra4aem List Auto s konkretnoj make
+        return res;
     }
 
-    public Map<String, List> createMap(List<Auto> lstOfAutos) {
+    public Map<String, List<Auto>> createMap(List<Auto> lstOfAutos) {
 
         // key -> make
         // val -> list of make
