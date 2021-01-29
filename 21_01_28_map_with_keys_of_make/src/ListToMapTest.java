b306eada6da58220c69b96678d2d7c225d6297a2
enextus <enextus@gmail.com>
2021-01-29 03:33:19 +0100

    upd

--- 21_01_28_map_with_keys_of_make/src/ListToMapTest.java
+++ 21_01_28_map_with_keys_of_make/src/ListToMapTest.java
@@ -3,16 +3,33 @@ import org.junit.jupiter.api.Test;
 import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ListToMapTest {
 
-    private final List<Auto> lstAutos = new ArrayList<>();
+    private final ListToMap testMap = new ListToMap();
 
     private final Auto car_1 = new Auto("Opel", "Green");
+    private final Auto car_2 = new Auto("BMW", "White");
+    private final Auto car_3 = new Auto("Audi", "Red");
+    private final Auto car_4 = new Auto("Audi", "Blue");
+    private final Auto car_5 = new Auto("BMW", "Black");
+
+    private final List<Auto> lstAutos = new ArrayList<>();
+
 
     @Test
-    void findKeys() {
+    void test_findKeys() {
+
+        lstAutos.add(car_1);
+        lstAutos.add(car_2);
+        lstAutos.add(car_3);
+        lstAutos.add(car_4);
+        lstAutos.add(car_5);
+
+        testMap.findKeys(lstAutos);
+
+        assertEquals(3, testMap.findKeys(lstAutos).size());
     }
 
     @Test
