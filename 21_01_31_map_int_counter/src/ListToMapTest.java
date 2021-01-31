16d974faf4942503e62e6b0b19320fe7265b5b5d
enextus <enextus@gmail.com>
2021-01-31 23:03:21 +0100

    upd

---
+++ 21_01_31_map_int_counter/src/ListToMapTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ListToMapTest {
+
+    @Test
+    void createMap() {
+    }
+
+    @Test
+    void findKeys() {
+    }
+
+    @Test
+    void countFrequency() {
+    }
+}
