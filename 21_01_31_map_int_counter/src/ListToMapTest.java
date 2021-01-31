b805c4926d1a2aa21beeea2053ae221fccb21f3e
enextus <enextus@gmail.com>
2021-01-31 23:34:56 +0100

    upd

--- 21_01_31_map_int_counter/src/ListToMapTest.java
+++ 21_01_31_map_int_counter/src/ListToMapTest.java
@@ -9,56 +9,56 @@ class ListToMapTest {
 
     // Map format expectation: [1,2,4,2,3,1]-> {{1: 2}, {2: 2}, {3: 1}, {4: 1}}
 
-    private final List<Integer> lstInt = new ArrayList<>();
+    private final List<Integer> lst = new ArrayList<>();
     private final ListToMap testMap = new ListToMap();
 
     @Test
     void test_findKeys_size_of_keySet_1() {
 
-        lstInt.add(1);
-        assertEquals(1, testMap.findKeys(lstInt).size());
+        lst.add(1);
+        assertEquals(1, testMap.findKeys(lst).size());
     }
 
     @Test
     void test_findKeys_size_of_keySet_2() {
 
-        lstInt.add(1);
-        lstInt.add(2);
-        assertEquals(2, testMap.findKeys(lstInt).size());
+        lst.add(1);
+        lst.add(2);
+        assertEquals(2, testMap.findKeys(lst).size());
     }
 
     @Test
     void test_findKeys_size_of_keySet_4() {
 
-        lstInt.add(1);
-        lstInt.add(2);
-        lstInt.add(3);
-        lstInt.add(4);
-        assertEquals(4, testMap.findKeys(lstInt).size());
+        lst.add(1);
+        lst.add(2);
+        lst.add(3);
+        lst.add(4);
+        assertEquals(4, testMap.findKeys(lst).size());
     }
 
     @Test
     void test_findKeys_size_of_keySet_4_() {
 
-        lstInt.add(1);
-        lstInt.add(2);
-        lstInt.add(1);
-        lstInt.add(2);
-        lstInt.add(3);
-        lstInt.add(4);
-        assertEquals(4, testMap.findKeys(lstInt).size());
+        lst.add(1);
+        lst.add(2);
+        lst.add(1);
+        lst.add(2);
+        lst.add(3);
+        lst.add(4);
+        assertEquals(4, testMap.findKeys(lst).size());
     }
 
     @Test
     void test_findKeys_size_of_keySet_3_() {
 
-        lstInt.add(4);
-        lstInt.add(4);
-        lstInt.add(3);
-        lstInt.add(3);
-        lstInt.add(2);
-        lstInt.add(2);
-        assertEquals(3, testMap.findKeys(lstInt).size());
+        lst.add(4);
+        lst.add(4);
+        lst.add(3);
+        lst.add(3);
+        lst.add(2);
+        lst.add(2);
+        assertEquals(3, testMap.findKeys(lst).size());
     }
 
     @Test
