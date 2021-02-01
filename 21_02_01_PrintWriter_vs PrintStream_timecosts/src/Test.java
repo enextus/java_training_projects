2242870f22e81c07e726e3cedd8727c1b22b1dc2
enextus <enextus@gmail.com>
2021-02-01 23:50:53 +0100

    upd

--- 21_02_01_PrintWriter_vs PrintStream_timecosts/src/Test.java
+++ 21_02_01_PrintWriter_vs PrintStream_timecosts/src/Test.java
@@ -1,2 +1,41 @@
+import java.io.*;
+
 public class Test {
+    static byte[] bytes = new byte[10_000_000];
+
+    static {
+        for (int i = 0; i < bytes.length; i++) bytes[i] = (byte) (i % 100 + 32);
+    }
+
+    public static void main(String[] args) throws Exception {
+        writer(true);
+        writer(false);
+        stream(true);
+        stream(false);
+    }
+
+    static void writer(boolean flush) throws IOException {
+        Writer out = new FileWriter("output.txt");
+        long a = System.currentTimeMillis();
+        for (byte j : bytes) {
+            out.write(j);
+            if (flush) out.flush();
+        }
+        out.close();
+        System.out.println("FileWriter with" + (flush ? "" : "out") + " flushing: " +
+                (System.currentTimeMillis() - a));
+    }
+
+    static void stream(boolean flush) throws IOException {
+        OutputStream out = new FileOutputStream("output.txt");
+        long a = System.currentTimeMillis();
+        for (byte j : bytes) {
+            out.write(j);
+            if (flush) out.flush();
+        }
+        out.close();
+        System.out.println("FileOutputStream with" + (flush ? "" : "out") + " flushing: " +
+                (System.currentTimeMillis() - a));
+    }
+
 }
