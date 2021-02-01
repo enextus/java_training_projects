5b278cf486696aeb94d610e4ce6abb7faf960c6e
enextus <enextus@gmail.com>
2021-02-01 23:31:31 +0100

    upd

--- 21_01_29_map_exercises_О(logN)/src/AutoService.java
+++ 21_01_29_map_exercises_О(logN)/src/AutoService.java
@@ -14,19 +14,28 @@ public class AutoService {
 
         Map<String, List<Auto>> result = new HashMap<>();
 
-        System.out.println("result: " + result);
-
-        System.out.println("______________________________________");
-
         for (Auto auto : autos) {
+
             List<Auto> autosList = result.get(auto.getMake());
+
+            System.out.println(result.get(auto.getMake()));
+            System.out.println(autosList);
 
             if (autosList == null) {
                 autosList = new ArrayList<>();
+
+                // wir benutzen Map um in dem Map die Keys zu erstellen
+                // falls das Key bereits existiert - wird nichts passieren
+                // es wird kein neuen Key erstellt
                 result.put(auto.getMake(), autosList);
             }
 
+            System.out.println("1. ->>>" + autosList);
+
             autosList.add(auto);
+
+            System.out.println("2. ->>>" + autosList);
+            System.out.println("\n");
         }
 
         return result;
--- 21_01_29_map_exercises_О(logN)/src/Main.java
+++ 21_01_29_map_exercises_О(logN)/src/Main.java
@@ -6,16 +6,20 @@ public class Main {
     public static void main(String[] args) {
 
         Auto auto_1 = new Auto("bmw", "white");
+        Auto auto_2 = new Auto("opel", "green");
+        Auto auto_3 = new Auto("opel", "red");
 
         AutoService ats = new AutoService();
 
         List<Auto> lst = new ArrayList<>();
 
         lst.add(auto_1);
+        lst.add(auto_2);
+        lst.add(auto_3);
 
-        System.out.println("lst: " + lst);
+        System.out.println("0. List<Auto> autos (Main) ---> " + lst);
 
-        ats.groupByMake(lst);
+        System.out.println("Map result: " + ats.groupByMake(lst));
     }
-    
+
 }
