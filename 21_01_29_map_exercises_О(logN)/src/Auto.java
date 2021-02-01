df6eb3ce95c058d4a5e4114c748d19dd64dbd0c4
enextus <enextus@gmail.com>
2021-02-01 22:18:33 +0100

    upd

---
+++ 21_01_29_map_exercises_О(logN)/src/Auto.java
@@ -0,0 +1,23 @@
+public class Auto {
+
+    private String make;
+    private String color;
+
+    public Auto(String make, String color) {
+        this.color = color;
+        this.make = make;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public String getMake() {
+        return make;
+    }
+    
+}
