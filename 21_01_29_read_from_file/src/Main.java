dbcf5c9854c013871a1c3278521f9fd29ed06a54
enextus <enextus@gmail.com>
2021-02-01 21:55:55 +0100

    upd

--- 21_01_29_read_from_file/src/Main.java
+++ 21_01_29_read_from_file/src/Main.java
@@ -8,18 +8,20 @@ public class Main {
     final static String FILE_NAME = "warandpeace_copy.txt";
 
     public static void main(String[] args) throws IOException {
+
         // System.out.println("Current dir: " + dir);
 
         System.out.println("________________________________\n");
-
         System.out.println("FILE_NAME: " + FILE_NAME);
-
         System.out.println("________________________________\n");
 
         FileInputStream inputStream = new FileInputStream(FILE_NAME);
         byte[] bytesArray = new byte[inputStream.available()];
 
+        System.out.println("bytesArray.length: " + bytesArray.length);
+
         int num = inputStream.read(bytesArray);
+
         String res = new String(bytesArray);
 
         System.out.println("________________________________\n");
