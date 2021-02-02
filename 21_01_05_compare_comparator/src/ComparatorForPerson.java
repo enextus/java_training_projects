0638ac25ceccf7b2f5d486757a82ad8cafa38ca3
enextus <enextus@gmail.com>
2021-02-02 02:00:56 +0100

    upd

--- 21_01_05_compare_comparator/src/ComparatorForPerson.java
+++ 21_01_05_compare_comparator/src/ComparatorForPerson.java
@@ -1,23 +1,10 @@
 import java.util.Comparator;
 
 class ComparatorForPerson implements Comparator<PersonComparator> {
-    @Override
-    public String toString() {
-        return "ComparatorForPerson{}";
-    }
-
-    @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
-    }
 
     @Override
     public int compare(PersonComparator o1, PersonComparator o2) {
         return o1.age - o2.age;
     }
+
 }
--- 21_01_05_compare_comparator/src/PersonComparator.java
+++ 21_01_05_compare_comparator/src/PersonComparator.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class PersonComparator {
 
     int age;
@@ -8,6 +10,26 @@ public class PersonComparator {
 
     public int getAge() {
         return age;
+    }
+
+    @Override
+    public String toString() {
+        return "PersonComparator{" +
+                "age=" + age +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof PersonComparator)) return false;
+        PersonComparator that = (PersonComparator) o;
+        return getAge() == that.getAge();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getAge());
     }
 
 }
