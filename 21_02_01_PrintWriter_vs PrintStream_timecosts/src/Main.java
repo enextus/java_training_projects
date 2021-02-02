18654a2990f0cb7a491c3d585c064abed82648f7
enextus <enextus@gmail.com>
2021-02-02 09:43:53 +0100

    upd

--- 21_02_01_PrintWriter_vs PrintStream_timecosts/src/Main.java
+++ 21_02_01_PrintWriter_vs PrintStream_timecosts/src/Main.java
@@ -3,11 +3,12 @@ import printtester.PrintTest;
 import printtester.printtester.PrintStreamPrinter;
 import printtester.printtester.PrintWriterPrinter;
 
+import java.io.IOException;
 import java.io.PrintWriter;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         IPrinter printWriterPrinter = new PrintWriterPrinter();
         IPrinter printStreamPrinter = new PrintStreamPrinter();
