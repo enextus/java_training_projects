2dfc87dd507a24f1e074865bf1d2e7ec55222212
enextus <enextus@gmail.com>
2021-02-02 01:23:49 +0100

    upd

---
+++ 21_01_05_compare_comparator/src/PersonComparator.java
@@ -0,0 +1,13 @@
+public class PersonComparator {
+
+    int age;
+
+    public PersonComparator(int age) {
+        this.age = age;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+}
