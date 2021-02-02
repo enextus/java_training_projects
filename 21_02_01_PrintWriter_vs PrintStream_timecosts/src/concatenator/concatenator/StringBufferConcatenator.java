95ee5904f4651cc15a0f5b6f641957161c61af81
enextus <enextus@gmail.com>
2021-02-02 08:45:40 +0100

    upd

---
+++ 21_02_01_PrintWriter_vs PrintStream_timecosts/src/concatenator/concatenator/StringBufferConcatenator.java
@@ -0,0 +1,18 @@
+package concatenator.concatenator;
+
+import concatenator.IConcatenator;
+
+public class StringBufferConcatenator implements IConcatenator {
+
+    @Override
+    public void concatenate(String[] arrayToConcatenate) {
+
+        StringBuffer buffer = new StringBuffer();
+
+        for (String str : arrayToConcatenate)
+            buffer.append(str);
+
+        buffer.toString(); // anschließende Umwandlung Obj toString
+    }
+
+}
---
+++ 21_02_01_PrintWriter_vs PrintStream_timecosts/src/concatenator/concatenator/StringBuilderConcatenator.java
@@ -0,0 +1,18 @@
+package concatenator.concatenator;
+
+import concatenator.IConcatenator;
+
+public class StringBuilderConcatenator implements IConcatenator {
+
+    @Override
+    public void concatenate(String[] arrayToConcatenate) {
+
+        StringBuilder builder = new StringBuilder();
+
+        for (String str : arrayToConcatenate)
+            builder.append(str);
+
+        builder.toString();
+    }
+
+}
