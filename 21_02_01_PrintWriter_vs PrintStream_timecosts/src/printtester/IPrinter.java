320b034600814e0c50d85e82ab857225d4ae554a
enextus <enextus@gmail.com>
2021-02-02 09:27:17 +0100

    upd

--- 21_02_01_PrintWriter_vs PrintStream_timecosts/src/printtester/IPrinter.java
+++ 21_02_01_PrintWriter_vs PrintStream_timecosts/src/printtester/IPrinter.java
@@ -1,9 +1,12 @@
 package printtester;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 /**
  * The interface provides the single method print which should provide printing two String variables
  */
 public interface IPrinter {
 
-    void print(String[] arrayToPrint);
+    void print(String[] arrayToPrint) throws IOException;
 }
--- 21_02_01_PrintWriter_vs PrintStream_timecosts/src/printtester/printtester/StringBufferPrinter.java
+++ 21_02_01_PrintWriter_vs PrintStream_timecosts/src/printtester/printtester/StringBufferPrinter.java
@@ -2,17 +2,52 @@ package printtester.printtester;
 
 import printtester.IPrinter;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.Writer;
+
 public class StringBufferPrinter implements IPrinter {
 
+    private final static byte[] bytes = new byte[1_000_000];
+
+    static {
+        for (int i = 0; i < bytes.length; i++) {
+            bytes[i] = (byte) (i % 100 + 32);
+        }
+    }
+
+    static long getCurrentTimeMillis() {
+
+        return System.currentTimeMillis();
+    }
+
     @Override
-    public void print(String[] arrayToPrint) {
+    public void print(String[] arrayToPrint) throws IOException {
 
-        StringBuffer buffer = new StringBuffer();
+        boolean flush = true;
 
+        Writer pw = new PrintWriter("output.txt");
+        long beforeTest = getCurrentTimeMillis();
+
+        for (byte j : bytes) {
+            pw.write(j);
+
+            if (flush)
+                pw.flush();
+        }
+
+        pw.close();
+
+
+
+
+/*      StringBuffer buffer = new StringBuffer();
         for (String str : arrayToPrint)
             buffer.append(str);
 
-        buffer.toString(); // anschließende Umwandlung Obj toString
+        buffer.toString(); // anschließende Umwandlung Obj toString*/
+
     }
 
 }
