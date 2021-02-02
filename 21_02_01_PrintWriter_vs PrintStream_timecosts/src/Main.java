88f04a76b48252fb543eebc468d55d43f1fa62a4
enextus <enextus@gmail.com>
2021-02-02 08:46:14 +0100

    upd

--- 21_02_01_PrintWriter_vs PrintStream_timecosts/src/Main.java
+++ 21_02_01_PrintWriter_vs PrintStream_timecosts/src/Main.java
@@ -2,21 +2,15 @@ import concatenator.IConcatenator;
 import concatenator.ConcatenationTest;
 import concatenator.concatenator.StringBufferConcatenator;
 import concatenator.concatenator.StringBuilderConcatenator;
-import concatenator.concatenator.StringConcatenator;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        IConcatenator stringConcatenator = new StringConcatenator();
         IConcatenator stringBuilderConcatenator = new StringBuilderConcatenator();
         IConcatenator stringBufferConcatenator = new StringBufferConcatenator();
 
         ConcatenationTest test;
-
-        //string test
-        test = new ConcatenationTest(stringConcatenator);
-        test.test(100000, "Santa Claus");
 
         //string builder test
         test = new ConcatenationTest(stringBuilderConcatenator);
