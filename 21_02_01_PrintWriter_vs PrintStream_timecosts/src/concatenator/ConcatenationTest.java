0c4496770d97036e99d41318cf12dd33c151fad9
enextus <enextus@gmail.com>
2021-02-02 08:16:55 +0100

    upd

---
+++ 21_02_01_PrintWriter_vs PrintStream_timecosts/src/concatenator/ConcatenationTest.java
@@ -0,0 +1,29 @@
+package concatenator;
+
+import java.util.Arrays;
+
+public class ConcatenationTest {
+
+    private final IConcatenator concatenator;
+
+    public ConcatenationTest(IConcatenator concatenator) {
+        this.concatenator = concatenator;
+    }
+
+    public void test(int times, String text) {
+        String[] arrayToConcatenate = composeArray(times, text);
+
+        long beforeTest = System.currentTimeMillis();
+        concatenator.concatenate(arrayToConcatenate);
+        long afterTest = System.currentTimeMillis();
+
+        System.out.println(afterTest - beforeTest);
+    }
+
+    private String[] composeArray(int times, String text) {
+        String[] strings = new String[times];
+
+        Arrays.fill(strings, text);
+        return strings;
+    }
+}
