b2d55aa8a159ee00e562c31307393a89bd2bd2ef
enextus <enextus@gmail.com>
2021-02-03 05:30:36 +0100

    upd

---
+++ 21_02_02_read_console_line_with_BufferedReader/src/Main.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+class Main {
+    // Read multi-line input from console in Java 7 using BufferedReader class
+    public static void main(String[] args) {
+        try (InputStreamReader in = new InputStreamReader(System.in);
+             BufferedReader buffer = new BufferedReader(in)) {
+            String line;
+            while ((line = buffer.readLine()) != null) {
+                String[] tokens = line.split("\\s");
+                System.out.println(Arrays.toString(tokens));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
