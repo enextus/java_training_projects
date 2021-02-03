c66992f8410263048dd798adff6f1fb56ebe2db1
enextus <enextus@gmail.com>
2021-02-03 06:22:27 +0100

    upd

--- 21_02_02_read_console_line_with_BufferedReader/src/ReadLinesFromConsole.java
+++ 21_02_02_read_console_line_with_BufferedReader/src/ReadLinesFromConsole.java
@@ -9,14 +9,11 @@ public class ReadLinesFromConsole {
             String line;
 
             while ((line = inBuffer.readLine()) != null && !line.equals("exit")) {
-
                 String[] arrWords = line.split("\\s");
-
                 System.out.println("Your input was: " + Arrays.toString(arrWords));
             }
 
         } catch (Exception e) {
-
             e.printStackTrace();
         }
 
