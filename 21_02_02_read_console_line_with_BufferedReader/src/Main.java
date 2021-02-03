86cad6aa63372e510750b40b819819f5a0210600
enextus <enextus@gmail.com>
2021-02-03 06:05:12 +0100

    upd

--- 21_02_02_read_console_line_with_BufferedReader/src/Main.java
+++ 21_02_02_read_console_line_with_BufferedReader/src/Main.java
@@ -1,26 +1,14 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
 class Main {
     // Read multi-line input using BufferedReader class
-    public static void main(String[] args) {
-        try (BufferedReader inBuffer = new BufferedReader(new InputStreamReader(System.in))) {
-
-            String line;
-
-            while ((line = inBuffer.readLine()) != null && !line.equals("exit")) {
-
-                String[] arrWords = line.split("\\s");
-
-                System.out.println("Your input was: " + Arrays.toString(arrWords));
-            }
-
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("Bye!");
+    public static void main(String[] args) throws IOException {
+        readFromConsole();
     }
+
+
 
 }
