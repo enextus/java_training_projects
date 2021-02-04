214bd825d1b7b191aede0f26298155881bacfb63
enextus <enextus@gmail.com>
2021-02-04 23:52:13 +0100

    upd

--- 21_02_03_revers_of_a_string/src/Main.java
+++ 21_02_03_revers_of_a_string/src/Main.java
@@ -2,9 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String strToReverse;
-
         ReverseString strReverser = new ReverseString();
+        String strToReverse;
 
         strToReverse = "А роза упала на лапу Азора";
 
