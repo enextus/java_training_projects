14330805cedc0e652456d754d9308af72b475635
enextus <enextus@gmail.com>
2021-02-05 00:31:31 +0100

    upd

--- 21_02_04_ninety_nine_numbers/src/Main.java
+++ 21_02_04_ninety_nine_numbers/src/Main.java
@@ -1,7 +1,37 @@
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
-        //
+
+
+        List<Integer> intList = new ArrayList<>();
+
+        Random r = new Random();
+        int low = 1;
+        int high = 99;
+        int secretNumber = r.nextInt(high - low) + low;
+
+
+        for (int i = 1; i <= 99; i++) {
+            intList.add(i);
+        }
+
+        Collections.shuffle(intList);
+
+        System.out.println("intList: " + intList);
+
+        // _______________________________________________________
+
+        Set<Integer> intSetFull = new TreeSet<>(); // ordered
+
+        for (int i = 1; i <= 100; i++) {
+            intSetFull.add(i);
+        }
+
+        System.out.println(intSetFull);
+
+        Set<Integer> intSetShort = new HashSet<>(); // not ordered
 
 
     }
