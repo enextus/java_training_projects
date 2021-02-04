68ebcdb3e40835aa20d47b0c30d485989ca1d7f2
enextus <enextus@gmail.com>
2021-02-04 23:42:55 +0100

    upd

--- 21_02_03_revers_of_a_string/src/ReverseStringTest.java
+++ 21_02_03_revers_of_a_string/src/ReverseStringTest.java
@@ -7,7 +7,17 @@ class ReverseStringTest {
     private final ReverseString strReverser = new ReverseString();
 
     @Test
-    void test_reverseString_abc_cba() {
+    void test_reverseString_abc_vs_cba() {
         assertEquals("cba", strReverser.reverseString("abc"));
+    }
+
+    @Test
+    void test_reverseString_a_b_c_vs_c_b_a() {
+        assertEquals("c b a", strReverser.reverseString("a b c"));
+    }
+
+    @Test
+    void test_reverseString_A_b_C_vs_C_b_A() {
+        assertEquals("C b A", strReverser.reverseString("A b C"));
     }
 }
