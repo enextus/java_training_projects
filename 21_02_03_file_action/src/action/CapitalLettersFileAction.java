2f6ef3c6f99d2970b96df7f5f9a9ede1a3a5045a
enextus <enextus@gmail.com>
2021-02-04 07:43:27 +0100

    upd

---
+++ 21_02_03_file_action/src/action/CapitalLettersFileAction.java
@@ -0,0 +1,16 @@
+package action;
+
+import java.util.List;
+
+public class CapitalLettersFileAction extends FileAction {
+    public CapitalLettersFileAction(FileOperations fileOperations) {
+        super(fileOperations);
+    }
+
+    @Override
+    protected List<String> perform(List<String> text) {
+        // TODO implement
+        return null;
+    }
+
+}
