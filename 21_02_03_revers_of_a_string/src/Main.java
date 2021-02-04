73f70b90226a781f0838782f58d877a97be6f9e2
enextus <enextus@gmail.com>
2021-02-04 07:24:38 +0100

    upd

--- 21_02_03_revers_of_a_string/src/Main.java
+++ 21_02_03_revers_of_a_string/src/Main.java
@@ -1,7 +1,13 @@
 public class Main {
     public static void main(String[] args) {
 
-        //
+        String strToReverse;
+
+        ReverseString strReverser = new ReverseString();
+
+        strToReverse = "А роза упала на лапу Азора";
+
+        System.out.println("Result: " + strReverser.reverseString(strToReverse.toLowerCase()));
 
     }
 }
--- 21_02_03_revers_of_a_string/src/ReverseString.java
+++ 21_02_03_revers_of_a_string/src/ReverseString.java
@@ -3,14 +3,18 @@ import java.util.Stack;
 public class ReverseString {
 
     public String reverseString(String str) {
+
         Stack<Character> stack = new Stack<>();
         String result = "";
+
         for (Character character : str.toCharArray()) {
             stack.add(character);
         }
+
         while (!stack.isEmpty()) {
             result = result + stack.pop();
         }
+
         return result;
     }
 
