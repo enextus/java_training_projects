290fbc0302d234021e6990a52c306bc1cd4c15f3
enextus <enextus@gmail.com>
2021-02-05 10:41:17 +0100

    upd

--- 21_02_05_runnable/src/MyRunnable.java
+++ 21_02_05_runnable/src/MyRunnable.java
@@ -1,2 +1,24 @@
-public class MyRunnable {
+
+class MyRunnable implements Runnable {
+    Thread thread;
+
+    // Конструктор
+    MyRunnable() {
+        // Создаём новый второй поток
+        thread = new Thread(this, "Поток для примера");
+        Log.i(TAG, "Создан второй поток " + thread);
+        thread.start(); // Запускаем поток
+    }
+
+    // Обязательный метод для интерфейса Runnable
+    public void run() {
+        try {
+            for (int i = 5; i > 0; i--) {
+                Log.i(TAG, "Второй поток: " + i);
+                Thread.sleep(500);
+            }
+        } catch (InterruptedException e) {
+            Log.i(TAG, "Второй поток прерван");
+        }
+    }
 }
