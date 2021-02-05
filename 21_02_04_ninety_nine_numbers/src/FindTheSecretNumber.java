09906546575802d1983ff2d939162d8de1011d0a
enextus <enextus@gmail.com>
2021-02-05 01:05:02 +0100

    upd

--- 21_02_04_ninety_nine_numbers/src/FindTheSecretNumber.java
+++ 21_02_04_ninety_nine_numbers/src/FindTheSecretNumber.java
@@ -1,2 +1,27 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 public class FindTheSecretNumber {
+
+    public List<Integer> generateTheList(){
+
+        List<Integer> intList = new ArrayList<>();
+        Random rnd = new Random();
+        int low = 1;
+        int high = 100;
+        int secretNumber = rnd.nextInt(high - low) + low;
+
+        for (int i = 1; i <= 100; i++) {
+            if (i == secretNumber)
+                continue;
+
+            intList.add(i);
+        }
+
+        Collections.shuffle(intList);
+
+        return intList;
+    }
 }
