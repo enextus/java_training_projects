e983dd84fe7c497cce5b5c949b5ad84502f48d41
enextus <enextus@gmail.com>
2021-02-05 11:30:10 +0100

    upd

--- 21_02_05_runnable/src/MyRunnable.java
+++ 21_02_05_runnable/src/MyRunnable.java
@@ -1,28 +1,28 @@
 class MyRunnable implements Runnable {
 
     Thread thread;
-    final String TAG = "Some TAG here - ";
+    final String tag = "Some TAG here - ";
 
     MyRunnable() {
         // second
         thread = new Thread(this, "first thread");
-        System.out.println(TAG + "first thread " + thread + "\n");
+        System.out.println(tag + "first thread " + thread + "\n");
         thread.start(); // start of first thread
     }
 
     @Override
     public void run() {
 
-        System.out.println("Start second thread: ");
+        System.out.println("Start thread: ");
 
         try {
             for (int i = 5; i > 0; i--) {
-                System.out.println(TAG + "thread: " + i);
+                System.out.println(tag + "thread: " + i);
                 Thread.sleep(5);
             }
         } catch (InterruptedException e) {
             System.out.println("e: " + e + "\n");
-            System.out.println(TAG + "thread was broken");
+            System.out.println(tag + "thread was broken");
             // break;
         }
     }
