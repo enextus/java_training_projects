0ae5e7d5d52567d22df7b7f6f8676acad2256b7c
enextus <enextus@gmail.com>
2021-02-05 01:46:17 +0100

    upd

--- 21_02_04_ninety_nine_numbers/src/FindTheLostNumberTest.java
+++ 21_02_04_ninety_nine_numbers/src/FindTheLostNumberTest.java
@@ -1,9 +1,14 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class FindTheLostNumberTest {
 
+    private final FindTheLostNumber findTheLostNumber = new FindTheLostNumber();
+
     @Test
-    void generateTheList() {
+    void test_generateTheList_size() {
+        assertEquals(99, findTheLostNumber.generateTheList().size());
     }
 
 }
