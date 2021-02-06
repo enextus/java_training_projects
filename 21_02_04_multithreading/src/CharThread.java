7199dbb33efc15024e3fc1bcdc97513255d7a78e
enextus <enextus@gmail.com>
2021-02-06 15:08:25 +0100

    upd

---
+++ 21_02_04_multithreading/src/CharThread.java
@@ -0,0 +1,25 @@
+public class CharThread extends Thread {
+
+    final int times;
+    final char ch;
+
+    public CharThread(int times, char ch) {
+        this.times = times;
+        this.ch = ch;
+    }
+
+    @Override
+    public void run() {
+        System.out.println("Start CharThread");
+        for (int i = 0; i < times; i++) {
+            System.out.println(ch);
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
