35e70137a27946b5ce154692432625ff6e39f955
enextus <enextus@gmail.com>
2021-02-06 15:22:03 +0100

    upd

--- 21_02_04_multithreading/src/CharThread.java
+++ 21_02_04_multithreading/src/CharThread.java
@@ -1,11 +1,24 @@
 public class CharThread extends Thread {
 
     final int times;
+    final int commonNumber;
     final char ch;
 
     public CharThread(int times, char ch) {
         this.times = times;
         this.ch = ch;
+        this.commonNumber = makeCommonNumber();
+    }
+
+    public int getCommonNumber() {
+        return commonNumber;
+    }
+
+    int makeCommonNumber() {
+        int temp = this.getCommonNumber();
+        temp += 1;
+
+        return temp;
     }
 
     @Override
