33bfa4d36fe6f4c93e89067489ee19565764be51
enextus <enextus@gmail.com>
2021-02-06 15:29:06 +0100

    upd

--- 21_02_04_multithreading/src/CharThread.java
+++ 21_02_04_multithreading/src/CharThread.java
@@ -2,28 +2,33 @@
 public class CharThread extends Thread {
 
     public static class instanceCounter {
-     //
+
+        final int commonNumber;
+
+        public instanceCounter(int commonNumber) {
+            this.commonNumber = commonNumber;
+        }
+
+        public int getCommonNumber() {
+            return commonNumber;
+        }
+
+        public int makeCommonNumber() {
+            int temp = this.getCommonNumber();
+            temp += 1;
+
+            return temp;
+        }
     }
 
     final int times;
-    final int commonNumber;
     final char ch;
+    final int commonNumber;
 
     public CharThread(int times, char ch) {
         this.times = times;
         this.ch = ch;
-        this.commonNumber = makeCommonNumber();
-    }
-
-    public int getCommonNumber() {
-        return commonNumber;
-    }
-
-    int makeCommonNumber() {
-        int temp = this.getCommonNumber();
-        temp += 1;
-
-        return temp;
+        this.commonNumber = instanceCounter.makeCommonNumber();
     }
 
     @Override
