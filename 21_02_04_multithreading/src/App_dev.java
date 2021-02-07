661ac51c0ae65aa30ac552c4b0e22b3c04a407f5
enextus <enextus@gmail.com>
2021-02-07 07:06:18 +0100

    upd

--- 21_02_04_multithreading/src/App_dev.java
+++ 21_02_04_multithreading/src/App_dev.java
@@ -3,25 +3,25 @@ public class App_dev {
     public static void main(String[] args) {
 
         CharThread hath = new CharThread(1, '#');
+        Thread th = new Thread(hath);
+        th.start();
+        // __________________________________________________________________
 
-        System.out.println(hath.times);
-        System.out.println(hath.ch);
-        System.out.println(hath.getCommonNumber());
-        System.out.println();
+        CharThread hath1 = new CharThread(1, '#');
+        Thread th1 = new Thread(hath1);
+        th1.start();
+        // __________________________________________________________________
 
-        CharThread hath2 = new CharThread(1, '*');
+        CharThread hath2 = new CharThread(1, '#');
+        Thread th2 = new Thread(hath2);
+        th2.start();
+        // __________________________________________________________________
 
-        System.out.println(hath2.times);
-        System.out.println(hath2.ch);
-        System.out.println(hath2.getCommonNumber());
-        System.out.println();
+        CharThread hath3 = new CharThread(1, '#');
+        Thread th3 = new Thread(hath3);
+        th3.start();
+        // __________________________________________________________________
 
-        CharThread hath3 = new CharThread(1, '=');
-
-        System.out.println(hath3.times);
-        System.out.println(hath3.ch);
-        System.out.println(hath3.getCommonNumber());
-        System.out.println();
 
     }
 
