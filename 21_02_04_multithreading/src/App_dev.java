8ca9bb4ade81f7bf101b3797497694be478bf313
enextus <enextus@gmail.com>
2021-02-07 06:10:38 +0100

    upd

--- 21_02_04_multithreading/src/App_dev.java
+++ 21_02_04_multithreading/src/App_dev.java
@@ -1,2 +1,12 @@
 public class App_dev {
+
+    public static void main(String[] args) {
+
+        CharThread chth = new CharThread(1, '*');
+
+        System.out.println(chth.times);
+        System.out.println(chth.ch);
+        System.out.println(chth.commonNumber);
+    }
+
 }
