9163ca5bf2e72ee98bea8b0c7320f8e9f094b937
enextus <enextus@gmail.com>
2021-02-07 20:05:34 +0100

    upd

---
+++ 21_02_04_tarakans/src/App_dev.java
@@ -0,0 +1,63 @@
+public class App_dev {
+
+    public static void main(String[] args) {
+
+        // __________________________________________________________________
+
+        MyCharThread myOwnThread = new MyCharThread(1, '#');
+        // realise Thread with JAVA implementation and our MyCharThread class
+        Thread threadRes = new Thread(myOwnThread);
+
+        // start of our thread
+        threadRes.start();
+
+        // get parameters
+        System.out.println("threadRes.getName(): " + threadRes.getName());
+        System.out.println("threadRes.getId(): " + threadRes.getId());
+        System.out.println("threadRes.getState(): " + threadRes.getState());
+        System.out.println("threadRes.getPriority(): " + threadRes.getPriority());
+        System.out.println();
+
+        // __________________________________________________________________
+
+        MyCharThread hath1 = new MyCharThread(1, '*');
+        // realise Thread with JAVA implementation and our MyCharThread class
+        Thread threadRes1 = new Thread(hath1);
+
+        // start of our thread
+        threadRes1.start();
+
+        // get parameters
+        System.out.println("threadRes1.getName(): " + threadRes1.getName());
+        System.out.println("threadRes1.getId(): " + threadRes1.getId());
+        System.out.println("threadRes1.getState(): " + threadRes1.getState());
+        System.out.println("threadRes1.getPriority(): " + threadRes1.getPriority());
+        System.out.println();
+
+        // __________________________________________________________________
+
+        MyCharThread hath2 = new MyCharThread(1, '%');
+        // realise Thread with JAVA implementation and our MyCharThread class
+        Thread threadRes2 = new Thread(hath2);
+
+        // start of our thread
+        threadRes2.start();
+
+        // get parameters
+        System.out.println("threadRes2.getName(): " + threadRes2.getName());
+        System.out.println("threadRes2.getId(): " + threadRes2.getId());
+        System.out.println("threadRes2.getState(): " + threadRes2.getState());
+        System.out.println("threadRes2.getPriority(): " + threadRes2.getPriority());
+        System.out.println();
+
+/*      // __________________________________________________________________
+
+        MyCharThread hath3 = new MyCharThread(1, '~');
+        // realise Thread with JAVA implementation and our MyCharThread class
+        Thread threadRes3 = new Thread(hath3);
+
+        // start of our thread
+        threadRes3.start();*/
+    }
+
+}
---
+++ 21_02_04_tarakans/src/MyCharThread.java
@@ -0,0 +1,67 @@
+import java.util.Date;
+
+public class MyCharThread extends Thread {
+
+    public static int callCount = 0;
+    public static long classCreationTime = 0;
+
+    // This static block will be executed if the class CharThread will be loaded by JVM;
+    static {
+        final long classCreationTime = new Date().getTime();
+        setClassCreationTime(classCreationTime);
+
+        System.out.println("0. - " + classCreationTime + " - was executed.\n");
+    }
+
+    // This non-static block will be executed if an instance will be created;
+    {
+        callCount += 1;
+    }
+
+    final int times;
+    final char ch;
+    final int commonNumber;
+
+    public MyCharThread(int times, char ch) {
+        this.times = times;
+        this.ch = ch;
+        this.commonNumber = getCallCount();
+    }
+
+    public static void setClassCreationTime(long classCreationTime) {
+        MyCharThread.classCreationTime = classCreationTime;
+    }
+
+    public static int getCallCount() {
+        return callCount;
+    }
+
+    public char getCh() {
+        return this.ch;
+    }
+
+    public int getCommonNumber() {
+        return this.commonNumber;
+    }
+
+    @Override
+    public void run() {
+
+        // Here is the logic of not-main thread!!!
+        System.out.println("Start CharThread.");
+
+        for (int i = 0; i < times; i++) {
+
+            System.out.println("commonNumber: " + getCommonNumber());
+            System.out.println("ch: " + getCh());
+
+
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+}
