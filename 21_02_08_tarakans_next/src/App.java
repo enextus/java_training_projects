7f03af06b94a39322fbf493e9362ad56ef3cac42
enextus <enextus@gmail.com>
2021-02-08 19:19:46 +0100

    upd

--- 21_02_08_tarakans_next/src/App.java
+++ 21_02_08_tarakans_next/src/App.java
@@ -5,18 +5,18 @@ import java.util.List;
 /*
 
 Done!! Print Your Tarakan - List:
-        [006:  time: 659 ms.]
-    [008:  time: 707 ms.]
-    [003:  time: 717 ms.]
-    [005:  time: 720 ms.]
-    [007:  time: 736 ms.]
-    [004:  time: 739 ms.]
-    [0010:  time: 753 ms.]
-    [009:  time: 767 ms.]
-    [002:  time: 769 ms.]
-    [001:  time: 780 ms.]
+[005:  time: 664 ms.]
+[007:  time: 706 ms.]
+[003:  time: 707 ms.]
+[001:  time: 723 ms.]
+[009:  time: 742 ms.]
+[0010:  time: 765 ms.]
+[002:  time: 784 ms.]
+[004:  time: 805 ms.]
+[006:  time: 827 ms.]
+[008:  time: 836 ms.]
 
-Tarakan: 006 won wit the time: 4974 | -> whole real time: 4974
+Tarakan: 005 won wit the time: 664
  */
 public class App {
 
