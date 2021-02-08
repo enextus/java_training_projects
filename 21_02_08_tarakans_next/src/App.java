7f91572184d859eebe1ef307065d153b55f4f183
enextus <enextus@gmail.com>
2021-02-08 18:53:20 +0100

    upd

---
+++ 21_02_08_tarakans_next/src/App.java
@@ -0,0 +1,91 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Date;
+import java.util.List;
+
+public class App {
+
+    public static void main(String[] args) throws InterruptedException {
+
+        int count = 10;
+
+        MyTarakanThread[] threadTrack = new MyTarakanThread[count];
+        List<MyTarakanThread> listParticipants = new ArrayList<>();
+
+        for (int i = 0; i < count; i++) {
+            threadTrack[i] = new MyTarakanThread(listParticipants, "00" + (i + 1));
+            threadTrack[i].start();
+        }
+
+        for (int i = 0; i < count; i++) {
+            threadTrack[i].join();
+        }
+/*
+
+        while (isOperationRunning(threadTrack)) {
+
+            // do nothing
+        }
+*/
+
+        System.out.println("Done!! Print Your Tarakan - List:");
+
+        Collections.sort(listParticipants);
+        MyTarakanThread temp = listParticipants.get(0);
+
+        for (MyTarakanThread item : listParticipants) {
+            System.out.println("["
+                    + item.getNickname() + ": "
+                    + " WholeTimeForAllRealTime: -> " +
+                    +item.getWholeTimeForAllRealTimes() + " ms.; ---> " +
+
+                    new Date(item.getWholeTimeForAllRealTimes()).getTime()
+                    + " whole time as given: "
+                    + item.getWholeTimeForAllTimes() + " ms."
+                    + "]");
+        }
+
+        System.out.println();
+        System.out.println(
+                "Tarakan: " + temp.getNickname()
+                + " won wit the time: " + temp.getWholeTimeForAllRealTimes()
+                + " | -> common number: " + temp.getCommonNumber()
+                + " | -> class creation real time: " + temp.getClassCreationTime()
+                + " | -> instance creation time: " + temp.getInstanceCreationTime()
+                + " | -> whole real time: " + temp.getWholeTimeForAllRealTimes()
+        );
+    }
+
+    private static boolean isOperationRunning(Thread[] threads) {
+
+        boolean running = false;
+
+        for (Thread thread : threads) {
+            if (thread.isAlive()) {
+                running = true;
+                break;
+            }
+        }
+
+        return running;
+    }
+
+}
+
+/*
+
+Done!! Print Your Tarakan - List:
+[007: 663 ms.]
+[003: 666 ms.]
+[006: 703 ms.]
+[002: 718 ms.]
+[008: 722 ms.]
+[005: 760 ms.]
+[004: 767 ms.]
+[009: 783 ms.]
+[0010: 787 ms.]
+[001: 796 ms.]
+
+Tarakan: 007 won wit the time: 663
+
+*/
---
+++ 21_02_08_tarakans_next/src/MyTarakanThread.java
@@ -0,0 +1,143 @@
+import java.util.Date;
+import java.util.List;
+import java.util.Objects;
+
+public class MyTarakanThread extends Thread implements Comparable<MyTarakanThread> {
+
+    private static int callCount = 0;
+    private static long classCreationTime = 0;
+
+    public static long getClassCreationTime() {
+        return classCreationTime;
+    }
+
+    static {
+        final long classCreationTime = new Date().getTime();
+        setClassCreationTime(classCreationTime);
+    }
+
+    {
+        callCount += 1;
+    }
+
+    private final List<MyTarakanThread> listParticipants;
+    private final int shortiesTime = 50;
+    private final int longestTime = 100;
+    private final int minutesCount = 10;
+    private final String nickname;
+    private final int commonNumber;
+    private final IntRandomNumberGenerator timeSequenceGenerator;
+    private final long instanceCreationTime;
+    private int wholeTimeForAllTimes;
+    private int wholeTimeForAllRealTimes;
+
+    public MyTarakanThread(List<MyTarakanThread> masterList, String nickname) {
+        this.listParticipants = masterList;
+        this.nickname = nickname;
+        this.commonNumber = getCallCount();
+        this.timeSequenceGenerator = new IntRandomNumberGenerator(shortiesTime, longestTime);
+        this.instanceCreationTime = new Date().getTime();
+        this.wholeTimeForAllTimes = 0;
+        this.wholeTimeForAllRealTimes = 0;
+    }
+
+    public void setWholeTimeForAllRealTimes(int wholeTimeForAllRealTimes) {
+        this.wholeTimeForAllRealTimes = wholeTimeForAllRealTimes;
+    }
+
+    public int getWholeTimeForAllRealTimes() {
+        return wholeTimeForAllRealTimes;
+    }
+
+    public long getInstanceCreationTime() {
+        return instanceCreationTime;
+    }
+
+    int getWholeTimeForAllTimes() {
+        return wholeTimeForAllTimes;
+    }
+
+    void setWholeTimeForAllTimes(int wholeTimeForAllTimes) {
+        this.wholeTimeForAllTimes = wholeTimeForAllTimes;
+    }
+
+    static void setClassCreationTime(long classCreationTime) {
+        MyTarakanThread.classCreationTime = classCreationTime;
+    }
+
+    static int getCallCount() {
+        return callCount;
+    }
+
+    String getNickname() {
+        return nickname;
+    }
+
+    int getCommonNumber() {
+        return commonNumber;
+    }
+
+    @Override
+    public void run() {
+
+        for (int i = 0; i < this.minutesCount; ++i) {
+
+            randomWait();
+
+            System.out.println(" ----> " + this.getWholeTimeForAllTimes());
+            int temp = timeSequenceGenerator.nextInt();
+            System.out.println(" --> " + temp);
+
+            this.setWholeTimeForAllTimes(this.getWholeTimeForAllTimes()
+                    + temp);
+
+            //Pause for 4 seconds
+            try {
+                Thread.sleep(temp);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        int result = (int) (System.currentTimeMillis() - this.getInstanceCreationTime());
+        this.setWholeTimeForAllRealTimes(result);
+        listParticipants.add(this);
+    }
+
+    private void randomWait() {
+        try {
+            Thread.currentThread();
+            Thread.sleep((long) (10000 * Math.random()));
+        } catch (InterruptedException x) {
+        }
+    }
+
+    @Override
+    public int compareTo(MyTarakanThread other) {
+        return this.getWholeTimeForAllRealTimes() - other.getWholeTimeForAllRealTimes();
+    }
+
+    @Override
+    public String toString() {
+        return "MyCharThread{" +
+                "times=" + minutesCount +
+                ", nickname='" + nickname + '\'' +
+                ", commonNumber=" + commonNumber +
+                ", timeSequenceGenerator=" + timeSequenceGenerator +
+                ", wholeTimeForAllTimes=" + wholeTimeForAllTimes +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof MyTarakanThread)) return false;
+        MyTarakanThread that = (MyTarakanThread) o;
+        return shortiesTime == that.shortiesTime && longestTime == that.longestTime && minutesCount == that.minutesCount && getCommonNumber() == that.getCommonNumber() && getInstanceCreationTime() == that.getInstanceCreationTime() && getWholeTimeForAllTimes() == that.getWholeTimeForAllTimes() && getWholeTimeForAllRealTimes() == that.getWholeTimeForAllRealTimes() && Objects.equals(listParticipants, that.listParticipants) && Objects.equals(getNickname(), that.getNickname()) && Objects.equals(timeSequenceGenerator, that.timeSequenceGenerator);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(listParticipants, shortiesTime, longestTime, minutesCount, getNickname(), getCommonNumber(), timeSequenceGenerator, getInstanceCreationTime(), getWholeTimeForAllTimes(), getWholeTimeForAllRealTimes());
+    }
+}
