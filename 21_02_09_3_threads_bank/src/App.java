d7976368944da77ffcfa58f16c44982089b6d832
enextus <enextus@gmail.com>
2021-02-09 07:25:04 +0100

    upd

--- 21_02_09_3_threads_bank/src/App.java
+++ 21_02_09_3_threads_bank/src/App.java
@@ -6,13 +6,24 @@ public class App {
 
     public static void main(String[] args) throws InterruptedException {
 
+
+        Employee e1 = new Employee("tanya", 100, 200, false);
+        Employee e2 = new Employee("vasya", 200, 300, false);
+        Employee e3 = new Employee("petya", 200, 300, false);
+
+
+
+
+
+/*
         int count = 10;
 
         BankThread[] bankThreads = new BankThread[count];
         List<BankThread> listEmployee = new ArrayList<>();
 
         for (int i = 0; i < count; i++) {
-            bankThreads[i] = new BankThread(listEmployee, "00" + (i + 1));
+            //   bankThreads[i] = new BankThread(listEmployee, "00" + (i + 1));
+            bankThreads[i] = null;
             bankThreads[i].start();
         }
 
@@ -24,7 +35,7 @@ public class App {
         System.out.println("Done. Print Your employees - List:");
 
         Collections.sort(listEmployee);
-        BankThread temp = listEmployee.get(0);
+        BankThread temp = listEmployee.get(0);*/
 
 /*        for (BankThread item : listEmployee) {
             System.out.println("[" + item.getNickname() + ": "
