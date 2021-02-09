81adf3199c8a4f07f4a2b6305e85da0be5c36139
enextus <enextus@gmail.com>
2021-02-09 07:21:23 +0100

    upd

--- 21_02_09_3_threads_bank/src/Employee.java
+++ 21_02_09_3_threads_bank/src/Employee.java
@@ -4,11 +4,13 @@ public class Employee {
     private final int shortiesTime;
     private final int longestTime;
     private final IntRandomNumberGenerator sequenceGenerator;
+    private final boolean turboPossibility;
 
-    public Employee(String nickname, int shortiesTime, int longestTime) {
+    public Employee(String nickname, int shortiesTime, int longestTime, boolean turboPossibility) {
         this.nickname = nickname;
         this.shortiesTime = shortiesTime;
         this.longestTime = longestTime;
+        this.turboPossibility = turboPossibility;
         this.sequenceGenerator = new IntRandomNumberGenerator(shortiesTime, longestTime);
     }
 
