01ea01f7bf26276857d7e1d0e4e6524345d3ec05
enextus <enextus@gmail.com>
2021-02-09 08:21:11 +0100

    upd

--- 21_02_09_3_threads_bank/src/BankThread.java
+++ 21_02_09_3_threads_bank/src/BankThread.java
@@ -1,56 +1,22 @@
 
 import java.util.List;
 
-public class BankThread extends Thread implements Comparable<BankThread> {
+public class BankThread{
 
-    private final List<BankThread> bankThreadList;
-    private final List<Employee> listEmployee;
+    private final Employee employee;
     private final Integer threadName;
-    private final int numberOfCompletedContracts = 20;
-    private int wholeTimeForAllContracts;
 
     public BankThread(Employee employee, Integer threadNumber) {
-        this.listEmployee = listEmployee;
+        this.employee = employee;
         this.threadName = threadNumber;
-
-        //this.bankThreadList = bankThreadList;
-        this.bankThreadList = null;
-
-
-        this.wholeTimeForAllContracts = 0;
     }
 
-    int getWholeTimeForAllContracts() {
-        return wholeTimeForAllContracts;
+
+    public Employee getEmployee() {
+        return employee;
     }
 
-    void setWholeTimeForAllContracts(int wholeTimeForAllContracts) {
-        this.wholeTimeForAllContracts = wholeTimeForAllContracts;
+    public Integer getThreadName() {
+        return threadName;
     }
-
-    @Override
-    public void run() {
-
-            randomWait();
-
-        synchronized (this) {
-            bankThreadList.add(this);
-        }
-
-    }
-
-    private void randomWait() {
-        try {
-            Thread.currentThread();
-            Thread.sleep((long) (1000 * Math.random()));
-        } catch (InterruptedException x) {
-            System.out.println("Thread  interrupted.");
-        }
-    }
-
-/*    @Override
-    public int compareTo(BankThread other) {
-        return this.getWholeTimeForAllContracts() - other.getWholeTimeForAllContracts();
-    }*/
-
 }
--- 21_02_09_3_threads_bank/src/Work.java
+++ 21_02_09_3_threads_bank/src/Work.java
@@ -1,2 +1,52 @@
-public class Work {
+import java.util.List;
+
+public class Work extends Thread implements Comparable<BankThread> {
+
+    private int numberOfCompletedContracts = 0;
+    private int wholeTimeForAllContracts;
+    private final List<BankThread> bankThreadList;
+    private final List<Employee> listEmployee;
+
+
+    public Work(int numberOfCompletedContracts) {
+        this.numberOfCompletedContracts = numberOfCompletedContracts;
+        this.bankThreadList = null;
+    }
+
+    int getWholeTimeForAllContracts() {
+        return wholeTimeForAllContracts;
+    }
+
+
+
+    void setWholeTimeForAllContracts(int wholeTimeForAllContracts) {
+        this.wholeTimeForAllContracts = wholeTimeForAllContracts;
+    }
+
+
+
+    @Override
+    public void run() {
+
+        randomWait();
+
+        synchronized (this) {
+            bankThreadList.add(this);
+        }
+
+    }
+
+    private void randomWait() {
+        try {
+            Thread.currentThread();
+            Thread.sleep((long) (1000 * Math.random()));
+        } catch (InterruptedException x) {
+            System.out.println("Thread  interrupted.");
+        }
+    }
+    @Override
+    public int compareTo(BankThread other) {
+        return this.getWholeTimeForAllContracts() - other.getWholeTimeForAllContracts();
+
+    }
 }
