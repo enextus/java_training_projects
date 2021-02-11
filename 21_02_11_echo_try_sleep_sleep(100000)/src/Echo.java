0d9c3a4293e3ee3588bf64af4e0e66f8cc56e4f9
enextus <enextus@gmail.com>
2021-02-11 06:54:53 +0100

    upd

---
+++ 21_02_11_echo_try_sleep_sleep(100000)/src/Echo.java
@@ -0,0 +1,21 @@
+public class Echo implements Runnable {
+
+    private String message;
+
+    public Echo(String message) {
+        this.message = message;
+    }
+
+    @Override
+    public void run() {
+
+
+
+
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+}
