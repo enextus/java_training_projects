1109e97e74e9a3d6e6133576473316ea7e90b52b
enextus <enextus@gmail.com>
2021-02-11 07:08:15 +0100

    upd

--- 21_02_11_echo_try_sleep_sleep(100000)/src/Echo.java
+++ 21_02_11_echo_try_sleep_sleep(100000)/src/Echo.java
@@ -8,10 +8,16 @@ public class Echo implements Runnable {
 
     @Override
     public void run() {
+        while (true) {
 
-
-
-
+            // Some complex logic lasting long time
+            try {
+                Thread.sleep(100000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                System.out.println(message);
+            }
+        }
     }
 
     public void setMessage(String message) {
--- 21_02_11_echo_try_sleep_sleep(100000)/src/Main.java
+++ 21_02_11_echo_try_sleep_sleep(100000)/src/Main.java
@@ -1,6 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        // write your code here
+
+        Echo echo = new Echo("Hallo!");
+
+        Thread echoTh = new Thread(echo);
+        echoTh.start();
+
+
+        try (BufferedReader br = new BufferedReader((new InputStreamReader(System.in)))) {
+
+            String line;
+
+            while ((line = br.readLine()) !=null && !line.equals("exit")){
+
+                echo.setMessage(line);
+                echoTh.interrupt();
+            }
+
+        }
+
     }
 }
