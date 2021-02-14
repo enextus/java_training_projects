4118026bdca2ef3a354195fbf6db364e75a5acee
enextus <enextus@gmail.com>
2021-02-14 15:27:43 +0100

    upd

--- 21_02_14_threads_testing_vorlesung/src/App.java
+++ 21_02_14_threads_testing_vorlesung/src/App.java
@@ -1,21 +1,22 @@
 public class App {
     public static void main(String[] args) throws InterruptedException {
-        //
 
+        System.out.println("MAIN: Thread.currentThread(): " + Thread.currentThread());
+        System.out.println("MAIN: Thread.activeCount(): " + Thread.activeCount());
 
         EchoRunnable echoRunnable = new EchoRunnable("www");
 
+        Thread myThread = new Thread(echoRunnable);
 
-        for (int i = 0; i < 50; i++) {
+        myThread.start();
+
+        for (int i = 0; i < 10; i++) {
 
             Thread.sleep(250);
 
             System.out.println(i + " ->>>    A");
             System.out.println("");
         }
-
-
-
 
     }
 
--- 21_02_14_threads_testing_vorlesung/src/EchoRunnable.java
+++ 21_02_14_threads_testing_vorlesung/src/EchoRunnable.java
@@ -1,26 +1,38 @@
 public class EchoRunnable implements Runnable {
 
-    {
-        System.out.println("1 -> " + this.getMessage());
-    }
-
     private String message;
+
+/*    {
+        System.out.println("->> hier wird { im EchoRunnable } abgearbeitet");
+        System.out.println("");
+    }*/
 
     public EchoRunnable(String message) {
         this.message = message;
         this.setMessage(this.getMessage());
+        System.out.println("-> (constructor of EchoRunnable): " + this.getMessage());
+        System.out.println("");
     }
 
     @Override
     public void run() {
 
-
         System.out.println("Thread.currentThread(): " + Thread.currentThread());
-        System.out.println("Thread.class: " + Thread.class);
         System.out.println("Thread.activeCount(): " + Thread.activeCount());
 
-/*        while (true) {
+        for (int i = 50; i < 60; i++) {
 
+            try {
+                Thread.sleep(200);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            System.out.println(i + " ->>>    B");
+            System.out.println("");
+        }
+
+/*        while (true) {
             // Some complex logic lasting long time
             try {
 
@@ -33,7 +45,6 @@ public class EchoRunnable implements Runnable {
                 e.printStackTrace();
                 System.out.println(message);
             }
-
         }*/
 
     }
