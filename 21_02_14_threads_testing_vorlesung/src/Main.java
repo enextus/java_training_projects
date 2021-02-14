10e7da1adca695b1af0fa7c2dbc439a543ce82f0
enextus <enextus@gmail.com>
2021-02-14 13:39:36 +0100

    upd

---
+++ 21_02_14_threads_testing_vorlesung/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+
+        for (int i = 0; i < 100; i++) {
+
+            for (int j = 0; j < 1000000; i++) {
+                // do nothing
+            }
+            System.out.println("print");
+        }
+    }
+}
