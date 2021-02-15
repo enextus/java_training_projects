73659bafa458f036b2d5bb79226668fd054f2d25
enextus <enextus@gmail.com>
2021-02-15 04:59:29 +0100

    upd

--- 21_02_14_2_part_threads_testing_vorlesung/src/Main.java
+++ 21_02_14_2_part_threads_testing_vorlesung/src/Main.java
@@ -1,19 +1,9 @@
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
-        // write your code here
 
-        for (int i = 0; i < 50; i++) {
-
-            for (int j = 0; j < 1000000000L; j++) {
-                // do nothing
-            }
-
-            //Thread.sleep(250);
-
-            System.out.println(i + " ->>>    A");
-            System.out.println("");
-        }
+        // death look MAIN
+        Thread.currentThread().join();
     }
 
 }
