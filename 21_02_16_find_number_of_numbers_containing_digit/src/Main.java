443cfd967d4578116e2b7b13d6b342e76e1e0cf9
enextus <enextus@gmail.com>
2021-02-16 03:12:22 +0100

    upd

--- 21_02_16_find_number_of_numbers_containing_digit/src/Main.java
+++ 21_02_16_find_number_of_numbers_containing_digit/src/Main.java
@@ -1,6 +1,34 @@
 public class Main {
 
+    /**
+     * There is a range of numbers from a to b inclusive.
+     * Find the number of numbers containing the digit 1.
+     */
     public static void main(String[] args) {
-	// write your code here
+
+        int n;
+        n = 0;
+
+        int count = 0;
+        for (int i = 2; i <= n; i++) { // Можем начать с 2
+            count += numberOf2s(i);
+        }
+
     }
+
+    private static int numberOf2s(int n) {
+        int count = 0;
+        while (n > 0) {
+            if (n % 10 == 2) {
+                count++;
+            }
+            n = n / 10;
+        }
+        return count;
+    }
+
+
+
+
+
 }
