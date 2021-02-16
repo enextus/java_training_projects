0aa0cba9f7342af125b3803b746babe6138928bf
enextus <enextus@gmail.com>
2021-02-16 09:58:56 +0100

    upd

---
+++ 21_02_16_find_number_of_numbers_containing_digit/src/FindNumberOfDigitOneInRangeTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FindNumberOfDigitOneInRangeTest {
+
+    @Test
+    void startNumberOfNumbers() {
+    }
+
+    @Test
+    void calculateNumberOfNumbers() {
+    }
+
+    @Test
+    void findTheNumberOfDigitsOne() {
+    }
+}
