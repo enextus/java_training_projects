91f1829545597059f4299d512515da4b4e92d6ef
enextus <enextus@gmail.com>
2021-02-16 16:48:43 +0100

    upd

--- 21_02_16_find_number_of_numbers_containing_digit/src/FindNumberOfDigitOneInRangeTest.java
+++ 21_02_16_find_number_of_numbers_containing_digit/src/FindNumberOfDigitOneInRangeTest.java
@@ -1,19 +1,37 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class FindNumberOfDigitOneInRangeTest {
 
     FindNumberOfDigitOneInRange calculateNumber = new FindNumberOfDigitOneInRange();
 
     @Test
-    void test_countTheNumberOfDigitsOne() {
+    void test_count_TheNumberOfDigitsOne_0_0_() {
+        assertEquals(0, calculateNumber.countTheNumberOfDigitsOne(0));
     }
 
     @Test
-    void startNumberOfNumbers() {
+    void test_count_TheNumberOfDigitsOne_1_1_() {
+        assertEquals(1, calculateNumber.countTheNumberOfDigitsOne(1));
     }
 
     @Test
-    void calculateNumberOfNumbers() {
+    void test_count_TheNumberOfDigitsOne_2_min_0_() {
+        assertEquals(0, calculateNumber.countTheNumberOfDigitsOne(2));
+    }
+
+    @Test
+    void test_count_TheNumberOfDigitsOne_10_min_1_() {
+        assertEquals(1, calculateNumber.countTheNumberOfDigitsOne(10));
+    }
+
+    @Test
+    void test_get_NumberOfNumbers() {
+    }
+
+    @Test
+    void test_calc_NumberOfNumbers() {
     }
 
 }
