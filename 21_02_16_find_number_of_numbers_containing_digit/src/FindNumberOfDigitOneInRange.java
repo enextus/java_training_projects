f09d2b13b4a7131cf78742878a62de0d7f525e06
enextus <enextus@gmail.com>
2021-02-17 07:01:25 +0100

    upd

--- 21_02_16_find_number_of_numbers_containing_digit/src/FindNumberOfDigitOneInRange.java
+++ 21_02_16_find_number_of_numbers_containing_digit/src/FindNumberOfDigitOneInRange.java
@@ -61,7 +61,7 @@ public class FindNumberOfDigitOneInRange {
      * @param rC right range corner
      * @return return a result
      */
-    public int calcNumbers(int lC, int rC) {
+    int calcNumbers(int lC, int rC) {
         int count = 0;
         for (int i = lC; i <= rC; i++)
             count += calcDigitsOne(i);
