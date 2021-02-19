d495b5d0e9860be2b3225850bcc2a9770d6d580d
enextus <enextus@gmail.com>
2021-02-19 09:03:52 +0100

    upd

--- 21_02_18_int_supplier_bipredisate/src/Main.java
+++ 21_02_18_int_supplier_bipredisate/src/Main.java
@@ -2,6 +2,14 @@ public class Main {
 
     public static void main(String[] args) {
 
+        final int a;
+        final int b;
+        final int c;
+
+        a = 2;
+        b = 5;
+        c = 10;
+
 
     }
 }
