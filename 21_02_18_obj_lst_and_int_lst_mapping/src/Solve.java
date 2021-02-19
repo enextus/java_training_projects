06753500f49a15ffef8704caf5ba4f791f5bccac
enextus <enextus@gmail.com>
2021-02-19 07:07:27 +0100

    upd

---
+++ 21_02_18_obj_lst_and_int_lst_mapping/src/Solve.java
@@ -0,0 +1,21 @@
+import java.util.List;
+import java.util.Map;
+
+public class Solve {
+
+    List<Cut> cuts;
+    List<Integer> numbers;
+
+    public Solve(List<Cut> cuts, List<Integer> numbers) {
+        this.cuts = cuts;
+        this.numbers = numbers;
+    }
+
+    public Map<Integer, Integer> solve(List<Cut> cuts, List<Integer> numbers){
+
+
+
+        return null;
+    }
+
+}
