8ab96624299317f61ceaf457ac27c42c6ad9b0d5
enextus <enextus@gmail.com>
2021-02-19 06:28:26 +0100

    upd

--- 21_02_18_obj_lst_and_int_lst_mapping/src/Main.java
+++ 21_02_18_obj_lst_and_int_lst_mapping/src/Main.java
@@ -1,6 +1,12 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+
+        Cut cut_01 = new Cut(-10, -7);
+
+        System.out.println(cut_01.getLeft());
+        System.out.println(cut_01.getRight());
     }
+
 }
