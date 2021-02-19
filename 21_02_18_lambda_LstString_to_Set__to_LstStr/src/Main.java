8eba34ba94d6f4fff0e122c090e626286fed4ac4
enextus <enextus@gmail.com>
2021-02-19 08:09:49 +0100

    upd

--- 21_02_18_lambda_LstString_to_Set__to_LstStr/src/Main.java
+++ 21_02_18_lambda_LstString_to_Set__to_LstStr/src/Main.java
@@ -1,6 +1,24 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Function;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        // write your code here
+
+        List<String> lstStr = Arrays.asList("abc", "abc", "abc", "bcd", "bcd", "efg", "efg");
+
+        System.out.println(lstStr);
+
+        Function<List<String>, List<String>> resLst = (List<String> lst) -> {
+
+            System.out.println("1.");
+            System.out.println("2.");
+
+            return lst;
+        };
+
+        resLst.apply(lstStr);
     }
 }
