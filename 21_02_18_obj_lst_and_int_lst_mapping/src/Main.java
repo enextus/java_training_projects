4f0e71140c59055711b97cf07db979b153f2df30
enextus <enextus@gmail.com>
2021-02-19 06:43:44 +0100

    upd

--- 21_02_18_obj_lst_and_int_lst_mapping/src/Main.java
+++ 21_02_18_obj_lst_and_int_lst_mapping/src/Main.java
@@ -1,18 +1,16 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
 
-        Cut cut_01 = new Cut(-10, -7);
-
-        System.out.println(cut_01.getLeft());
-        System.out.println(cut_01.getRight());
-
-        List<Cut> lstCut = new ArrayList<>();
-
+        List<Cut> lstCut = Arrays.asList(
+                new Cut(-100, -90),
+                new Cut(-5, 5),
+                new Cut(40, 80)
+        );
 
     }
 
