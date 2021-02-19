a792b341c19eaa67c6be70be21fa60e100fbaccf
enextus <enextus@gmail.com>
2021-02-19 14:08:48 +0100

    upd

--- 21_02_19_stream_factorial/src/FactorialTest.java
+++ 21_02_19_stream_factorial/src/FactorialTest.java
@@ -1,11 +1,23 @@
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class FactorialTest {
 
+    Factorial<Integer> factorial = new Factorial();
+
     @Test
-    void test_calcFact_() {
+    void test_calcFact_1_1() {
+        assertEquals(1, factorial.calcFact(1));
+    }
 
-        //num
+    @Test
+    void test_calcFact_5_120() {
+        assertEquals(120, factorial.calcFact(5));
+    }
 
+    @Test
+    void test_calcFact_100_() {
+        assertEquals(3628800, factorial.calcFact(10));
     }
 }
--- 21_02_19_stream_factorial/src/Main.java
+++ 21_02_19_stream_factorial/src/Main.java
@@ -2,8 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        // ...
-
         Factorial<Integer> factorial = new Factorial();
 
         System.out.println(factorial.calcFact(5));
