65db314fdbceba23e8ea06aa2e8f86eeaf3abe38
enextus <enextus@gmail.com>
2021-02-19 08:21:44 +0100

    upd

--- 21_02_18_lambda_LstString_to_Set__to_LstStr/src/Main.java
+++ 21_02_18_lambda_LstString_to_Set__to_LstStr/src/Main.java
@@ -5,19 +5,19 @@ public class Main {
 
     public static void main(String[] args) {
 
-        List<String> lstStr = Arrays.asList("abc", "abc", "abc", "bcd", "bcd", "efg", "efg");
+        List<String> initLstStr = Arrays.asList("abc", "abc", "abc", "bcd", "bcd", "efg", "efg");
 
         Function<List<String>, List<String>> resLst = (List<String> lst) -> {
 
-            Set<String> resSet = new HashSet<String>(lst);
-            List<String> result = new ArrayList<String>();
+            Set<String> resSet = new HashSet<>(lst);
+            List<String> result = new ArrayList<>();
 
             result.addAll(resSet);
 
             return (List<String>) result;
         };
 
-        System.out.println(resLst.apply(lstStr));
+        System.out.println(resLst.apply(initLstStr));
     }
 
 }
