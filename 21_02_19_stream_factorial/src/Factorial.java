9eb3e0a804a71caf7e7bb576c3b4a0a0bd25de8f
enextus <enextus@gmail.com>
2021-02-22 10:47:14 +0100

    upd

--- 21_02_19_stream_factorial/src/Factorial.java
+++ 21_02_19_stream_factorial/src/Factorial.java
@@ -4,7 +4,7 @@ public class Factorial<Integer> {
 
     public int calcFact(int num) {
         int res;
-        res = IntStream.rangeClosed(2, num).reduce(1, (x, y) -> x * y);
+        res = IntStream.rangeClosed(1, num).reduce(1, (x, y) -> x * y);
 
         return res;
     }
