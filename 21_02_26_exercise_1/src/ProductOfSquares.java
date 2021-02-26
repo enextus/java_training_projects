968a7a9d704567efe833173f0bb0f087b33b5677
enextus <enextus@gmail.com>
2021-02-26 09:36:23 +0100

    upd

--- 21_02_26_exercise_1/src/ProductOfSquares.java
+++ 21_02_26_exercise_1/src/ProductOfSquares.java
@@ -1,11 +1,12 @@
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class ProductOfSquares {
     public static void main(String[] args) {
 
 
-
         // BigInteger.valueOf(1)
-        // BigInteger.valueOf((long) i * i)
-
         // s = s.multiply(BigInteger.valueOf((long) i * i));
     }
 }
