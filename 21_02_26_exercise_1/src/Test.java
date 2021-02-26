7df2905d69e4d50176b370adcab5fb2b4f147c05
enextus <enextus@gmail.com>
2021-02-26 08:40:05 +0100

    upd

--- 21_02_26_exercise_1/src/Test.java
+++ 21_02_26_exercise_1/src/Test.java
@@ -1,16 +1,21 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Test {
     public static void main(String[] args) {
 
         List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 5, 12, 34);
 
-        Long res2 = integerList.stream()
+        Long res = (Long) integerList.stream()
                 .mapToLong(integer -> (long) integer * integer)
-                .reduce(0, Long::sum);
+                //   .reduce(0, Long::sum)
+                .collect(Collectors.summingInt(Long::intValue));
+        ;
 
-        System.out.println(res2);
+        //  .collect(Collectors.summingInt(Long::intValue));
+
+        System.out.println(res);
 
     }
 
