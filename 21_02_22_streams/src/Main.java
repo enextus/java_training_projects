5b40cef9befdd56ff02ad5808ab12c7ac564b84f
enextus <enextus@gmail.com>
2021-02-26 07:06:17 +0100

    upd

--- 21_02_22_streams/src/Main.java
+++ 21_02_22_streams/src/Main.java
@@ -1,32 +1,27 @@
-import java.util.Arrays;
-import java.util.List;
 import java.util.stream.IntStream;
 
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
 
-        // IntService intService = new IntService();
+        IntService intService = new IntService();
 
-        // System.out.println(intService.isPrime(1));
-        // System.out.println(intService.isPrime(7));
+        System.out.println(intService.isPrime(1));
+        System.out.println(intService.isPrime(7));
 
-        // IntStream intStream1 = IntStream.of(1, 30);
-        // IntStream intStream2 = IntStream.of(6, 49);
+        IntStream intStream1 = IntStream.of(1, 30);
+        IntStream intStream2 = IntStream.of(6, 49);
 
-        // System.out.println(intService.compose(intStream1, intStream2));
+        System.out.println(intService.compose(intStream1, intStream2));
 
 
-        List<String> mList = Arrays.asList("aa1","cc2", "cc1", "aa2", "bb1");
+/*        List<String> mList = Arrays.asList("aa1","cc2", "cc1", "aa2", "bb1");
 
-        mList
-                .stream()
+        mList.stream()
                 .filter(s -> s.startsWith("a"))
                 .map(String::toUpperCase)
                 .sorted()
-                .forEach(System.out::println);
-
+                .forEach(System.out::println);*/
     }
 
 }
