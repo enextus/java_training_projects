1291e7c5ceccaf6eae98432c92eebf554b44c050
enextus <enextus@gmail.com>
2021-02-26 08:20:05 +0100

    upd

--- 21_02_26_exercise_1/src/Test.java
+++ 21_02_26_exercise_1/src/Test.java
@@ -7,7 +7,7 @@ public class Test {
         List<Integer> num = Arrays.asList(1, 2, 3, 4, 5, 12, 34);
 
         Long res = num.stream()
-                .mapToLong(n -> n * n)
+                .mapToLong(n -> (long) n * n)
                 .reduce(0, Long::sum);
 
         System.out.println(res);
