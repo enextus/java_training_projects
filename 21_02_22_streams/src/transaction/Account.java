77d957a15c8af8e700b6db9a0d83c5384b5f009b
enextus <enextus@gmail.com>
2021-02-26 06:40:45 +0100

    upd

---
+++ 21_02_22_streams/src/transaction/Account.java
@@ -0,0 +1,38 @@
+package transaction;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Account {
+    private final String uuid;
+    private long balance;
+    private final List<Transaction> transactions;
+
+    public Account(String uuid, long balance) {
+        this.uuid = uuid;
+        this.balance = balance;
+        this.transactions = new ArrayList<>();
+    }
+
+    public void setBalance(long balance) {
+        this.balance = balance;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public long getBalance() {
+        return balance;
+    }
+
+    public void addTransaction(Transaction transaction) {
+        transactions.add(transaction);
+    }
+
+    public List<Transaction> getTransactions() {
+        return Collections.unmodifiableList(transactions);
+    }
+
+}
