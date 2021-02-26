5f8197b9f51b136afdeb542ab9a34a34c8f79d4a
enextus <enextus@gmail.com>
2021-02-26 06:47:33 +0100

    upd

--- 21_02_22_streams/src/transaction/Account.java
+++ 21_02_22_streams/src/transaction/Account.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.List;
 
 public class Account {
+
     private final String uuid;
     private long balance;
     private final List<Transaction> transactions;
