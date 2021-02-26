165833a2fe265d881449b8050907ca089d967cfc
enextus <enextus@gmail.com>
2021-02-26 08:13:21 +0100

    upd

---
+++ 21_02_26_exercise_1/src/Test.java
@@ -0,0 +1,17 @@
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Test {
+    public static void main(String[] args) {
+
+        List<Integer> num = Arrays.asList(1, 2, 3, 4, 5);
+
+        Integer result = num.stream()
+                .map(n -> n * n)
+                .reduce(0, Integer::sum);
+
+        System.out.println(result);
+    }
+
+}
