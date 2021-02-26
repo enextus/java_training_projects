a387691d595a342371013cc4775614ddd94e91db
enextus <enextus@gmail.com>
2021-02-26 09:20:11 +0100

    upd

--- 21_02_26_exercise_1/src/SumOfSquares.java
+++ 21_02_26_exercise_1/src/SumOfSquares.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-public class Test {
+public class SumOfSquares {
     public static void main(String[] args) {
 
         List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 5);
