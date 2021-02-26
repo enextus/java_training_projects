8d854b4bf31c292a2df2940921a03cfc4714505b
enextus <enextus@gmail.com>
2021-02-26 07:24:30 +0100

    upd

--- 21_02_22_streams/src/Main.java
+++ 21_02_22_streams/src/Main.java
@@ -1,10 +1,35 @@
-import java.util.stream.IntStream;
+import transaction.Account;
+import transaction.AccountService;
+import transaction.State;
+import transaction.Transaction;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
 
-        IntService intService = new IntService();
+        AccountService accountService_1 = new AccountService();
+
+        List<Account> accounts = new ArrayList<>();
+
+
+        Transaction transaction_1 = new Transaction("uuid_1", State.CANCELLED, 10L);
+
+        Account account_1 = new Account("uuid_1", 1000L);
+
+        accounts.add(account_1);
+
+        System.out.println(accounts.size());
+
+        System.out.println(account_1.getBalance());
+
+
+        System.out.println(account_1.getTransactions());
+        System.out.println(accountService_1.getSumAllCancelledTransactions(accounts));
+
+/*        IntService intService = new IntService();
 
         System.out.println(intService.isPrime(1));
         System.out.println(intService.isPrime(7));
@@ -12,7 +37,7 @@ public class Main {
         IntStream intStream1 = IntStream.of(1, 30);
         IntStream intStream2 = IntStream.of(6, 49);
 
-        System.out.println(intService.compose(intStream1, intStream2));
+        System.out.println(intService.compose(intStream1, intStream2));*/
 
 
 /*        List<String> mList = Arrays.asList("aa1","cc2", "cc1", "aa2", "bb1");
@@ -22,6 +47,7 @@ public class Main {
                 .map(String::toUpperCase)
                 .sorted()
                 .forEach(System.out::println);*/
+
     }
 
 }
