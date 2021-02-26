7a8ce0085d5f5e63d95ff403dc0cb51247e59021
enextus <enextus@gmail.com>
2021-02-26 08:15:16 +0100

    upd

--- 21_02_26_exercise_1/src/Test.java
+++ 21_02_26_exercise_1/src/Test.java
@@ -5,13 +5,13 @@ import java.util.List;
 public class Test {
     public static void main(String[] args) {
 
-        List<Integer> num = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> num = Arrays.asList(1, 2, 3, 4, 5, 12, 34);
 
-        Integer result = num.stream()
+        Integer res = num.stream()
                 .map(n -> n * n)
                 .reduce(0, Integer::sum);
 
-        System.out.println(result);
+        System.out.println(res);
     }
 
 }
