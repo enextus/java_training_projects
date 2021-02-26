c1102ec98bf14dd3de6c1185615963ef2cfbfb7d
enextus <enextus@gmail.com>
2021-02-26 08:33:20 +0100

    upd

--- 21_02_26_exercise_1/src/Test.java
+++ 21_02_26_exercise_1/src/Test.java
@@ -4,18 +4,9 @@ import java.util.List;
 public class Test {
     public static void main(String[] args) {
 
-        List<Integer> num = Arrays.asList(1, 2, 3, 4, 5, 12, 34);
+        List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 5, 12, 34);
 
-        Long res = num.stream()
-                .mapToLong(integer -> (long) integer * integer)
-                .reduce(0, Long::sum);
-
-        System.out.println(res);
-
-
-        List<Integer> num2 = Arrays.asList(1, 2, 3, 4, 5, 12, 34);
-
-        Long res2 = num2.stream()
+        Long res2 = integerList.stream()
                 .mapToLong(integer -> (long) integer * integer)
                 .reduce(0, Long::sum);
 
