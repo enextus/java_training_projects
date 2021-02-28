04571d4547153636082b4d579786b6c0ef724ba5
enextus <enextus@gmail.com>
2021-02-28 21:53:00 +0100

    upd

--- 21_02_26_exercise_1/src/SumOfSquares.java
+++ 21_02_26_exercise_1/src/SumOfSquares.java
@@ -1,5 +1,6 @@
 import java.util.Arrays;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
 public class SumOfSquares {
@@ -7,8 +8,12 @@ public class SumOfSquares {
 
         List<Integer> integerLst = Arrays.asList(1, 2, 3, 4, 5, 6, 7);
 
+        AtomicInteger count = new AtomicInteger();
         Long res = integerLst.stream()
                 .map((integer) -> {
+
+                            count.getAndIncrement();
+                            System.out.println("count: " + count + ", integer: " + integer + ", i*i: " + (integer * integer));
 
                             return integer * integer;
                         }
