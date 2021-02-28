86dbb07b4ff3db4897b96f8c3fce2fb11297da2c
enextus <enextus@gmail.com>
2021-02-28 22:55:03 +0100

    upd

---
+++ 21_02_28_normalise_between_0_and_1/src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+
+    public static void main(String[] args) {
+        // write your code here
+        // normalise
+
+
+    }
+
+    public static float normalise(float inValue, float min, float max) {
+        return (inValue - min) / (max - min);
+    }
+
+}
