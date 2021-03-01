460742e852b6a9e202dc417a151b5439bbf4e7a7
enextus <enextus@gmail.com>
2021-03-01 07:24:25 +0100

    upd

--- 21_03_01_MedianCollector/src/Main.java
+++ 21_03_01_MedianCollector/src/Main.java
@@ -1,6 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        String[] programming_languages = new String[]{"language:java", "os:linux", "editor:emacs"};
+
+        List<String> strLst = new ArrayList<>();
+
+        strLst.add("java");
+        strLst.add("python");
+        strLst.add("nodejs");
+        strLst.add("ruby");
+
+        // List<Integer> integerLst = Arrays.asList(-10, -4, 9, 12, 50);
+
+        MedianCollector medianCollector = new MedianCollector();
+        Integer res = strLst.stream().map(String::length).collect(medianCollector);
+
+        System.out.println(res);
+
     }
+
 }
