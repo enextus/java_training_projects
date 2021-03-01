d1056047ef42c33a24d3864594ccb000853cfbde
enextus <enextus@gmail.com>
2021-03-01 08:34:49 +0100

    upd

--- 21_02_28_normalise_between_0_and_1/src/Main.java
+++ 21_02_28_normalise_between_0_and_1/src/Main.java
@@ -1,30 +1,13 @@
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
         // normalise
 
-
-        NormUtil normUtil = new NormUtil(150., -10., 1, 0);
+        NormUtil normUtil = new NormUtil(150.23, -10.12, 1.45, 0.01);
 
         System.out.println(normUtil);
 
         System.out.println(normUtil.normalize(-5.12));
-
     }
-/*
-    public static float normalise(float inValue, float min, float max) {
-        return (inValue - min) / (max - min);
-    }
-
-
-     * Normalize x.
-     * @param x The value to be normalized.
-     * @return The result of the normalization.
-    public double normalize(double x) {
-        return ((x - dataLow)
-                / (dataHigh - dataLow))
-                * (normalizedHigh - normalizedLow) + normalizedLow;
-    }*/
 
 }
---
+++ 21_02_28_normalise_between_0_and_1/src/NormCollector.java
@@ -0,0 +1,58 @@
+import java.util.ArrayList;
+import java.util.EnumSet;
+import java.util.Set;
+import java.util.function.BiConsumer;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.function.Supplier;
+import java.util.stream.Collector;
+
+public class NormCollector implements Collector<Double, ArrayList<Double>, Double> {
+    @Override
+    public Supplier<ArrayList<Double>> supplier() {
+        return ArrayList::new; // create a new ArrayList Object, temporary container for data store
+    }
+
+    @Override
+    public BiConsumer<ArrayList<Double>, Double> accumulator() {
+        return ArrayList::add; // add an element to our ArrayList
+    }
+
+    @Override
+    public BinaryOperator<ArrayList<Double>> combiner() {
+        return (l, r) -> {
+            l.addAll(r);
+            return l;
+        };
+    }
+
+    @Override
+    public Function<ArrayList<Double>, Double> finisher() {
+        return doubleStream -> {
+
+            long size = doubleStream.size();
+
+            if (size % 2 == 0) {
+
+                return doubleStream
+                        .stream()
+                        .mapToDouble(i -> i)
+                        .average()
+                        .getAsDouble();
+            }
+
+            return doubleStream
+                    .stream()
+                    .skip(size % 2 + 2)
+                    .findFirst()
+                    .orElse(null);
+        };
+
+    }
+
+    @Override
+    public Set<Characteristics> characteristics() {
+        return EnumSet.of(Characteristics.CONCURRENT);
+    }
+
+}
