844edadcec731e77055acb5449534f1240a1b8cc
enextus <enextus@gmail.com>
2021-03-01 10:39:29 +0100

    upd

--- 21_02_28_normalise_between_0_and_1/src/NormCollector.java
+++ 21_02_28_normalise_between_0_and_1/src/NormCollector.java
@@ -41,7 +41,7 @@ public class NormCollector implements Collector<Double, ArrayList<Double>, Doubl
 
             return ((doubleArrayList
                     .stream()
-                    .reduce(  0,(x ->  x - dataLow) / (dataHigh - dataLow)) * (normalizedHigh - normalizedLow) + normalizedLow);
+                    .reduce(  0.,(currentRes, currentNum )->  currentRes - dataLow) / (dataHigh - dataLow) * (normalizedHigh - normalizedLow) + normalizedLow));
 
         };
 
