8e8b46acd6d368edf19e013b3ebc670e14275a78
enextus <enextus@gmail.com>
2021-03-04 00:47:32 +0100

    upd

--- 21_03_02_date/src/Main.java
+++ 21_03_02_date/src/Main.java
@@ -1,5 +1,5 @@
-import java.time.LocalDate;
-import java.time.Period;
+import java.time.*;
+import java.time.temporal.ChronoField;
 import java.time.temporal.ChronoUnit;
 
 public class Main {
@@ -25,7 +25,32 @@ public class Main {
 
         System.out.println(tomorrow);
 
-
+        System.out.println(now.getDayOfWeek());
         System.out.println(tomorrow);
+
+        System.out.println(now.isAfter(tomorrow));
+        System.out.println(now.isBefore(tomorrow));
+
+        //LocalTime
+        LocalTime currentTime = LocalTime.now();
+        LocalTime nextHour = currentTime.plus(Duration.ofHours(1));
+
+        nextHour = currentTime.plus(1, ChronoUnit.HOURS);   // вернет новый локалтиме  ДЛЯ + И -
+
+        System.out.println(nextHour);
+
+        nextHour = currentTime.plusHours(1);
+        System.out.println(nextHour);
+
+
+        // LocalDateTime
+        LocalDateTime today = LocalDateTime.now();
+        LocalDateTime todayParsed = LocalDateTime.parse("2021-03-02T12:26:24");
+
+        System.out.println(today);
+        System.out.println(todayParsed);
+
+        System.out.println(todayParsed.get(ChronoField.DAY_OF_YEAR));
+
     }
 }
