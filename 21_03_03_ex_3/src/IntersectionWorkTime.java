0ac077179d96a6bd2bcad0ff861fd054edb71e5d
enextus <enextus@gmail.com>
2021-03-04 05:22:57 +0100

    upd

--- 21_03_03_ex_3/src/IntersectionWorkTime.java
+++ 21_03_03_ex_3/src/IntersectionWorkTime.java
@@ -1,2 +1,13 @@
+import java.time.LocalTime;
+import java.time.ZoneId;
+
 public class IntersectionWorkTime {
+
+    public timeOne;
+    public timeTwo;
+
+    public int intersectWorkingTime(LocalTime start1, int hours, ZoneId zone1, LocalTime start2, int hours2, ZoneId zone2) {
+
+        return
+    }
 }
