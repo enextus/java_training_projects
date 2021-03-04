26f804ce6f909e49e74ccd305a7d912e221b19ba
enextus <enextus@gmail.com>
2021-03-04 05:51:01 +0100

    upd

--- 21_03_03_ex_3/src/Main.java
+++ 21_03_03_ex_3/src/Main.java
@@ -3,7 +3,11 @@ public class Main {
     public static void main(String[] args) {
 
 
-        convertGivenTimeToUTC
+        IntersectionWorkingTime intersectionWorkingTime = new IntersectionWorkingTime();
+
+
+        //intersectionWorkingTime.convertGivenTimeToUTC(8);
+
 
     }
 }
