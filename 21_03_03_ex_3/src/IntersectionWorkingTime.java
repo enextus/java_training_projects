2f3c58865fc71d969647ec3b1cedbb23efd52b6e
enextus <enextus@gmail.com>
2021-03-04 08:01:45 +0100

    upd

--- 21_03_03_ex_3/src/IntersectionWorkingTime.java
+++ 21_03_03_ex_3/src/IntersectionWorkingTime.java
@@ -1,47 +1,45 @@
 import java.time.*;
 import java.time.format.DateTimeFormatter;
-import java.time.temporal.ChronoUnit;
 
 public class IntersectionWorkingTime {
 
     public int intersectWorkingTime(LocalTime start1, int hours, ZoneId zone1, LocalTime start2, int hours2, ZoneId zone2) {
 
-        //
+
+        ZonedDateTime tempStart1 = convertGivenTimeToUTC(start1, zone1);
+
+        ZonedDateTime dateAndTimeInSydney = ZonedDateTime.of(tempStart1, zone1);
+        System.out.println("Current date and time in a particular timezone : " + dateAndTimeInSydney);
+
+        /*
+
+        long hoursBetween = ChronoUnit.HOURS.between(now1, now2);
+        long minutesBetween = ChronoUnit.MINUTES.between(now1, now2);
+
+        System.out.println("HERE1");
+
+        System.out.println(hoursBetween);
+        System.out.println(minutesBetween);
+
+        System.out.println("HERE2");*/
 
         return 0;
     }
 
-    public ZonedDateTime convertGivenTimeToUTC(LocalTime start, int hours, ZoneId zone) {
+    public ZonedDateTime convertGivenTimeToUTC(LocalTime start, ZoneId zone) {
 
         // https://zetcode.com/java/localtime/
-
-/*        ZoneId australia = zone; // ZoneId.of("Australia/Sydney");
 
         String str = "2015-01-05 17:00";
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
         LocalDateTime localDateAndTime = LocalDateTime.parse(str, formatter);
 
-        ZonedDateTime dateAndTimeInSydney = ZonedDateTime.of(localDateAndTime, australia );
+        ZonedDateTime dateAndTimeInSydney = ZonedDateTime.of(localDateAndTime, zone);
         System.out.println("Current date and time in a particular timezone : " + dateAndTimeInSydney);
 
         ZonedDateTime utcDate = dateAndTimeInSydney.withZoneSameInstant(ZoneOffset.UTC);
-        System.out.println("Current date and time in UTC : " + utcDate);*/
+        System.out.println("Current date and time in UTC : " + utcDate);
 
-        ZoneId zone1 = ZoneId.of("Europe/Bratislava");
-        ZoneId zone2 = ZoneId.of("Europe/Moscow");
-
-        LocalTime now1 = LocalTime.now(zone1);
-        LocalTime now2 = LocalTime.now(zone2);
-
-        System.out.printf("Bratislava time: %s%n", now1);
-        System.out.printf("Moscow time: %s%n", now2);
-
-        long hoursBetween = ChronoUnit.HOURS.between(now1, now2);
-        long minutesBetween = ChronoUnit.MINUTES.between(now1, now2);
-
-        System.out.println(hoursBetween);
-        System.out.println(minutesBetween);
-
-        return null;
+        return utcDate;
     }
 }
