a4f9da655533ffd649d90ad00153f2ecaa56c596
enextus <enextus@gmail.com>
2021-03-04 08:43:08 +0100

    upd

--- 21_03_03_ex_3/src/Main.java
+++ 21_03_03_ex_3/src/Main.java
@@ -11,22 +11,21 @@ public class Main {
 
 
         LocalTime officeOpeningTime_1 = LocalTime.of(6, 30);
-        System.out.println(officeOpeningTime_1.format(formatterOnlyHoursMinutes));
+            System.out.println(officeOpeningTime_1.format(formatterOnlyHoursMinutes));
         int hours_1 = 10;
         ZoneId zone_1 = ZoneId.of("Europe/Berlin");
 
 
         LocalTime officeOpeningTime_2 = LocalTime.of(10, 0);
-        System.out.println(officeOpeningTime_2.format(formatterOnlyHoursMinutes));
-/*        int hours_2 = 23;
+            System.out.println(officeOpeningTime_2.format(formatterOnlyHoursMinutes));
+/*      int hours_2 = 20;
         ZoneId zone_2 = ZoneId.of("Asia/Seoul");*/
         int hours_2 = 8;
         ZoneId zone_2 = ZoneId.of("Europe/Berlin");
 
 
-        int intersectTime = intersectionWorkingTime.intersectWorkingTime(
-                officeOpeningTime_1, hours_1, zone_1, officeOpeningTime_2, hours_2, zone_2);
-        System.out.println("intersectTime: " + intersectTime);
+        int intersectTime = intersectionWorkingTime.intersectWorkingTime(officeOpeningTime_1, hours_1, zone_1, officeOpeningTime_2, hours_2, zone_2);
+            System.out.println("intersectTime: " + intersectTime);
     }
 
 }
