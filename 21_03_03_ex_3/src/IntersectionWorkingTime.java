52144cdd4138de2b1b670311d6bcf3b2df759190
enextus <enextus@gmail.com>
2021-03-04 09:38:18 +0100

    upd

--- 21_03_03_ex_3/src/IntersectionWorkingTime.java
+++ 21_03_03_ex_3/src/IntersectionWorkingTime.java
@@ -6,27 +6,30 @@ public class IntersectionWorkingTime {
     public int intersectWorkingTime(LocalTime start1, int hours1, ZoneId zone1, LocalTime start2, int hours2, ZoneId zone2) {
 
         ZonedDateTime tempStart1 = convertGivenTimeToUTC(start1, zone1);
-            System.out.println("4. " + "Current date and time in a particular office 1 : " + tempStart1);
+        System.out.println("4. " + "Current date and time in a particular office 1 : " + tempStart1);
 
         ZonedDateTime tempStart2 = convertGivenTimeToUTC(start2, zone2);
-            System.out.println("5. " + "Current date and time in a particular office 2 : " + tempStart2);
+        System.out.println("5. " + "Current date and time in a particular office 2 : " + tempStart2);
 
         long hoursBetweenOpenTime = ChronoUnit.HOURS.between(tempStart1, tempStart2);  // first possibility
         System.out.println("6-open. " + "-> Duration between close time: " + hoursBetweenOpenTime);
 
-
-        ZonedDateTime   tempStart1Closed =   tempStart1.plusHours((long)hours1);  // closed time
-        ZonedDateTime   tempStart2Closed =   tempStart2.plusHours((long)hours2);  // closed time
+        ZonedDateTime tempStart1Closed = tempStart1.plusHours((long) hours1);  // closed time
+        ZonedDateTime tempStart2Closed = tempStart2.plusHours((long) hours2);  // closed time
 
         long durationBetweenCloseTime = Duration.between(tempStart1Closed, tempStart2Closed).toHours(); // second possibility
         System.out.println("6-close. " + "-> Duration between close time: " + durationBetweenCloseTime);
 
-        // long resultTime = tempStart1Open;
+        int resultTime = 1;
 
-/*        if (tempTime <= 0)
-            return 0;*/
+        // pseudoCode:
+        // resultTime = (openTimeOfOffice1 + closeTimeOfOffice2) - (durationBetweenOpenTimes + durationBetweenCloseTimes);
 
-        return (int) durationBetweenCloseTime;
+
+        if (resultTime <= 0)
+            return 0;
+
+        return (int) resultTime;
     }
 
     ZonedDateTime convertGivenTimeToUTC(LocalTime start, ZoneId zone) {
