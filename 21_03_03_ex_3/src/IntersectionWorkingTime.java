20f6210c1034b9b183ee718a219399aca9015759
enextus <enextus@gmail.com>
2021-03-04 07:29:32 +0100

    upd

--- 21_03_03_ex_3/src/IntersectionWorkingTime.java
+++ 21_03_03_ex_3/src/IntersectionWorkingTime.java
@@ -1,5 +1,6 @@
 import java.time.*;
 import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
 
 public class IntersectionWorkingTime {
 
@@ -12,19 +13,34 @@ public class IntersectionWorkingTime {
 
     public ZonedDateTime convertGivenTimeToUTC(LocalTime start, int hours, ZoneId zone) {
 
-        ZoneId australia = ZoneId.of("Australia/Sydney");
-        String str = "2015-01-05 17:00";
+        // https://zetcode.com/java/localtime/
 
+/*        ZoneId australia = zone; // ZoneId.of("Australia/Sydney");
+
+        String str = "2015-01-05 17:00";
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
         LocalDateTime localDateAndTime = LocalDateTime.parse(str, formatter);
-
 
         ZonedDateTime dateAndTimeInSydney = ZonedDateTime.of(localDateAndTime, australia );
         System.out.println("Current date and time in a particular timezone : " + dateAndTimeInSydney);
 
-
         ZonedDateTime utcDate = dateAndTimeInSydney.withZoneSameInstant(ZoneOffset.UTC);
-        System.out.println("Current date and time in UTC : " + utcDate);
+        System.out.println("Current date and time in UTC : " + utcDate);*/
+
+        ZoneId zone1 = ZoneId.of("Europe/Bratislava");
+        ZoneId zone2 = ZoneId.of("Europe/Moscow");
+
+        LocalTime now1 = LocalTime.now(zone1);
+        LocalTime now2 = LocalTime.now(zone2);
+
+        System.out.printf("Bratislava time: %s%n", now1);
+        System.out.printf("Moscow time: %s%n", now2);
+
+        long hoursBetween = ChronoUnit.HOURS.between(now1, now2);
+        long minutesBetween = ChronoUnit.MINUTES.between(now1, now2);
+
+        System.out.println(hoursBetween);
+        System.out.println(minutesBetween);
 
         return null;
     }
--- 21_03_03_ex_3/src/Main.java
+++ 21_03_03_ex_3/src/Main.java
@@ -1,12 +1,28 @@
+import java.time.LocalTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
 
     public static void main(String[] args) {
 
-
         IntersectionWorkingTime intersectionWorkingTime = new IntersectionWorkingTime();
 
+        DateTimeFormatter formatterOnlyHoursMinutes = DateTimeFormatter.ofPattern("HH:mm");
 
-        //intersectionWorkingTime.convertGivenTimeToUTC(8);
+        LocalTime officeOpeningTime_1 = LocalTime.of(9, 30);
+        System.out.println(officeOpeningTime_1.format(formatterOnlyHoursMinutes));
+        int hours_1 = 8;
+        ZoneId zone1 = ZoneId.of("Europe/Berlin");
+
+        LocalTime officeOpeningTime_2 = LocalTime.of(10, 0);
+        System.out.println(officeOpeningTime_2.format(formatterOnlyHoursMinutes));
+        int hours_2 = 6;
+        ZoneId zone2 = ZoneId.of("Asia/Seoul");
+
+
+        intersectionWorkingTime.convertGivenTimeToUTC(officeOpeningTime_1, hours_1, australia);
+
 
 
     }
