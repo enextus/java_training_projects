a33b8250a5ad55dbee59ba637ac767d5d5acd336
enextus <enextus@gmail.com>
2021-03-08 13:34:31 +0100

    upd

--- 21_03_08_echo_server-client_app/src/EchoClient.java
+++ 21_03_08_echo_server-client_app/src/EchoClient.java
@@ -1,4 +1,7 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.Socket;
 
 public class EchoClient {
@@ -9,6 +12,11 @@ public class EchoClient {
         int port = 32000;
 
         try (Socket socket = new Socket(host, port)) {
+
+            PrintWriter outWriter = new PrintWriter(socket.getOutputStream(), true); // we build a PrintWriter
+            BufferedReader inReader = new BufferedReader(new InputStreamReader(socket.getInputStream())); //  we build a PrintWriter
+            //
+
 
         } catch (IOException e) {
             e.printStackTrace();
