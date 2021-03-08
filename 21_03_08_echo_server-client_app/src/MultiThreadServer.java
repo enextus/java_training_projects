3ad81683039e9740ce4442ea3e469fd9a3410b12
enextus <enextus@gmail.com>
2021-03-08 14:05:17 +0100

    upd

--- 21_03_08_echo_server-client_app/src/MultiThreadServer.java
+++ 21_03_08_echo_server-client_app/src/MultiThreadServer.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class MultiThreadServer {
     public static void main(String[] args) {
@@ -7,6 +8,19 @@ public class MultiThreadServer {
         ServerSocket serverSocket = null;
 
         try {
+
+            serverSocket = new ServerSocket(32000);
+            serverSocket.setReuseAddress(true);
+
+            // the main thread is just accepting new connections
+            while (true) {
+
+                Socket socket = serverSocket.accept();
+                System.out.println("New client connected " + socket.getInetAddress().getHostAddress());
+
+                ClientHandler clientSocket = new ClientHandler(socket);
+            }
+
 
         } catch (IOException e) {
             e.printStackTrace();
