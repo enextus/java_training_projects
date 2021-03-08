1712e18e22bd05d64ac97d1c4bdab72214572301
enextus <enextus@gmail.com>
2021-03-08 13:30:37 +0100

    upd

--- 21_03_08_echo_server-client_app/src/EchoClient.java
+++ 21_03_08_echo_server-client_app/src/EchoClient.java
@@ -1,9 +1,18 @@
+import java.io.IOException;
+import java.net.Socket;
+
 public class EchoClient {
 
     public static void main(String[] args) {
 
         String host = "127.0.0.1";
         int port = 32000;
+
+        try (Socket socket = new Socket(host, port)) {
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
 
     }
