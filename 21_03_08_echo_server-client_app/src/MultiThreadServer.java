3e3aa83963bf8beaade000abfea84c11d4d9fbdb
enextus <enextus@gmail.com>
2021-03-08 14:17:38 +0100

    upd

--- 21_03_08_echo_server-client_app/src/MultiThreadServer.java
+++ 21_03_08_echo_server-client_app/src/MultiThreadServer.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
@@ -51,10 +52,34 @@ public class MultiThreadServer {
             PrintWriter out = null;
             BufferedReader in = null;
 
+            try {
+                out = new PrintWriter(clientSocket.getOutputStream(), true);
+                in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
+                String line;
+
+                while ((line = in.readLine()) != null) {
+                    System.out.printf("Sent from client: %s\n", line);
+                    out.println(line);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (out != null) {
+                        out.close();
+                    }
+
+                    if (in != null)
+                        in.close();
+
+                    clientSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
 
     }
-
 
 }
