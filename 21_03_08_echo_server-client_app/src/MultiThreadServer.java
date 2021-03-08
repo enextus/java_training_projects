a88763d8db4c7b39413ddc6f62e14b1cd7bb7547
enextus <enextus@gmail.com>
2021-03-08 13:59:29 +0100

    upd

--- 21_03_08_echo_server-client_app/src/MultiThreadServer.java
+++ 21_03_08_echo_server-client_app/src/MultiThreadServer.java
@@ -1,3 +1,4 @@
+import java.io.IOException;
 import java.net.ServerSocket;
 
 public class MultiThreadServer {
@@ -5,5 +6,24 @@ public class MultiThreadServer {
 
         ServerSocket serverSocket = null;
 
+        try {
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (serverSocket != null) {
+                try {
+                    serverSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
+
+    public static class ClientHandler implements Runnable {
+
+    }
+
+
 }
