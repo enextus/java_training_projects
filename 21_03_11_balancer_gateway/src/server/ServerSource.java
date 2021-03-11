16c9816bdd3b38b534b00682941b2c9568f1db2f
enextus <enextus@gmail.com>
2021-03-11 20:56:48 +0100

    upd

---
+++ 21_03_11_balancer_gateway/src/server/ServerSource.java
@@ -0,0 +1,14 @@
+package server;
+
+public class ServerSource {
+
+    private ServerData serverData;
+
+    public synchronized ServerData getLast() {
+        return serverData;
+    }
+
+    public synchronized void update(ServerData serverData) {
+        this.serverData = serverData;
+    }
+}
