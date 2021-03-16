c82ada44f7fb850c41afa92a0a2b8815b9a2d2fb
enextus <enextus@gmail.com>
2021-03-16 02:35:44 +0100

    upd

--- 21_03_05_TCP_IP/src/Main.java
+++ 21_03_05_TCP_IP/src/Main.java
@@ -14,10 +14,12 @@ public class Main {
 
         // tool for sending lines to the 3000 port program
         PrintStream socketOutput = new PrintStream(socket.getOutputStream());
+
         // tool for receiving responses from the 3000 port program
         BufferedReader socketInput = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
         String line;
+
         while ((line = fromConsole.readLine()) != null && !line.equals("exit")) {
             socketOutput.println(line);
             String response = socketInput.readLine();
