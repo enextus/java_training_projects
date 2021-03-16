d5736959d1a025219610bdab713b9dcb9a893fa1
enextus <enextus@gmail.com>
2021-03-16 03:27:10 +0100

    upd

--- 21_03_12_balancer_stress_client/src/Main.java
+++ 21_03_12_balancer_stress_client/src/Main.java
@@ -3,9 +3,9 @@ import java.util.concurrent.Executors;
 
 public class Main {
 
-    static final int SIMULTANEOUS_CONNECTION_NUMBER = 20;
-    static final int CONNECTION_NUMBER = 1000;
-    static final int MESSAGES_PER_CONNECTION = 1000;
+    static final int SIMULTANEOUS_CONNECTION_NUMBER = 5;
+    static final int CONNECTION_NUMBER = 15;
+    static final int MESSAGES_PER_CONNECTION = 500;
     static final String MESSAGE = "I love you";
 
     static final String GATEWAY_HOST = "localhost";
