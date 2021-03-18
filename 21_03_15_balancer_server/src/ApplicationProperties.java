99c0f5c4a7b71156001e9669a1ed2afe12eb6ead
enextus <enextus@gmail.com>
2021-03-18 12:16:14 +0100

    upd

--- 21_03_15_balancer_server/src/ApplicationProperties.java
+++ 21_03_15_balancer_server/src/ApplicationProperties.java
@@ -1,30 +1,19 @@
 
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.concurrent.atomic.AtomicInteger;
+import java.util.Properties;
 
-public class Main {
+public class ApplicationProperties {
 
-    private static final String DEFAULT_PROPS_PATH = "config/application.props";
-    private static final String UDP_BALANCER_PORT_KEY = "udp.balancer.port";
-    private static final String BALANCER_HOST_KEY = "balancer.host";
-    private static final String LOAD_TIME_INTERVAL_KEY = "load.time.interval";
+    Properties properties;
 
-    public static void main(String[] args) throws IOException {
-        int selfTcpPort = Integer.parseInt(args[0]);
+    ApplicationProperties(String filePath) throws IOException {
+        properties = new Properties();
+        properties.load(new FileReader(filePath));
+    }
 
-        String propsPath = args.length > 1 ? args[1] : DEFAULT_PROPS_PATH;
-        ApplicationProperties properties = new ApplicationProperties(propsPath);
-
-        AtomicInteger connectionCounter = new AtomicInteger();
-
-        TcpGatewayListener tcpGatewayListener = new TcpGatewayListener(selfTcpPort, connectionCounter);
-        new Thread(tcpGatewayListener).start();
-
-        String balancerHost = properties.getProperty(BALANCER_HOST_KEY);
-        int udpBalancerPort = Integer.parseInt(properties.getProperty(UDP_BALANCER_PORT_KEY));
-        int sendInterval = Integer.parseInt(properties.getProperty(LOAD_TIME_INTERVAL_KEY));
-        UdpLoadSender loadSender = new UdpLoadSender(balancerHost, udpBalancerPort, connectionCounter, sendInterval, selfTcpPort);
-        new Thread(loadSender).start();
+    public String getProperty(String key) {
+        return properties.getProperty(key);
     }
 
 }
