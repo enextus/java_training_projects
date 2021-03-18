7ebc90da9b75833b2c051f47ec5586b220efa288
enextus <enextus@gmail.com>
2021-03-18 12:23:38 +0100

    upd

--- 21_03_12_balancer_stress_client/src/ConnectionTask.java
+++ 21_03_12_balancer_stress_client/src/ConnectionTask.java
@@ -20,9 +20,7 @@ class ConnectionTask implements Runnable {
 
     @Override
     public void run() {
-
         Socket socket;
-
         try {
             System.out.println(gatewayHost + ":" + gatewayPort);
             socket = new Socket(gatewayHost, gatewayPort);
