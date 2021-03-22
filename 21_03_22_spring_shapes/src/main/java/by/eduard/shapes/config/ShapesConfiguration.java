7d5e2799ffbce7dff33523c37f7590bbd32d9511
enextus <enextus@gmail.com>
2021-03-22 11:01:14 +0100

    upd

--- 21_03_22_spring_shapes/src/main/java/by/eduard/shapes/config/ShapesConfiguration.java
+++ 21_03_22_spring_shapes/src/main/java/by/eduard/shapes/config/ShapesConfiguration.java
@@ -2,23 +2,31 @@ package by.eduard.shapes.config;
 
 import by.eduard.shapes.entity.Line;
 import by.eduard.shapes.entity.Rectangle;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 
 @Configuration
 public class ShapesConfiguration {
 
     @Bean
+    @Qualifier("picture")
+    @Order(2)
     public Line line1() {
         return new Line('x', 20);
     }
 
     @Bean
+    @Qualifier("picture")
+    @Order(1)
     public Line line2() {
         return new Line('-', 32);
     }
 
     @Bean
+    @Qualifier("picture")
+    @Order(3)
     public Line line3() {
         return new Line('+', 17);
     }
