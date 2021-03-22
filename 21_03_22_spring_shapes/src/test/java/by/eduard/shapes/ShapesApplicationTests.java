271dd88361823c9003284087fbbd7f7253f828d1
enextus <enextus@gmail.com>
2021-03-22 10:21:59 +0100

    upd

---
+++ 21_03_22_spring_shapes/src/test/java/by/eduard/shapes/ShapesApplicationTests.java
@@ -0,0 +1,13 @@
+package by.eduard.shapes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShapesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
