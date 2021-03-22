8df8786460e85abf9fda6168f408dc85ae310444
enextus <enextus@gmail.com>
2021-03-22 10:31:36 +0100

    upd

--- 21_03_22_spring_shapes/src/main/java/by/eduard/shapes/config/ShapesConfiguration.java
+++ 21_03_22_spring_shapes/src/main/java/by/eduard/shapes/config/ShapesConfiguration.java
@@ -1,4 +1,27 @@
 package by.eduard.shapes.config;
 
+import by.eduard.shapes.entity.Line;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class ShapesConfiguration {
+
+    @Bean
+    public Line line1() {
+        return new Line('x', 20);
+    }
+
+
+    @Bean
+    public Line line2() {
+        return new Line('-', 32);
+    }
+
+    @Bean
+    public Line line3() {
+        return new Line('+', 17);
+    }
+
+
 }
