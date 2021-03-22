d3e10d676b3c0c3a5d520fa14002584041d5f5d3
enextus <enextus@gmail.com>
2021-03-22 11:15:34 +0100

    upd

--- 21_03_22_spring_shapes/src/main/java/by/eduard/shapes/ShapesFlow.java
+++ 21_03_22_spring_shapes/src/main/java/by/eduard/shapes/ShapesFlow.java
@@ -1,4 +1,19 @@
 package by.eduard.shapes;
 
-public class ShapesFlow {
+import by.eduard.shapes.entity.Line;
+import by.eduard.shapes.entity.Picture;
+import org.springframework.boot.ApplicationRunner;
+
+import java.util.List;
+
+public class ShapesFlow implements ApplicationRunner {
+
+    private final List<Line> lines;
+    private final Picture picture;
+
+
+    public ShapesFlow(List<Line> lines, Picture picture) {
+
+    }
+
 }
