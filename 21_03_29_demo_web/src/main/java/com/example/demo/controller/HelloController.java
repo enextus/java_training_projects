406772c6f7e25b6c29285c6fd73088ed627c8854
enextus <enextus@gmail.com>
2021-03-29 13:49:42 +0200

    upd

--- 21_03_29_demo_web/src/main/java/com/example/demo/controller/HelloController.java
+++ 21_03_29_demo_web/src/main/java/com/example/demo/controller/HelloController.java
@@ -5,12 +5,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.util.Locale;
+
 @Controller
 public class HelloController {
 
     @GetMapping("/hello/{name}")
     public String hello(@PathVariable String name, Model model) {
-
+        String resName = name.toUpperCase(Locale.ROOT);
         model.addAttribute(name);
         return "hello";
     }
