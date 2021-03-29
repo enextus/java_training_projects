d75e5e32f733ddc24abc8fed3e5d6770c7709ae5
enextus <enextus@gmail.com>
2021-03-29 13:20:20 +0200

    upd

--- 21_03_29_demo_web/src/main/java/com/example/demo/controller/Contorller.java
+++
@@ -1,4 +0,0 @@
-package com.example.demo.controller;
-
-public @interface Contorller {
-}
--- 21_03_29_demo_web/src/main/java/com/example/demo/controller/HelloController.java
+++ 21_03_29_demo_web/src/main/java/com/example/demo/controller/HelloController.java
@@ -1,13 +1,16 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@Contorller
+@Controller
 public class HelloController {
 
     @GetMapping("/hello/{name}")
-    public String hello(@PathVariable String name) {
+    public String hello(@PathVariable String name, Model model) {
+        model.addAttribute(name);
         return "hello";
     }
 }
