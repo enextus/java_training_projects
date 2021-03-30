13694702bc7694a49c495ae0f9f49c35fdbcf7f9
enextus <enextus@gmail.com>
2021-03-30 05:05:50 +0200

    upd

---
+++ 21_03_29_balancer_gateway/src/main/java/by/project/balancer_gateway/BalancerGatewayApplication.java
@@ -0,0 +1,13 @@
+package by.project.balancer_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BalancerGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BalancerGatewayApplication.class, args);
+    }
+
+}
---
+++ 21_03_29_balancer_gateway/src/main/java/by/project/balancer_gateway/server/ServerSource.java
@@ -0,0 +1,18 @@
+package by.project.balancer_gateway.server;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ServerSource {
+
+    private ServerData serverData;
+
+    public synchronized ServerData getLast() {
+        return serverData;
+    }
+
+    public synchronized void update(ServerData serverData) {
+        this.serverData = serverData;
+    }
+
+}
