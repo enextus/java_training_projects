1818f0ef7953fb70352f9e12256f9a0b7dd6c9b0
enextus <enextus@gmail.com>
2021-03-31 13:05:36 +0200

    upd

--- 21_03_29_demo_web/src/main/java/com/example/demo_web/dto/Greetings.java
+++ 21_03_29_demo_web/src/main/java/com/example/demo_web/dto/Greetings.java
@@ -1,16 +1,23 @@
 package com.example.demo_web.dto;
 
+//Data transfer object dto
 public class Greetings {
-
-    public String name = "werwer";
-    public String greetings = "YES!";
 
     public Greetings() {
     }
 
+    public String name = "werwer";
+    public String greetings = "YES!";
+
+
     public Greetings(String name, String greetings) {
         this.name = name;
         this.greetings = greetings;
+    }
+
+    @Override
+    public String toString() {
+        return greetings + " " + name;
     }
 
 }
