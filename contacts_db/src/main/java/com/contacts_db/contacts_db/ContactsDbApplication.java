47cf43e4e40f8bb734c2f19eacd645faaededfb8
enextus <enextus@gmail.com>
2021-05-14 12:44:42 +0200

    sdfgsdf

---
+++ contacts_db/src/main/java/com/contacts_db/contacts_db/ContactsDbApplication.java
@@ -0,0 +1,13 @@
+package com.contacts_db.contacts_db;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsDbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ContactsDbApplication.class, args);
+    }
+
+}
---
+++ contacts_db/src/test/java/com/contacts_db/contacts_db/ContactsDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contacts_db.contacts_db;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsDbApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
